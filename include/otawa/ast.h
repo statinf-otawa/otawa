d0c484ba74717cb05ed5c61bbc53000e3d380570
casse <devnull@localhost>
2004-05-25 13:53:07 +0000

    Multiple header file reorganization. Fixed version of AST management.

---
+++ include/otawa/ast.h
@@ -0,0 +1,21 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2003, IRIT UPS.
+ *
+ *	src/otawa/ast.h -- global header for using AST.
+ */
+#ifndef OTAWA_AST_H
+#define OTAWA_AST_H
+
+#include <otawa/ast/AST.h>
+#include <otawa/ast/ASTInfo.h>
+#include <otawa/ast/FunAST.h>
+#include <otawa/ast/BlockAST.h>
+#include <otawa/ast/CallAST.h>
+#include <otawa/ast/SeqAST.h>
+#include <otawa/ast/IfAST.h>
+#include <otawa/ast/WhileAST.h>
+#include <otawa/ast/DoWhileAST.h>
+#include <otawa/ast/ForAST.h>
+
+#endif	// OTAWA_AST_H
