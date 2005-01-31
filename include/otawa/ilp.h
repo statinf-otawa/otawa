4a9aa74999bcdb79a2a871aa948e0ddd917ed36c
casse <devnull@localhost>
2005-01-31 09:39:37 +0000

    Added ILP interfaces.

---
+++ include/otawa/ilp.h
@@ -0,0 +1,14 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/ilp.h -- common header for ILP module.
+ */
+#ifndef OTAWA_ILP_H
+#define OTAWA_ILP_H
+
+#include <otawa/ilp/Var.h>
+#include <otawa/ilp/Constraint.h>
+#include <otawa/ilp/System.h>
+
+#endif	// OTAWA_ILP_H
