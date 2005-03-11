ffde24e46a582e2605945e5500d50fb8cec90204
casse <devnull@localhost>
2005-03-11 16:42:57 +0000

    New header directories.

---
+++ include/otawa/otawa.h
@@ -0,0 +1,13 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2003, IRIT UPS.
+ *
+ *	otawa/otawa.h -- base header file.
+ */
+#ifndef OTAWA_OTAWA_H
+#define OTAWA_OTAWA_H
+
+#include <otawa/manager.h>
+#include <otawa/prog/FrameWork.h>
+
+#endif	// OTAWA_OTAWA_H
