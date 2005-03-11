bc9ee4da84547f3d0d87cfd4c5e0c9c655b5a31d
casse <devnull@localhost>
2005-03-11 16:45:03 +0000

    Code cleanup. Added of Collection access to BB in CFG.

--- include/otawa/cfg/CFG.h
+++ include/otawa/cfg/CFG.h
@@ -7,6 +7,10 @@
 #ifndef OTAWA_CFG_CFG_H
 #define OTAWA_CFG_CFG_H
 
+#include <assert.h>
+#include <elm/Collection.h>
+#include <elm/genstruct/Vector.h>
+
 namespace otawa {
 
 // Classes
@@ -15,17 +19,55 @@ class Code;
 
 	
 // CFG class
-class CFG: public ProgObject {
+class CFG: public ProgObject, private elm::Collection<BasicBlock *> {
 	Code *_code;
 	BasicBlock *ent;
+
+	virtual elm::IteratorInst<BasicBlock *> *visit(void) const;
+	virtual elm::MutableCollection<BasicBlock *> *empty(void) const;
+
 public:
 	static id_t ID;
+	
+	// Iterator
+	class BBIterator: public elm::IteratorInst<BasicBlock *> {
+		elm::genstruct::Vector<BasicBlock *> bbs;
+		int pos;
+	public:
+		inline BBIterator(const CFG *cfg);
+		virtual bool ended(void) const;
+		virtual BasicBlock *item(void) const;
+		virtual void next(void);
+	};
+	
+	// Methods
 	CFG(Code *code, BasicBlock *entry);
-	inline BasicBlock *entry(void) const { return ent; };
-	inline Code *code(void) const { return _code; };
+	inline BasicBlock *entry(void) const;
+	inline Code *code(void) const;
 	String label(void);
 	address_t address(void);
+	inline elm::Collection<BasicBlock *>& bbs(void);
 };
+
+
+// Inlines
+inline elm::Collection<BasicBlock *>& CFG::bbs(void) {
+	return *this;
+}
+
+inline BasicBlock *CFG::entry(void) const {
+	return ent;
+}
+
+inline Code *CFG::code(void) const {
+	return _code;
+};
+
+inline CFG::BBIterator::BBIterator(const CFG *cfg) {
+	assert(cfg);
+	bbs.add(cfg->entry());
+	pos = 0;
+}
 
 } // otawa
 
