7ccc631c2984c3e62e017dda799d79fc5e08bda6
casse <devnull@localhost>
2005-03-11 16:50:44 +0000

    First version.

---
+++ include/otawa/ipet/IPET.h
@@ -0,0 +1,21 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/ipet/IPET.h -- IPET class interface.
+ */
+#ifndef OTAWA_IPET_IPET_H
+#define OTAWA_IPET_IPET_H
+
+#include <otawa/prop/Identifier.h>
+
+namespace otawa {
+	
+class IPET {
+public:
+	static Identifier ID_BB_Time;
+};
+		
+} // otawa
+
+#endif	// OTAWA_IPET_IPET_H
