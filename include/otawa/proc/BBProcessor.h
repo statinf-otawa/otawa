5b0da7334fc98f65145cd45d7d57aaa09ad34da9
casse <devnull@localhost>
2005-03-16 10:14:39 +0000

    BBProcessor class added.

---
+++ include/otawa/proc/BBProcessor.h
@@ -0,0 +1,25 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/proc/BBProcessor.h -- BBProcessor class interface.
+ */
+#ifndef OTAWA_PROC_BBPROCESSOR_H
+#define OTAWA_PROC_BBPROCESSOR_H
+
+#include <otawa/cfg/BasicBlock.h>
+#include <otawa/proc/CFGProcessor.h>
+
+namespace otawa {
+
+class BBProcessor: public CFGProcessor {
+public:
+	virtual void processBB(BasicBlock *bb) = 0;
+	
+	// CFGProcessor overload
+	virtual void processCFG(CFG *cfg);
+};
+	
+} // otawa
+
+#endif	// OTAWA_PROC_BBPROCESSOR_H
