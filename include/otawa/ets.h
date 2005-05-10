4c9ffc861719f5b79d37ce8927de7f506542121f
benoit <devnull@localhost>
2005-05-10 15:08:52 +0000

    Support for ETS processors.

---
+++ include/otawa/ets.h
@@ -0,0 +1,15 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2003, IRIT UPS.
+ *
+ *	src/otawa/ets.h -- global header for using ETS.
+ */
+#ifndef OTAWA_ETS_H
+#define OTAWA_ETS_H
+
+#include <otawa/ets/ETS.h>
+#include <otawa/ets/WCETComputation.h>
+#include <otawa/ets/TrivialAstBlockTime.h>
+#include <otawa/ets/FlowFactReader.h>
+
+#endif	// OTAWA_ETS_H
