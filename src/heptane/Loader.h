9809b3b86aded1844e697a87c7318b1e23732f0d
benoit <devnull@localhost>
2005-05-26 17:26:49 +0000

    Change for debug (display).

--- src/heptane/Loader.h
+++ src/heptane/Loader.h
@@ -9,13 +9,16 @@
 
 #include <otawa/gliss.h>
 
+//#define LOAD_OUT(txt) txt
+#define LOAD_OUT(txt)
+
 namespace otawa { namespace heptane {
 
 // HeptLoader class
 class Loader: public gliss::Loader {
 public:
-	inline Loader(void) { cout << "INIT Heptane\n"; };
-	inline ~Loader(void) { cout << "FINISH Heptane\n"; };
+	inline Loader(void) { LOAD_OUT(cout << "INIT Heptane\n"); };
+	inline ~Loader(void) { LOAD_OUT(cout << "FINISH Heptane\n"); };
 	
 	// gliss::Loader overload
 	virtual CString getName(void) const;
