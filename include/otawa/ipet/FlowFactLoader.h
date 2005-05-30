ec71841064ef2883d7ce5ca77a8be6b698b58f98
casse <devnull@localhost>
2005-05-30 08:15:17 +0000

    Flow fact loader for IPET method.

---
+++ include/otawa/ipet/FlowFactLoader.h
@@ -0,0 +1,40 @@
+/*
+ * $Id$
+ * Copyright (c) 2005, IRIT-UPS <casse@irit.fr>
+ *
+ * otawa/ipet/IPETFlowFactLoader.h -- IPETFlowFactLoader class interface.
+ */
+#ifndef OTAWA_IPET_IPET_FLOW_FACT_LOADER_H
+#define OTAWA_IPET_IPET_FLOW_FACT_LOADER_H
+
+#include <otawa/proc/CFGProcessor.h>
+#include <otawa/util/FlowFactLoader.h>
+
+namespace otawa { 
+
+// Externals
+namespace ilp {
+	class System;
+} // ilp
+
+namespace ipet {
+	
+// FlowFactLoader clas
+class FlowFactLoader: public CFGProcessor, private otawa::FlowFactLoader {
+	CFG *cfg;	
+	otawa::ilp::System *system;
+	bool dom_done;
+protected:
+	// FlowFactLoader overload
+	virtual void onError(const char *fmt, ...);
+	virtual void onLoop(address_t addr, int count);	
+public:
+
+	// CFGProcessor overload
+	virtual void processCFG(FrameWork *fw, CFG *cfg);
+};
+
+
+} } // otawa::ipet
+
+#endif // OTAWA_IPET_IPET_FLOW_FACT_LOADER_H
---
+++ src/prog/ipet_FlowFactLoader.cpp
@@ -0,0 +1,92 @@
+/*
+ * $Id$
+ * Copyright (c) 2005, IRIT-UPS <casse@irit.fr>
+ *
+ * src/prog/ipet_IPETFlowFactLoader.h -- IPETFlowFactLoader class implementation.
+ */
+
+#include <otawa/cfg.h>
+#include <elm/io.h>
+#include <otawa/ipet/FlowFactLoader.h>
+#include <otawa/ipet/IPET.h>
+#include <otawa/util/Dominance.h>
+#include <otawa/ilp.h>
+
+namespace otawa { namespace ipet {
+
+/**
+ * @class FlowFactLoader
+ * This processor allows using extern flow facts in an IPET system.
+ */
+
+
+/**
+ */
+void FlowFactLoader::onError(const char *fmt, ...) {
+	assert(fmt);
+	va_list args;
+	va_start(args, fmt);
+	StringBuffer buffer;
+	buffer.print(fmt, args);
+	cout << buffer.toString();
+	va_end(args);
+}
+
+
+/**
+ */
+void FlowFactLoader::onLoop(address_t addr, int count) {
+	assert(cfg);
+	assert(system);
+	assert(count >= 0);
+	cout << "LOOP " << count << " times at " << addr << "\n";
+	
+	// Process basic blocks
+	for(CFG::BBIterator bb(cfg); bb; bb++)
+		if(bb->address() == addr) {
+			
+			// Check domination
+			if(!cfg->entry()->getProp(&Dominance::ID_RevDom)) {
+				Dominance dom;
+				dom.processCFG(0, cfg);
+			}
+			
+			// Build the constraint
+			otawa::ilp::Constraint *cons =
+				system->newConstraint(otawa::ilp::Constraint::LE);
+			for(Iterator<Edge *> edge(bb->inEdges()); edge; edge++) {
+				assert(edge->source());
+				otawa::ilp::Var *var =
+					edge->source()->use<otawa::ilp::Var *>(IPET::ID_Var);
+				if(Dominance::dominates(bb, edge->source()))
+					cons->addLeft(1, var);
+				else
+					cons->addRight(count, var);
+			}
+		}
+}
+
+
+/**
+ */
+void FlowFactLoader::processCFG(FrameWork *fw, CFG *_cfg) {
+	assert(fw);
+	assert(_cfg);
+	
+	// Initialization
+	cfg = _cfg;
+	system = cfg->get<ilp::System *>(IPET::ID_System, 0);
+	dom_done = false;
+	
+	// Get an ILP system
+	if(!system) {
+		system = fw->newILPSystem(true);
+		cfg->set<ilp::System *>(IPET::ID_System, system);
+		out << "INFO: ILP system created.\n";
+	}
+	
+	// Launch load
+	run(fw);
+}
+	
+} } // otawa::ipet
