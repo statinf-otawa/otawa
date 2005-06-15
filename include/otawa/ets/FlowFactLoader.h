bed254c34e0e01718de7d2a01ec242d3d8d4c645
benoit <devnull@localhost>
2005-06-15 14:19:39 +0000

    Managing of FlowFactLoader to allow using extern flow facts in ETS.

---
+++ include/otawa/ets/FlowFactLoader.h
@@ -0,0 +1,41 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/ets/FlowFactLoader.h -- FlowFactLoader class interface.
+ */
+ 
+#ifndef OTAWA_ETS_FLOWFACTLOADER_H
+#define OTAWA_ETS_FLOWFACTLOADER_H
+
+#include <otawa/proc/ASTProcessor.h>
+#include <otawa/util/FlowFactLoader.h>
+#include <elm/genstruct/HashTable.h>
+
+namespace otawa { namespace ets {
+
+// FlowFactLoader class	
+class FlowFactLoader: public ASTProcessor, private otawa::FlowFactLoader {
+	public :
+		genstruct::HashTable<void *, int> loop_table;
+		
+		inline FlowFactLoader(FrameWork *fw);
+		
+		// ASTProcessor overload
+		void processAST(FrameWork *fw, AST *ast);
+		
+	protected:
+		// FlowFactLoader overload
+		virtual void onError(const char *fmt, ...);
+		virtual void onLoop(address_t addr, int count);
+};
+
+//inlines
+inline FlowFactLoader::FlowFactLoader(FrameWork *fw) {
+	run(fw);
+}
+
+} } // otawa::ets
+
+#endif // OTAWA_ETS_FLOWFACTLOADER_H
+
