f6712cbc38f09156da2526d6163a807ac951f662
casse <devnull@localhost>
2005-08-29 15:09:12 +0000

    Added plugin for ILP engine.

---
+++ include/otawa/ilp/ILPPlugin.h
@@ -0,0 +1,26 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT-UPS.
+ *
+ *	otawa/plugins/ILPlLugin.h -- ILPPlugin class interface.
+ */
+#ifndef OTAWA_PLUGINS_ILP_PLUGIN_H
+#define OTAWA_PLUGINS_ILP_PLUGIN_H
+
+#include <elm/system/Plugin.h>
+
+namespace otawa { namespace ilp {
+	
+// External class
+class System;
+
+// ILPPlugin class
+class ILPPlugin: public elm::system::Plugin {
+public:
+	ILPPlugin(elm::String name, const elm::Version& plugger_version);
+	virtual System *newSystem(void) = 0;
+};
+
+} } // otawa::ilp
+
+#endif //OTAWA_PLUGINS_ILP_PLUGIN_H
---
+++ include/otawa/prog/Manager.h
@@ -0,0 +1,69 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2003, IRIT UPS.
+ *
+ *	manager.h -- manager classes interface.
+ */
+#ifndef OTAWA_MANAGER_H
+#define OTAWA_MANAGER_H
+
+#include <elm/datastruct/Vector.h>
+#include <elm/system/Plugger.h>
+#include <otawa/properties.h>
+#include <otawa/base.h>
+#include <otawa/platform.h>
+#include <otawa/program.h>
+#include <otawa/cfg.h>
+#include <otawa/prog/FrameWork.h>
+#include <otawa/prog/Loader.h>
+using namespace elm;
+
+namespace otawa {
+
+// Classes
+class File;
+class Platform;
+class Manager;
+namespace ilp {
+	class System;
+}
+
+// LoadException class
+class LoadException: public Exception {
+public:
+	LoadException(const String& message);
+	LoadException(const char *format, va_list args);
+	LoadException(const char *format, ...);
+};
+
+
+// UnsupportedPlatformException class
+class UnsupportedPlatformException: public Exception {
+public:
+	UnsupportedPlatformException(const String& message);
+	UnsupportedPlatformException(const char *format, va_list args);
+	UnsupportedPlatformException(const char *format, ...);
+};
+
+
+// Manager class
+class Manager {
+	friend class FrameWork;
+	datastruct::Vector<Loader *> loaders;
+	datastruct::Vector<Platform *> platforms;
+	datastruct::Vector<FrameWork *> frameworks;
+	elm::system::Plugger ilp_plugger;
+public:
+	Manager(void);
+	~Manager(void);
+	Loader *findLoader(CString name);
+	Platform *findPlatform(CString name);
+	Platform *findPlatform(const PlatformId& id);
+	FrameWork *load(CString path, PropList& props);
+	ilp::System *newILPSystem(String plugin = "");
+};
+
+
+};
+
+#endif	// OTAWA_MANAGER_H
