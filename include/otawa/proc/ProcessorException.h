118b10c439f1b34bea4b47bc1a1d46cee8e62a72
casse <devnull@localhost>
2005-08-30 16:46:31 +0000

    Added ProcessorException class.

---
+++ include/otawa/proc/ProcessorException.h
@@ -0,0 +1,27 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/proc/ProcessorException.h -- ProcessorException class interface.
+ */
+#ifndef OTAWA_PROC_PROCESSOR_EXCEPTION_H
+#define OTAWA_PROC_PROCESSOR_EXCEPTION_H
+
+#include <otawa/proc/Processor.h>
+
+namespace otawa {
+
+// ProcessorException class
+class ProcessorException: public otawa::Exception {
+	String build(const Processor& proc, elm::String message);
+	String build(const Processor& proc, elm::CString format, elm::VarArg& args);
+public:
+	ProcessorException(const Processor& proc, elm::String message);
+	ProcessorException(const Processor& proc, elm::CString format, ...);
+	ProcessorException(const Processor& proc, elm::CString format,
+		elm::VarArg& args);
+};
+
+} // otawa
+
+#endif //OTAWA_PROC_PROCESSOR_EXCEPTION_H
---
+++ src/prog/proc_ProcessorException.cpp
@@ -0,0 +1,83 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	src/prog/proc_ProcessorException.cpp -- ProcessorException class implementation.
+ */
+
+#include <otawa/proc/ProcessorException.h>
+
+using namespace elm;
+
+namespace otawa {
+
+/**
+ * @class ProcessorException
+ * This class is used for returning exceptions from the processors.
+ */
+
+
+/**
+ */
+String ProcessorException::build(const Processor& proc, elm::String message) {
+	StringBuffer buffer;
+	if(!proc.name())
+		buffer << "no name";
+	else {
+		buffer << proc.name();
+		if(proc.version())
+			buffer << '(' << proc.version() << ')';
+	}
+	buffer << ": ";
+	buffer << message;
+	return buffer.toString();
+}
+
+
+/**
+ */
+String ProcessorException::build(const Processor& proc, elm::CString format,
+elm::VarArg& args) {
+	StringBuffer buffer;
+	buffer.format(format, args.args());
+	return build(proc, buffer.toString());	
+}
+
+
+/**
+ * Build a processor exception with a simple message.
+ * @param proc		Processor throwing the exception.
+ * @param message	Exception message.
+ */
+ProcessorException::ProcessorException(const Processor& proc,
+elm::String message) {
+	setMessage(build(proc, message));
+}
+
+
+/**
+ * Build a processor exception with a-la C formatted message.
+ * @param proc		Processor throwing the exception.
+ * @param format	Format string.
+ * @param ...		Variable arguments.
+ */
+ProcessorException::ProcessorException(const Processor& proc,
+elm::CString format, ...) {
+	VARARG_BEGIN(args, format)
+		setMessage(build(proc, format, args));
+	VARARG_END
+}
+
+
+/**
+ * Build a processor exception with a-la C formatted message.
+ * @param proc		Processor throwing the exception.
+ * @param format	Format string.
+ * @param args		Variable arguments.
+ */
+ProcessorException::ProcessorException(const Processor& proc,
+elm::CString format, elm::VarArg& args) {
+	setMessage(build(proc, format, args));
+}
+
+} // otawa
