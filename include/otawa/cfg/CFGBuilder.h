4c4ab516542f6e8d118ac5cf696c93dcc1cde7ca
casse <devnull@localhost>
2005-09-06 14:34:56 +0000

    This class implements a processor for CFG building.

---
+++ include/otawa/cfg/CFGBuilder.h
@@ -0,0 +1,37 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/cfg/CFGBuilder.h -- CFGBuilder class interface.
+ */
+
+#ifndef OTAWA_CFG_CFG_BUILDER_H
+#define OTAWA_CFG_CFG_BUILDER_H
+
+#include <otawa/proc/Processor.h>
+#include <otawa/cfg/CFGInfo.h>
+
+namespace otawa {
+
+// CFGBuilder class
+class CFGBuilder: public Processor {
+	static Identifier ID_Entry;
+	datastruct::Vector<CFG *> _cfgs;
+	
+	BasicBlock *nextBB(Inst *inst);
+	BasicBlock *thisBB(Inst *inst);
+	void addSubProgram(Inst *inst);
+	void buildCFG(Code *code);
+	void addFile(File *file);
+	void buildAll(FrameWork *fw);
+public:
+	CFGBuilder(const PropList& props = PropList::EMPTY);
+	
+	// Processor overload
+	virtual void processFrameWork(FrameWork *fw);
+};
+
+} // otawa
+
+#endif // OTAWA_CFG_CFG_BUILDER_H
+
