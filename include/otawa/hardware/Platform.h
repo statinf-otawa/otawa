c8117f11702e33c34931cc1dd8f030278fb615a0
casse <devnull@localhost>
2005-09-08 14:07:47 +0000

    Platform moved in its own header file.

---
+++ include/otawa/hardware/Platform.h
@@ -0,0 +1,96 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005, IRIT UPS.
+ *
+ *	otawa/hardware/Platform.h -- Platform class interface.
+ */
+#ifndef OTAWA_HARDWARE_PLATFORM_H
+#define OTAWA_HARDWARE_PLATFORM_H
+
+#include <otawa/properties.h>
+
+namespace otawa {
+
+// External classes
+class Manager;
+class CacheConfiguration;
+
+// Platform class
+class Platform {
+	friend class Manager;
+
+public:
+
+	// Platform identification
+	class Identification {
+		elm::String name;
+		elm::String arch;
+		elm::String _abi;
+		elm::String mach;
+		void split(void);
+	public:
+		Identification(const elm::String _name);
+		Identification(elm::CString arch, elm::CString abi, elm::CString machine);
+		inline const elm::String& architecture(void) const;
+		inline const elm::String& abi(void) const;
+		inline const elm::String& machine(void) const;
+	};
+
+private:
+	Identification id;
+	const CacheConfiguration *_cache;
+	void configure(const PropList& props);
+
+protected:
+	virtual ~Platform(void) { };
+
+public:
+	Platform(const Identification& id, const PropList& props = PropList::EMPTY);
+	Platform(const Platform& platform, const PropList& props = PropList::EMPTY);
+
+	// Static
+	static Identifier ID_Cache;
+
+	// Cache information
+	inline const Identification& identification(void) const;
+	inline const CacheConfiguration& cache(void) const;
+
+	// Compatibility test
+	virtual bool accept(const Identification& id);
+	inline bool accept(elm::CString name);
+	inline bool accept(const elm::String& name);
+};
+
+// Inlines
+inline const Platform::Identification& Platform::identification(void) const {
+	return id;
+}
+
+inline const CacheConfiguration& Platform::cache(void) const {
+	return *_cache;
+}
+
+inline bool Platform::accept(elm::CString name) {
+	return accept(Identification(name));
+}
+
+inline bool Platform::accept(const elm::String& name) {
+	return accept(Identification(name));
+}
+
+// Platform::Identification inlines
+inline const elm::String& Platform::Identification::architecture(void) const {
+	return arch;
+}
+
+inline const elm::String& Platform::Identification::abi(void) const {
+	return _abi;
+}
+
+inline const elm::String& Platform::Identification::machine(void) const {
+	return mach;
+}
+
+} // otawa
+
+#endif // OTAWA_HARDWARE_PLATFORM_H
