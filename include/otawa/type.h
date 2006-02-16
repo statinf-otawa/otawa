0ff6d7ee0d21aba174141b43d13f92c5f4d6a60e
casse <devnull@localhost>
2006-02-16 17:43:41 +0000

    Revival of the type system for RTTI in identifiers.

--- include/otawa/type.h
+++ include/otawa/type.h
@@ -7,85 +7,114 @@
 #ifndef OTAWA_TYPE_H
 #define OTAWA_TYPE_H
 
+#include <elm/io.h>
+
 namespace otawa {
 
 
 // Defined class
 class Type;
 class BaseType;
+class NoType;
 class BlockType;
-
-
-// Class kind
-typedef enum type_kind_t {
-	TYPE_None = 0,
-	TYPE_Base,
-	TYPE_Enum,
-	TYPE_Array,
-	TYPE_Struct,
-	TYPE_Union,
-	TYPE_Ptr,
-	TYPE_Fun
-} type_kind_t;
-
-
-// Base types
-typedef enum type_base_t {
-	TYPE_Void = 0,
-	TYPE_Block,
-	TYPE_Bool,
-	TYPE_Int8,
-	TYPE_UInt8,
-	TYPE_Int16,
-	TYPE_UInt16,
-	TYPE_Int32,
-	TYPE_UInt32,
-	TYPE_Int64,
-	TYPE_UInt64,
-	TYPE_Float32,
-	TYPE_Float64,
-	TYPE_Float128,
-	TYPE_Addr32
-} type_base_t;
 
 
 // Type class
 class Type {
 public:
-	virtual type_kind_t kind(void) = 0;
-	virtual BaseType *toBase(void) { return 0; };
+
+	// Kind
+	typedef enum kind_t {
+		NONE = 0,
+		BASE,
+		ENUM,
+		ARRAY,
+		STRUCT,
+		UNION,
+		PTR,
+		FUN
+	} kind_t;
+
+	// Base types
+	typedef enum base_t {
+		VOID = 0,
+		BLOCK,
+		BOOL,
+		CHAR,
+		INT8,
+		UINT8,
+		INT16,
+		UINT16,
+		INT32,
+		UINT32,
+		INT64,
+		UINT64,
+		FLOAT32,
+		FLOAT64,
+		FLOAT128,
+		ADDR32,
+		CSTRING,
+		BASE_TOP
+	} base_t;
+
+	// Predefined types
+	static const NoType no_type;
+	static const BaseType void_type;
+	static const BaseType block_type;
+	static const BaseType bool_type;
+	static const BaseType char_type;
+	static const BaseType int8_type;
+	static const BaseType uint8_type;
+	static const BaseType int16_type;
+	static const BaseType uint16_type;
+	static const BaseType int32_type;
+	static const BaseType uint32_type;
+	static const BaseType int64_type;
+	static const BaseType uint64_type;
+	static const BaseType float32_type;
+	static const BaseType float64_type;
+	static const BaseType float128_type;
+	static const BaseType addr32_type;
+	static const BaseType cstring_type;
+	
+	// Constructors
+	static const BaseType& getBaseType(base_t type);
+	
+	// Methods
+	inline bool equals(const Type& type) const { return this == &type; };
+	virtual kind_t kind(void) const = 0;
+	virtual const BaseType *toBase(void) const { return 0; };
+	virtual void print(elm::io::Output& output) const;
+	
+	// Operators
+	inline bool operator==(const Type& type) const { return equals(type); }
+	inline bool operator!=(const Type& type) const { return !equals(type); }
+};
+
+
+// NoType class
+class NoType: public Type {
+public:
+	
+	// Type overload
+	virtual kind_t kind(void) const;
+	virtual void print(elm::io::Output& output) const;
 };
 
 
 // BaseType class
 class BaseType: public Type {
-	type_base_t bknd;
-	inline BaseType(type_base_t base_kind): bknd(base_kind) { };
+	friend class Type;
+	base_t bknd;
+	inline BaseType(base_t base_kind): bknd(base_kind) { };
 public:
-
-	// Methods
-	inline type_base_t base(void) const { return bknd; };
-	virtual type_kind_t kind(void) { return TYPE_Base; };
-	virtual BaseType *toBase(void) { return this; };
+	inline base_t base(void) const { return bknd; };
 	
-	// Constant types
-	static BaseType void_type;
-	static BaseType block_type;
-	static BaseType bool_type;
-	static BaseType int8_type;
-	static BaseType uint8_type;
-	static BaseType int16_type;
-	static BaseType uint16_type;
-	static BaseType int32_type;
-	static BaseType uint32_type;
-	static BaseType int64_type;
-	static BaseType uint64_type;
-	static BaseType float32_type;
-	static BaseType float64_type;
-	static BaseType float128_type;
-	static BaseType addr32_type;
+	// Type overload
+	virtual Type::kind_t kind(void) const { return Type::BASE; };
+	virtual const BaseType *toBase(void) const { return this; };
+	virtual void print(elm::io::Output& output) const;
 };
-
 
 } // namespace otawa
 
--- src/prog/type.cpp
+++ src/prog/type.cpp
@@ -5,26 +5,131 @@
  *	type.h -- type class implementation.
  */
 
+#include <assert.h>
+#include <elm/string.h>
 #include <otawa/type.h>
  
 namespace otawa {
+
+/**
+ * @class NoType
+ * Null value for types.
+ */
+
+
+// Type::kind() overload 
+Type::kind_t NoType::kind(void) const {
+	return NONE;
+}
+
+
+// Type::print() overload
+void NoType::print(elm::io::Output& output) const {
+	output << "notype";
+}
+
 
 /**
  * @class Type
  * This class describes the type of the data in the program.
  */
 
+
 /**
- * @fn type_kind_t Type::kind(void);
+ * @fn Type::kind_t Type::kind(void) const;
  * Get the kind of the type.
  * @return The kind of the type.
  */
 
+
 /**
- * @fn BaseType *Type::toBase(void);
+ * @fn const BaseType *Type::toBase(void) const;
  * If the type is a base type, return it. Else return null.
  * @return Base type or null.
  */
+
+
+// Constant types
+const NoType Type::no_type;
+const BaseType Type::void_type(Type::VOID);
+const BaseType Type::block_type(Type::BLOCK);
+const BaseType Type::bool_type(Type::BOOL);
+const BaseType Type::char_type(Type::CHAR);
+const BaseType Type::int8_type(Type::INT8);
+const BaseType Type::uint8_type(Type::UINT8);
+const BaseType Type::int16_type(Type::INT16);
+const BaseType Type::uint16_type(Type::UINT16);
+const BaseType Type::int32_type(Type::INT32);
+const BaseType Type::uint32_type(Type::UINT32);
+const BaseType Type::int64_type(Type::INT64);
+const BaseType Type::uint64_type(Type::UINT64);
+const BaseType Type::float32_type(Type::FLOAT32);
+const BaseType Type::float64_type(Type::FLOAT64);
+const BaseType Type::float128_type(Type::FLOAT128);
+const BaseType Type::addr32_type(Type::ADDR32);
+const BaseType Type::cstring_type(Type::ADDR32);
+
+// array of base types
+static const BaseType *_base_types[] = {
+	&Type::void_type,
+	&Type::block_type,
+	&Type::bool_type,
+	&Type::char_type,
+	&Type::int8_type,
+	&Type::uint8_type,
+	&Type::int16_type,
+	&Type::uint16_type,
+	&Type::int32_type,
+	&Type::uint32_type,
+	&Type::int64_type,
+	&Type::uint64_type,
+	&Type::float32_type,
+	&Type::float64_type,
+	&Type::float128_type,
+	&Type::addr32_type,
+	&Type::cstring_type
+};
+
+// array of base types displays
+elm::CString _base_displays[] = {
+	"void",
+	"block",
+	"bool",
+	"char",
+	"int8",
+	"uint8",
+	"int16",
+	"uint16",
+	"int32",
+	"uint32",
+	"int64",
+	"uint64",
+	"float32",
+	"float64",
+	"float128",
+	"addr32",
+	"cstring"
+};
+
+
+/**
+ * Get a base type from its base type value.
+ * @param type	Base type.
+ * @return		Matching base type value.
+ */
+const BaseType& Type::getBaseType(base_t type) {
+	assert(type >= 0 && type < BASE_TOP);
+	return *_base_types[type];
+};
+
+
+/**
+ * Print a text representation of the type.
+ * @param output	Output to display to.
+ */
+void Type::print(elm::io::Output& output) const {
+	output << "<not displayable type>";
+}
 
 
 /**
@@ -33,86 +138,17 @@ namespace otawa {
  * base kind value defining the base type.
  */
 
+
 /**
- * @fn type_base_t BaseType::base(void) const;
+ * @fn Type::base_t BaseType::base(void) const;
  * Get the base type kind.
  * @return Base type kind.
  */
 
-/**
- * Void base type unique representation.
- */
-BaseType BaseType::void_type(TYPE_Void);
 
-/**
- * Bit block base type unique representation.
- */
-BaseType BaseType::block_type(TYPE_Block);
-
-/**
- * Boolean base type unique representation.
- */
-BaseType BaseType::bool_type(TYPE_Bool);
-
-/**
- * Signed 8-bits integer base type unique representation.
- */
-BaseType BaseType::int8_type(TYPE_Int8);
-
-/**
- * Unsigned 8-bits integer base type unique representation.
- */
-BaseType BaseType::uint8_type(TYPE_UInt8);
-
-/**
- * Signed 16-bits integer base type unique representation.
- */
-BaseType BaseType::int16_type(TYPE_Int16);
-
-/**
- * Unsigned 16-bits integer base type unique representation.
- */
-BaseType BaseType::uint16_type(TYPE_UInt16);
-
-/**
- * Signed 32-bits integer base type unique representation.
- */
-BaseType BaseType::int32_type(TYPE_Int32);
-
-/**
- * Unsigned 32-bits integer base type unique representation.
- */
-BaseType BaseType::uint32_type(TYPE_UInt32);
-
-/**
- * Signed 64-bits integerbase type unique representation.
- */
-BaseType BaseType::int64_type(TYPE_Int64);
-
-/**
- * Unsigned 64-bits integer base type unique representation.
- */
-BaseType BaseType::uint64_type(TYPE_UInt64);
-
-/**
- * 32-bits float base type unique representation.
- */
-BaseType BaseType::float32_type(TYPE_Float32);
-
-/**
- * 64-bits float base type unique representation.
- */
-BaseType BaseType::float64_type(TYPE_Float64);
-
-/**
- * 128-bits floats base type unique representation.
- */
-BaseType BaseType::float128_type(TYPE_Float128);
-
-/**
- * 32-bits address base type unique representation.
- */
-BaseType BaseType::addr32_type(TYPE_Addr32);
-
+// Type overload
+void BaseType::print(elm::io::Output& output) const {
+	output << _base_displays[bknd];
+}
 
 }	// namespace otawa
