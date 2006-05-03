b6316269e44855a71b849dbc249fd5a8af075958
casse <devnull@localhost>
2006-05-03 07:42:50 +0000

    Added support for new register system.

--- src/gliss/Inst.cpp
+++ src/gliss/Inst.cpp
@@ -18,7 +18,17 @@ namespace otawa { namespace gliss {
  * Representation of instructions in Gliss.
  */
 
-	
+
+/**
+ */
+Inst::~Inst(void) {
+	if(reads && reads != &no_regs)
+		delete (elm::genstruct::AllocatedTable<hard::Register *> *)reads;
+	if(writes && writes != &no_regs)
+		delete (elm::genstruct::AllocatedTable<hard::Register *> *)writes;
+}
+
+
 // Overloaded
 address_t Inst::address(void) {
 	return addr;
@@ -29,24 +39,6 @@ address_t Inst::address(void) {
 size_t Inst::size(void) {
 	return 4;
 }
-
-
-// Overloaded
-/*Collection<Operand *> *Inst::getOps(void) {
-	return 0;	// !!TODO!!
-}*/
-
-
-// Overloaded
-/*Collection<Operand *> *Inst::getReadOps(void) {
-	return 0;	// !!TODO!!
-}*/
-
-
-// Overloaded
-/*Collection<Operand *> *Inst::getWrittenOps(void) {
-	return 0;	// !!TODO!!
-}*/
 
 
 // Overloaded
@@ -91,5 +83,67 @@ void Inst::scan(void) {
 	flags |= FLAG_Built;
 }
 
+
+// Overload
+const elm::genstruct::Table<hard::Register *>& Inst::readRegs(void) {
+	if(!reads)
+		scanRegs();
+	return *reads;
+}
+
+
+// Overload
+const elm::genstruct::Table<hard::Register *>& Inst::writtenRegs(void) {
+	if(!writes)
+		scanRegs();
+	return *writes;
+}
+
+
+/**
+ * Scan the register used by the statement.
+ */
+void Inst::scanRegs(void) {
+
+	// Decode instruction
+	code_t buffer[20];
+	instruction_t *inst;
+	iss_fetch((::address_t)(unsigned long)addr, buffer);
+	inst = iss_decode((::address_t)(unsigned long)addr, buffer);
+	
+	// Get read registers
+	int cnt = 0;
+	for(int i = 0; inst->instrinput[i].type != VOID_T; i++)
+		if(inst->instrinput[i].type == GPR_T
+		|| inst->instrinput[i].type == FPR_T)
+			cnt++;
+	elm::genstruct::AllocatedTable<hard::Register *> *tab =
+		new elm::genstruct::AllocatedTable<hard::Register *>(cnt);
+	cnt = 0;
+	for(int i = 0; inst->instrinput[i].type != VOID_T; i++)
+		if(inst->instrinput[i].type == GPR_T)
+			tab->set(cnt++, Platform::GPR_bank[inst->instrinput[i].val.Uint5]);
+		else if(inst->instrinput[i].type == FPR_T)
+			tab->set(cnt++, Platform::FPR_bank[inst->instrinput[i].val.Uint5]);
+	reads = tab;
+
+	// Get write registers
+	cnt = 0;
+	for(int i = 0; inst->instroutput[i].type != VOID_T; i++)
+		if(inst->instroutput[i].type == GPR_T
+		|| inst->instroutput[i].type == FPR_T)
+			cnt++;
+	tab = new elm::genstruct::AllocatedTable<hard::Register *>(cnt);
+	cnt = 0;
+	for(int i = 0; inst->instroutput[i].type != VOID_T; i++)
+		if(inst->instroutput[i].type == GPR_T)
+			tab->set(cnt++, Platform::GPR_bank[inst->instroutput[i].val.Uint5]);
+		else if(inst->instroutput[i].type == FPR_T)
+			tab->set(cnt++, Platform::FPR_bank[inst->instroutput[i].val.Uint5]);
+	writes = tab;
+	
+	// Free instruction
+	iss_free(inst);
+}
 
 } } // otawa::gliss
