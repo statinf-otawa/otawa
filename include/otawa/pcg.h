f0fcb0f8c6e95cd020c7ec297f564393ebbb5cac
nemer <devnull@localhost>
2006-06-22 13:39:56 +0000

    first version of pcg.h

---
+++ include/otawa/pcg.h
@@ -0,0 +1,14 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT UPS.
+ *
+ *	otawa/pcg.h -- PCG module interface.
+ */
+#ifndef OTAWA_PCG_H
+#define OTAWA_PCG_H
+
+#include <otawa/pcg/PCGBlock.h>
+#include <otawa/pcg/PCGBuilder.h>
+#include <otawa/pcg/PCG.h>
+
+#endif	// OTAWA_PCG_H
