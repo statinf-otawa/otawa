5b91128ed31ab1ddc98496aba7402bc9829bdf39
rochange <devnull@localhost>
2006-08-11 15:20:00 +0000

    First version.

---
+++ test/simulator/ARMSimulator.h
@@ -0,0 +1,161 @@
+#ifndef _ARMSIMULATOR_H_
+#define _ARMSIMULATOR_H_
+
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT-UPS <casse@irit.fr>.
+ *
+ *	otawa/sim/TrivialSimulator.h -- TrivialSimulator class interface.
+ */
+
+#include <ARMProcessor.h>
+#include <otawa/sim/Simulator.h>
+#include <otawa/sim/State.h>
+#include <otawa/otawa.h>
+
+
+namespace otawa { 
+
+// Configuration
+extern GenericIdentifier<int> INSTRUCTION_TIME;
+
+class Driver {
+	public :
+		virtual ~Driver(); 
+		virtual Inst* nextInstruction()=0;
+		virtual Inst* nextInstruction(bool branch_is_taken)=0;
+		virtual void terminateInstruction(Inst* inst)=0;
+		virtual sim::mode_t simMode()=0;
+};
+class DriverUntilAddr : public Driver {
+	address_t end_addr;
+	Inst *next;
+	sim::mode_t sim_mode;
+	public:
+		inline DriverUntilAddr(Inst* start_inst, address_t addr);
+		Inst *nextInstruction();
+		Inst* nextInstruction(bool branch_is_taken);
+		void terminateInstruction(Inst* inst);
+		sim::mode_t simMode();
+};
+inline DriverUntilAddr::DriverUntilAddr(Inst* start_inst, address_t addr): 
+	next(start_inst), end_addr(addr), sim_mode(sim::NORMAL) {
+}
+class DriverUntilEnd : public Driver {
+	Inst *next;
+	sim::mode_t sim_mode;
+	public:
+		inline DriverUntilEnd(Inst* start_inst);
+		Inst *nextInstruction();
+		Inst* nextInstruction(bool branch_is_taken);
+		void terminateInstruction(Inst* inst);
+		sim::mode_t simMode();
+};
+inline DriverUntilEnd::DriverUntilEnd(Inst* start_inst): 
+	next(start_inst), sim_mode(sim::NORMAL) {
+}
+
+
+class ARMState: public sim::State {
+	FrameWork *fw;
+	int time;
+	int _cycle;
+	Inst* next;
+	ARMProcessor* processor;	
+	sim::mode_t sim_mode;
+	
+public:
+	Driver *driver;
+
+	ARMState(FrameWork *framework, int _time):
+	fw(framework), time(_time), _cycle(0), driver(NULL) {
+		next = fw->start();
+	}
+	
+	ARMState(const ARMState& state):
+	fw(state.fw), time(state.time), _cycle(state._cycle), next(state.next), driver(NULL) {
+	}
+	
+	void init();
+	
+	// State overload
+	State *clone(void) {
+		return new ARMState(*this);	
+	}
+	
+	
+	sim::mode_t step(void);
+	
+	 sim::mode_t run(void) {
+	 	driver = new DriverUntilEnd(next);
+		while(driver->simMode() == sim::NORMAL)
+			// mode = step(); ------------------------- pas besoin de retourner un mode ?
+			step();
+		delete driver;
+		return sim::NORMAL; //------------------------- pas besoin de retourner un mode ?
+	}
+	
+	 sim::mode_t runUntil(address_t addr) {
+	 	driver = new DriverUntilAddr(next,addr);
+		while(driver->simMode() == sim::NORMAL)
+			// mode = step(); ------------------------- pas besoin de retourner un mode ?
+			step();
+		delete driver;
+		return sim::NORMAL; //------------------------- pas besoin de retourner un mode ?
+	}
+
+	sim::mode_t runUntil(Inst *inst) {
+		assert(inst);
+		return runUntil(inst->address());
+	}
+
+	sim::mode_t runUntilBranch(void) {
+	}
+	
+	sim::mode_t flush(void) {
+		return sim::NORMAL;
+	}
+	
+	int cycle(void) {
+		return _cycle;
+	}
+	
+	void reset(void) {
+		_cycle = 0;
+	}
+	
+	address_t getPC(void) {
+		assert(next);
+		return next->address();
+	}
+
+	Inst *pcInst(void) {
+		return next;
+	}
+
+	void setPC(address_t pc);
+	
+	void setPC(Inst *inst) {
+		assert(inst);
+		next = inst;
+	}
+	
+};
+
+// ARMSimulator class
+class ARMSimulator: public sim::Simulator {
+public:
+	ARMSimulator(void);
+	
+	// Simulator overload
+	ARMState *instantiate(FrameWork *fw,
+		const PropList& props = PropList::EMPTY);
+};
+
+
+
+
+} // otawa
+
+
+#endif //_ARMSIMULATOR_H_
---
+++ test/simulator/simulator.cpp
@@ -0,0 +1,124 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2003, IRIT UPS.
+ *
+ *	test/ipet/exegraph.cpp -- test for Execution Graph modeling feature.
+ */
+
+#include <systemc>
+#include <stdlib.h>
+#include <elm/io.h>
+#include <otawa/otawa.h>
+#include <otawa/ipet.h>
+#include <otawa/proc/ProcessorException.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/ilp.h>
+#include <ARMSimulator.h>
+
+using namespace elm;
+using namespace elm::io;
+using namespace otawa;
+using namespace otawa::ipet;
+using namespace std;
+using namespace sc_core;
+using namespace sc_dt;
+
+// void sc_main
+int sc_main(int argc, char *argv[]) {
+	return 0;
+}
+
+
+
+int main(int argc, char **argv) {
+	
+	elm::io::OutFileStream logStream("/home/rochange/ECLIPSE/otawa/otawa/test/simulator/log");
+	elm::io::Output logFile(logStream);
+	
+	sc_core::sc_elab_and_sim(argc, argv);
+	
+	
+	Manager manager;
+	PropList props;
+	props.set<Loader *>(Loader::ID_Loader, &Loader::LOADER_Gliss_PowerPC);
+	
+	
+	try {
+		
+		// Load program
+		if(argc < 2) {
+			elm::cerr << "ERROR: no argument.\n"
+				 << "Syntax is : exegraph <executable>\n";
+			exit(2);
+		}
+		FrameWork *fw = manager.load(argv[1], props);
+		
+		
+		// Find main CFG
+		elm::cout << "Looking for the main CFG\n";
+		CFG *cfg = fw->getCFGInfo()->findCFG("main");
+		if(cfg == 0) {
+			elm::cerr << "ERROR: cannot find main !\n";
+			return 1;
+		}
+		else
+			elm::cout << "main found at 0x" << cfg->address() << '\n';
+		
+		// Removing __eabi call if available
+		for(CFG::BBIterator bb(cfg); bb; bb++)
+			for(BasicBlock::OutIterator edge(bb); edge; edge++)
+				if(edge->kind() == otawa::Edge::CALL
+				&& edge->target()
+				&& edge->calledCFG()->label() == "__eabi") {
+					delete(*edge);
+					break;
+				}
+		
+		// Now, use a VCFG
+		VirtualCFG vcfg(cfg);
+		
+		// Prepare processor configuration
+		PropList props;
+		props.set(IPET::ID_Explicit, true);
+		
+		ARMSimulator simulator;
+		ARMState *simulator_state = (ARMState *) simulator.instantiate(fw,props);
+		simulator_state->init();
+		
+		// Compute BB times
+		//elm::cout << "Timing the BB\n";
+		//TrivialBBTime tbt(5, props);
+		//tbt.processCFG(fw, &vcfg);
+		
+		// Compute BB times with execution graphs
+		logFile << "\n----------------------------------------------\n";
+		logFile << "Timing the BB with execution graphs\n";
+		logFile << "----------------------------------------------\n";
+		logFile << "CFG: \n";
+		for(CFG::BBIterator bb(&vcfg); bb; bb++) {
+			if (bb->countInstructions() != 0) {
+				logFile << "block b" << bb->number() << ":";
+				int start_cycle = simulator_state->cycle();
+				simulator_state->setPC(bb->address());
+				simulator_state->runUntil(bb->address() + 4*bb->size());
+				int finish_cycle = simulator_state->cycle();
+				logFile << finish_cycle - start_cycle << " cycles\n";
+			}
+		}
+		
+//		ExeGraphBBTime tbt(props, &processor, logFile);
+//		tbt.processCFG(fw, &vcfg);
+		
+	}
+	catch(LoadException e) {
+//		elm::cerr << "ERROR: " << e.message() << '\n';
+		exit(1);
+	}
+	catch(ProcessorException e) {
+//		elm::cerr << "ERROR: " << e.message() << '\n';
+		exit(1);
+	}
+	return 0;
+
+}
+
