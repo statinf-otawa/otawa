2b70818f5fa3b5724ca5cb4d5cecfadf8c2121f0
cavai <devnull@localhost>
2006-08-21 14:17:29 +0000

    added some parameters to the program test_delta, and a verbose error message

--- test/delta/test_delta.cpp
+++ test/delta/test_delta.cpp
@@ -28,13 +28,21 @@ using namespace otawa::hard;
 // Option string
 CString ccg_option = "-ccg";
 CString cat_option = "-cat";
+CString dump_option = "-dump";
+CString infos_option = "-infos";
 
 /**
  * Display help message and exit the program.
  */
 void help(void) {
 	cerr << "ERROR: bad arguments.\n";
-	cerr << "SYNTAX: test_delta [-ccg|-cat] program\n";
+	cerr << "SYNTAX: test_delta [-ccg|-cat] [-dump] program\n";
+	cerr << "    -dump dumps to the standard output the ILP system\n";
+	cerr << "    -D#   forces the depth of the delta algorithm\n";
+	cerr << "    -ccg  uses ccg method\n";
+	cerr << "    -cat  uses categorisation methos\n";
+	cerr << "  If neither -ccg nor -cat is specified, the program does NOT perform\n";
+	cerr << "  the Instruction Cache Processor\n";
 	exit(1);
 }
 
@@ -50,9 +58,11 @@ int main(int argc, char **argv) {
 		NONE = 0,
 		CCG,
 		CAT
-	} method = CCG;
+	} method = NONE; //CCG;
 	
-	int deltaLevels = 5;
+	elm::Option<int> deltaLevels;
+	bool dump = false;
+	bool infos = false;
 
 	// Cache configuration	
 	Cache::info_t inst_cache_info = {
@@ -87,8 +97,15 @@ int main(int argc, char **argv) {
 			method = CCG;
 		else if(cat_option == argv[i])
 			method = CAT;
-		else if(argv[i][0] == '-' && argv[i][1] == 'D')
-			sscanf(&argv[i][2],"%d",&deltaLevels);
+		else if(dump_option == argv[i])
+			dump = true;
+		else if(infos_option == argv[i])
+			infos = true;
+		else if(argv[i][0] == '-' && argv[i][1] == 'D'){
+			int d;
+			sscanf(&argv[i][2],"%d",&d);
+			deltaLevels = d;
+		}
 		else
 			help();
 	}
@@ -173,26 +190,34 @@ int main(int argc, char **argv) {
 				CATConstraintBuilder decomp;
 				decomp.processCFG(fw, &vcfg);
 			}
-
+			
 			// Build the object function to maximize
 			BasicObjectFunctionBuilder fun_builder;
 			fun_builder.processCFG(fw, &vcfg);	
+
 		}
-		
+
 		// Load flow facts
 		ipet::FlowFactLoader loader(props);
 		loader.processCFG(fw, &vcfg);
 		
 		// Calculate deltas
-		cout << "Computing deltas... ";
+		//cout << "Computing deltas... ";
 		elm::system::StopWatch delta_sw;
 		delta_sw.start();
 		//props.set<int>(Delta::ID_Levels,deltaLevels);
-		Delta::ID_Levels(props) = deltaLevels;
+		if(deltaLevels)
+			Delta::ID_Levels(props) = *deltaLevels;
 		Delta delta(props);
 		delta.processCFG(fw, &vcfg);
 		delta_sw.stop();
-		cout << "OK in " << delta_sw.delay()/1000 << " ms\n";
+		//cout << "OK in " << delta_sw.delay()/1000 << " ms\n";
+		if(infos){
+			cout << "CFG have " << vcfg.bbs().count() << " nodes\n";
+			cout << BBPath::instructions_simulated << " instructions have been simulated\n";
+			cout << "The longer path have " << delta.max_length << " basic blocks\n";
+			cout << "The average length of paths is " << delta.mean_length << '\n';
+		}
 		
 		// Resolve the system
 		elm::system::StopWatch ilp_sw;
@@ -211,6 +236,8 @@ int main(int argc, char **argv) {
 		
 		// Get the result
 		ilp::System *sys = vcfg.use<ilp::System *>(SYSTEM);
+		if(dump)
+			sys->dump();
 		cout << file << '\t'
 			 << sys->countVars() << '\t'
 			 << sys->countConstraints() << '\t'
