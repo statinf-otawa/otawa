5542d3c38c483a00038ba5d668c7e844cd95283a
rochange <devnull@localhost>
2006-08-22 07:56:11 +0000

    *** empty log message ***

--- test/generic_simulator/Execute.cpp
+++ test/generic_simulator/Execute.cpp
@@ -7,12 +7,14 @@ ExecuteInOrderStageIQ::ExecuteInOrderStageIQ(sc_module_name name, int width, ota
 	in_instruction = new sc_in<SimulatedInstruction *>[width];
 	
 	SC_METHOD(action);
-	sensitive_neg << in_clock;
+	sensitive_pos << in_clock;
 }
 
 void ExecuteInOrderStageIQ::action() {
 	SimulatedInstruction* inst;
 	int issued = 0;
+	elm::cout << "ExecuteStage->action()\n";
+	elm::cout << "\tin_number_of_ins=" << in_number_of_ins.read() << "\n";
 	for (int i=0 ; i<in_number_of_ins.read() ; i++) {
 		inst = in_instruction[i].read() ;
 		issued++;
@@ -20,6 +22,6 @@ void ExecuteInOrderStageIQ::action() {
 //		iss_free(inst->emulatedInst());
 		delete inst;
 	}
-	
+	elm::cout << "\tout_number_of_accepted_ins=" << stage_width << "\n";
 	out_number_of_accepted_ins.write(stage_width);	
 }
