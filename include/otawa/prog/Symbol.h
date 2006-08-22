83aa4c2da9a1e6aecec0dc28822d585c0327af9f
casse <devnull@localhost>
2006-08-22 15:48:25 +0000

    symbol_kind_t moved in Symbol and renamed kind_t. SYMBOL_xxx renamed to XXX.

--- include/otawa/prog/Symbol.h
+++ include/otawa/prog/Symbol.h
@@ -1,35 +1,40 @@
 /*
  *	$Id$
- *	Copyright (c) 2003, IRIT UPS.
+ *	Copyright (c) 2003-06, IRIT UPS.
  *
- *	otawa/Symbol.h -- Symbol class interface.
+ *	otawa/prog/Symbol.h -- Symbol class interface.
  */
 #ifndef OTAWA_PROG_SYMBOL_H
 #define OTAWA_PROG_SYMBOL_H
 
-#include <elm/string.h>
-#include <otawa/base.h>
+#include <elm/io.h>
+#include <otawa/properties.h>
 
 namespace otawa {
 
 // Symbol kind
-typedef enum symbol_kind_t {
-	SYMBOL_None,
-	SYMBOL_Function,
-	SYMBOL_Label
-} symbol_kind_t;
 	
 // Symbol class
 class Inst;
 class Symbol {
 public:
-	virtual ~Symbol(void) { };
-	virtual symbol_kind_t kind(void) = 0;
+	typedef enum kind_t {
+		NONE,
+		FUNCTION,
+		LABEL
+	} kind_t;
+
+	virtual ~Symbol(void);
+	virtual kind_t kind(void) = 0;
 	virtual elm::String name(void) = 0;
 	virtual address_t address(void) = 0;
 	virtual size_t size(void) = 0;
 	virtual Inst *findInst(void) = 0;
 };
+
+// GenericIdentifier<Symbol_t *>::print Specialization
+template <>
+void GenericIdentifier<Symbol *>::print(elm::io::Output& output, const Property& prop) const;
 
 } // otawa
 
