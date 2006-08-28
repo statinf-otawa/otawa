955d2e0223ae2692c0637fd893efab81419682ea
rochange <devnull@localhost>
2006-08-28 13:47:55 +0000

    Added OOO execution Used TRACE macro

--- include/otawa/gensim/Execute.h
+++ include/otawa/gensim/Execute.h
@@ -27,11 +27,51 @@ class ExecuteInOrderStageIQ : public PipelineStage {
 	public:
 		ExecuteInOrderStageIQ(sc_module_name name, int width, otawa::GenericState * gen_state,
 								elm::genstruct::AllocatedTable<rename_table_t> * _rename_tables);
-		bool isEmpty();
 		
 		SC_HAS_PROCESS(ExecuteInOrderStageIQ);
 		void action();
 };
+
+class ExecuteOOOStage : public PipelineStage {
+	public:
+		// signals
+		sc_in<bool> in_clock;
+		
+	private:
+		// variables
+		int stage_width;
+		elm::genstruct::AllocatedTable<rename_table_t> * rename_tables;
+		InstructionQueue * rob;
+		
+	public:
+		ExecuteOOOStage(sc_module_name name, int width, InstructionQueue * _rob,
+								elm::genstruct::AllocatedTable<rename_table_t> * _rename_tables);
+		
+		SC_HAS_PROCESS(ExecuteOOOStage);
+		void action();
+};
+
+class CommitStage : public PipelineStage {
+	public:
+		// signals
+		sc_in<bool> in_clock;
+		sc_in<SimulatedInstruction *> * in_instruction;
+		sc_in<int> in_number_of_ins;
+		sc_out<int> out_number_of_accepted_ins;
+		
+	private:
+		// variables
+		int width;
+		otawa::GenericState * sim_state;
+		
+	
+	public:
+		CommitStage(sc_module_name name, int _width, otawa::GenericState * gen_state);
+		
+		SC_HAS_PROCESS(CommitStage);
+		void action();
+};
+
 
 
 #endif //_EXECUTE_H_
