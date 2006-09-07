6652d8cfe1258588c797e676031590a1302702bf
casse <devnull@localhost>
2006-09-07 11:43:02 +0000

    Simple inclusion of all simulator classes.

---
+++ include/otawa/sim.h
@@ -0,0 +1,16 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT UPS.
+ *
+ *	otawa/sim.h -- quick access to sim classes interfaces.
+ */
+#ifndef OTAWA_SIM_H
+#define OTAWA_SIM_H
+
+#include <otawa/sim/Driver.h>
+#include <otawa/sim/Simulator.h>
+#include <otawa/sim/State.h>
+
+#endif	// OTAWA_SIM_H
+
+
