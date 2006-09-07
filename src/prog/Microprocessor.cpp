553885de4d9f0f84f2e9a38da94027ef401842b7
casse <devnull@localhost>
2006-09-07 11:39:17 +0000

    Added MUL and DIV instruction categories.

--- src/prog/Microprocessor.cpp
+++ src/prog/Microprocessor.cpp
@@ -9,18 +9,24 @@
 #include <otawa/hard/Register.h>
 
 namespace otawa { 
+
+static const unsigned long MUL_MASK = Inst::IS_MUL | Inst::IS_INT;
+static const unsigned long DIV_MASK = Inst::IS_DIV | Inst::IS_INT;
 	
 instruction_category_t instCategory(Inst *inst) {
-	if ( inst->isControl())
-		return CONTROL;
-	if (inst->isMem())
+	Inst::kind_t kind = inst->kind();
+	if(kind & Inst::IS_FLOAT)
+		return FALU;
+	else if(kind & Inst::IS_MEM)
 		return MEMORY;
-	const elm::genstruct::Table<hard::Register *>& writes = inst->writtenRegs();
-	for(int i = 0; i < writes.count(); i++) {
-		if (writes[i]->kind() == otawa::hard::Register::FLOAT)
-			return FALU;
-	}
-	return IALU;
+	else if(kind & Inst::IS_CONTROL)
+		return CONTROL;
+	else if((kind & MUL_MASK) == MUL_MASK)
+		return MUL;
+	else if((kind & DIV_MASK) == DIV_MASK)
+		return MUL;
+	else
+		return IALU;
 }
 
 } // otawa
