0068c54d771eaaba16a72d5df40ffc0b5ee5b0f5
casse <devnull@localhost>
2006-09-08 11:53:33 +0000

    Added hard.h.

---
+++ include/otawa/hard.h
@@ -0,0 +1,15 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT - UPS.
+ *
+ *	otawa/hardware.h -- common header for hardware module.
+ */
+#ifndef OTAWA_HARDWARE_H
+#define OTAWA_HARDWARE_H
+
+#include <otawa/hard/Cache.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/hard/Platform.h>
+#include <otawa/hard/Register.h>
+
+#endif	// OTAWA_HARDWARE_H
