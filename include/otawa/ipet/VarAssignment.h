c2c0a79c74afc63eccabdfb23a9c653dc2da7329
casse <devnull@localhost>
2006-09-11 19:16:30 +0000

    Support of new processor automatic facilities.

--- include/otawa/ipet/VarAssignment.h
+++ include/otawa/ipet/VarAssignment.h
@@ -13,19 +13,16 @@ namespace otawa { namespace ipet {
 
 // VarAsignment class
 class VarAssignment: public BBProcessor {
-	bool _explicit;
-	void process(BasicBlock *bb);
+	bool _explicit, _recursive;
 	String makeNodeVar(BasicBlock *bb);
 	String makeEdgeVar(Edge *edge);
 	void init(const PropList& props);
+
+protected:
+	virtual void processBB(FrameWork *fw, CFG *cfg, BasicBlock *bb);
+
 public:
 	VarAssignment(const PropList& props = PropList::EMPTY);
-
-	// BBProcessor overload
-	virtual void processBB(FrameWork *fw, CFG *cfg, BasicBlock *bb);
-	virtual void processCFG(FrameWork *fw, CFG *cfg);
-	
-	// Processor overload
 	virtual void configure(PropList& props);
 };
 
