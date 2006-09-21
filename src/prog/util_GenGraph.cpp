76da0e0afe53edb23a01a3409dd806571ca51efc
casse <devnull@localhost>
2006-09-21 16:29:41 +0000

    Added GenGraph class.

---
+++ src/prog/util_GenGraph.cpp
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ * Copyright (c) 2006 IRIT-UPS
+ * 
+ * otawa/util/GenGraph.h -- GenGraph, GenNode and GenEdge classes implementation.
+ */
+
+#include <otawa/util/GenGraph.h>
+
+namespace otawa {
+
+/**
+ * @class GenGraph<N, E> <otawa/util/GenGraph.h>
+ * GenGraph is inherited from @ref Graph class but enforce the type checking of
+ * the objects of graph: only nodes of type N and edges of type E are
+ * accepted.
+ * @param N	Type of nodes (must inherit from @ref GenGraph<N, E>::Node).
+ * @param E Type of edges (must inherit from @ref GenGraph<N, E>::Edge).
+ */
+
+} // otawa
+
