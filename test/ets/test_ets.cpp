fcbab57477c85c41d86f6a03ec8473ef73bf024e
ballabri <devnull@localhost>
2006-10-05 16:05:52 +0000

    Removed all the hardcoded references to the PPC loader plugin. The PPC loader plugin is now dynamically loaded by the plugin system.

--- test/ets/test_ets.cpp
+++ test/ets/test_ets.cpp
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
 	PropList props;
 	FrameWork *fw;
 	CACHE_CONFIG(props) = caches;
-	LOADER(props) = &Loader::LOADER_Gliss_PowerPC;
+//	LOADER(props) = &Loader::LOADER_Gliss_PowerPC;
 	
 	try { 
 		fw = manager.load(argv[1], props);
