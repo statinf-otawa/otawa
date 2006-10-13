2201fb95b63ab556cac91c91d595c76318229029
casse <devnull@localhost>
2006-10-13 15:48:19 +0000

    Added support for generic processors.

--- examples/piconsens/exp.sh
+++ examples/piconsens/exp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
 file="$1.res"
-opts="$2"
-
-echo -e "OPTIONS: $2\n" > $file
-
+echo -e "	aucune	-D 4	-E" > $file
 for d in ~casse/Benchs/snu-rt/*; do
 	if [ -d "$d" ]; then
-		echo "PROCESSING $d" >> $file
-		(./piconsens $opts $d/${d##*/}) >> $file 2>> $file
+		echo -n "${d##*/}	" >> $file
+		(./piconsens -p deg$1.xml $d/${d##*/}) >> $file 2>> $file
+		echo -n "	" >> $file
+		(./piconsens -p deg$1.xml -D 4 $d/${d##*/}) >> $file 2>> $file
+		echo -n "	" >> $file
+		(./piconsens -p deg$1.xml -E $d/${d##*/}) >> $file 2>> $file
 		echo >> $file
 	fi
 done
