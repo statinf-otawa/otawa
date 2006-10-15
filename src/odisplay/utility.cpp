0be3103bcb1723e55d8f5376e929d79cd15b69d8
casse <devnull@localhost>
2006-10-15 23:03:01 +0000

    Adaptation to the new implementation of hash keys.

--- src/odisplay/utility.cpp
+++ src/odisplay/utility.cpp
@@ -3,15 +3,3 @@
 
 using namespace elm;
 using namespace otawa;
-
-static class ConstIdentifierHashKey: public HashKey<const Identifier *> {
-	virtual unsigned long hash(const Identifier *v) {
-		return (unsigned long)v;
-	};
-	virtual bool equals(const Identifier *key1, const Identifier * key2) {
-		return key1 == key2;
-	};
-} constidentifier_hkey_obj;
-
-template <> HashKey<const Identifier *>& HashKey<const Identifier *>::def = constidentifier_hkey_obj;
-
