f90d8460cf5eaf1a70016d1db8636330cd83c9eb
barre <devnull@localhost>
2006-11-06 13:51:28 +0000

    fixed, former version didn't work, now you have to define DEBUG or GENSIM_TRACE to enable debugging trace

--- include/otawa/gensim/debug.h
+++ include/otawa/gensim/debug.h
@@ -2,10 +2,10 @@
 #define _DEBUG_H_
 
 
-#if defined(DEBUG) || !defined(GENSIM_TRACE)
-#	define TRACE(x)
-#else
+#if defined(DEBUG) || defined(GENSIM_TRACE)
 #	define TRACE(x) x
+#else
+#	define TRACE(x)
 #endif
 
 #endif //_DEBUG_H_
