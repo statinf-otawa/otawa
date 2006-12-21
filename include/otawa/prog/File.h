a43226778daa0aeb2931bf38e5c781cbc28aeb44
casse <devnull@localhost>
2006-12-21 10:37:41 +0000

    File moved in its own header File.h and source file prog_File.cpp.

---
+++ include/otawa/prog/File.h
@@ -0,0 +1,70 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT UPS.
+ *
+ *	File class interface
+ */
+#ifndef OTAWA_PROG_FILE_H
+#define OTAWA_PROG_FILE_H
+
+#include <elm/inhstruct/DLList.h>
+#include <elm/Collection.h>
+#include <elm/datastruct/Map.h>
+#include <otawa/prog/CodeItem.h>
+#include <otawa/instruction.h>
+#include <otawa/prog/Symbol.h>
+#include <otawa/prog/Segment.h>
+
+#include <elm/genstruct/Vector.h>
+#include <elm/genstruct/HashTable.h>
+
+namespace otawa {
+
+using namespace elm::genstruct;
+
+// Defined classes
+class ProgItem;
+class CodeItem;
+class Data;
+
+// File class
+class File: public ProgObject {
+	String _name;
+	Vector<Segment *> segs;
+	typedef HashTable<String, Symbol *> syms_t;
+	syms_t syms;
+	
+protected:
+	inline File(String name): _name(name) { }
+	inline void addSegment(Segment *seg) { segs.add(seg); }
+	inline void addSymbol(Symbol *sym) { syms.put(sym->name(), sym); }
+
+public:
+	inline CString name(void) { return _name.toCString(); }
+	
+	// Segment management
+	class SegIter: public Vector<Segment *>::Iterator {
+	public:
+		inline SegIter(const File *file)
+			: Vector<Segment *>::Iterator(file->segs) { }
+		inline SegIter(const SegIter& iter)
+			: Vector<Segment *>::Iterator(iter) { }
+	};
+	
+	// Symbol management
+	address_t findLabel(const String& label);
+	Symbol *findSymbol(String name);
+	class SymIter: public syms_t::ItemIterator {
+	public:
+		inline SymIter(const File *file): syms_t::ItemIterator(file->syms) { }
+		inline SymIter(const SymIter& iter): syms_t::ItemIterator(iter) { }
+	};
+};
+
+// Properties
+extern GenericIdentifier<String> LABEL;
+extern GenericIdentifier<String> FUNCTION_LABEL;
+
+}	// otawa
+
+#endif // OTAWA_PROG_FILE_H
---
+++ include/otawa/prog/Segment.h
@@ -0,0 +1,53 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2006, IRIT UPS.
+ *
+ *	Segment class interface
+ */
+#ifndef OTAWA_PROG_SEGMENT_H
+#define OTAWA_PROG_SEGMENT_H
+
+#include <elm/inhstruct/DLList.h>
+#include <elm/Collection.h>
+#include <elm/datastruct/Map.h>
+#include <otawa/prog/CodeItem.h>
+#include <otawa/instruction.h>
+#include <otawa/prog/Symbol.h>
+
+namespace otawa {
+
+
+// Defined classes
+class ProgObject;
+class ProgItem;
+class CodeItem;
+class Data;
+
+
+// Data class
+class Type;
+class Data: public ProgItem {
+public:
+	virtual Type *getType(void) = 0;
+	virtual Data *toData(void) { return this; };
+};
+
+
+// Segment class
+class Segment: public ProgObject {
+protected:
+	virtual ~Segment(void) { };
+public:
+	const static int EXECUTABLE = 0x01;	/**< Segment is executable. */
+	const static int WRITABLE = 0x02;	/**< Segment is writable. */
+
+	virtual CString name(void) = 0;
+	virtual int flags(void) = 0;
+	virtual address_t address(void) = 0;
+	virtual size_t size(void) = 0;
+	virtual elm::Collection<ProgItem *>& items(void) = 0;
+};
+
+};	// namespace otawa
+
+#endif		// OTAWA_PROG_SEGMENT_H
