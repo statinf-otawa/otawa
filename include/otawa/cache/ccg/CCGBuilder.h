63cfe1de008ac37be12128574de7d836eb0081af
casse <devnull@localhost>
2007-01-12 17:40:42 +0000

    CCG completely packaged with features.

--- include/otawa/cache/ccg/CCGBuilder.h
+++ include/otawa/cache/ccg/CCGBuilder.h
@@ -1,43 +1,68 @@
 /*
  *	$Id$
- *	Copyright (c) 2005, IRIT UPS.
+ *	Copyright (c) 2005-06, IRIT UPS.
  *
- *	otawa/cache/ccg/CCGBuilder.h -- interface of CCGBuilder class.
+ *	CCGBuilder class interface
  */
 #ifndef OTAWA_CACHE_CCGBUILDER_H
 #define OTAWA_CACHE_CCGBUILDER_H
 
 #include <assert.h>
-#include <otawa/proc/CFGProcessor.h>
-#include <otawa/prop/Identifier.h>
+#include <elm/genstruct/Table.h>
+#include <otawa/proc/Processor.h>
+#include <otawa/proc/Feature.h>
+#include <otawa/util/GenGraph.h>
 
 namespace otawa {
-	
+
+using namespace elm::genstruct;
+
 // Extern classes
 class LBlockSet;
 class CFG;
 class LBlockSet;
+class CCGNode;
+class CCGEdge;
+class CCGCollection;
+
+// CCG class
+class CCG: public GenGraph<CCGNode, CCGEdge> {
+public:
+	
+	// Properties
+	static GenericIdentifier<CCGNode *> NODE;
+	static GenericIdentifier<CCGCollection *> GRAPHS;
+};
+
+
+// CCGCollection class
+class CCGCollection {
+	friend class CCGBuilder;
+	AllocatedTable<CCG *> ccgs;
+	inline CCGCollection(int cnt): ccgs(cnt)
+		{ for(int i = 0; i < cnt; i++) ccgs[i] = 0; }
+public:
+	inline ~CCGCollection(void)
+		{ for(int i = 0; i < ccgs.count(); i++) if(ccgs[i]) delete ccgs[i]; }
+	inline int length(void) const { return ccgs.count(); }
+	inline CCG *get(int index) const { return ccgs[index]; }
+	inline CCG *operator[](int index) const { return get(index); }
+};
+
 
 // CCGBuilder class
 class CCGBuilder: public Processor {
-	bool _explicit;
-	int vars;
 	void processLBlockSet(FrameWork *fw, LBlockSet *lbset);
-	void initialize(const PropList& props);
-
 public:
-	static Identifier ID_NonConflict;
-	static Identifier ID_Node;
-	static Identifier ID_HitVar;
-	static Identifier ID_MissVar;
-	static Identifier ID_BBVar;
-
-	CCGBuilder(const PropList& props = PropList::EMPTY);
+	static GenericIdentifier<bool> NON_CONFLICT;
+	CCGBuilder(void);
 
 	// CFGProcessor overload
 	virtual void processFrameWork(FrameWork *fw);
-	virtual void configure(const PropList& props);
 };
+
+// Features
+extern Feature<CCGBuilder> CCG_FEATURE;
 
 }	// otawa
 
