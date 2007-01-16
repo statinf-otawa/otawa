ca882f13364820535644a3fdb7adc01a6d9388db
casse <devnull@localhost>
2007-01-16 12:32:37 +0000

    Adaptation to the feature system.

--- include/otawa/cfg.h
+++ include/otawa/cfg.h
@@ -13,5 +13,7 @@
 #include <otawa/cfg/CFGInfo.h>
 #include <otawa/cfg/VirtualBasicBlock.h>
 #include <otawa/cfg/VirtualCFG.h>
+#include <otawa/cfg.h>
+#include <otawa/proc/CFGProcessor.h>
 
 #endif	// OTAWA_CFG_H
