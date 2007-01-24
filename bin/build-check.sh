412ddddd981662845877169c7e38169c38c8a417
casse <devnull@localhost>
2007-01-24 17:49:17 +0000

    Added --prefix and --suffix options.

--- bin/build-check.sh
+++ bin/build-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# $Id$
+# Copyright (c) 2006-07 IRIT - UPS 
 
 # Defaults
 action=check
@@ -6,11 +8,32 @@ rebuild=no
 cmd=
 list=
 benchdir=../snu-rt
+prefix=
+suffix=
+verbose=
+
+
+# help
+function help {
+	echo "SYNTAX: build-check [options] command benchs..."
+	echo "        build-check [options] benchs... -- command_line"
+	echo "Options:"
+	echo "	--rebuild: rebuild all"
+	echo "	--benchdir[=]PATH: path to the bench directory"
+	echo "	--prefix=PREFIX: prefix to prepend to bench out files"
+	echo "	--suffix=SUFFIX: suffix to append to bench out files"
+	echo "	-h|--help: this display"
+	echo "	-v|--verbose: activate the verbose mode"
+	exit 1
+}
 
 
 # Scan arguments
 while [ -n "$1" ]; do
 	case "$1" in
+	-h|--help)
+		help
+		;;
 	--rebuild)
 		rebuild=yes
 		;;
@@ -21,9 +44,29 @@ while [ -n "$1" ]; do
 	--benchdir=*)
 		benchdir=${1#--benchdir=}
 		;;
+	--prefix=*)
+		benchdir=${1#--prefix=}
+		;;
+	--suffix=*)
+		suffix=${1#--suffix=}
+		;;
+	-v|--verbose)
+		verbose=ok
+		;;
+	--)
+		shift
+		if [ -n "$cmd" ]; then
+			list="$cmd $list"
+		fi
+		cmd=
+		while [ -n "$1" ]; do
+			cmd="$cmd $1"
+			shift
+		done
+		;;
 	-*)
 		echo "ERROR: unknown option $1"
-		exit 1
+		help
 		;;
 	*)
 		if [ -z "$cmd" ]; then
@@ -37,7 +80,7 @@ while [ -n "$1" ]; do
 done
 if [ -z "$cmd" ]; then
 	echo "ERROR: give at least the test program to call !"
-	exit 2
+	help
 fi
 
 
@@ -49,13 +92,17 @@ if [ -z "$list" ]; then
 		fi
 	done
 fi
+if [ x$verbose = xok ]; then
+	echo "benchs = '$list'"
+fi
 
 
 # Perform the work
 for bench in $list; do
-	if [ "$rebuild" = yes -o ! -f $bench.out ]; then
-		echo "$cmd $benchdir/$bench/$bench > $bench.out"
-		$cmd $benchdir/$bench/$bench > $bench.out || exit 2
+	file="$prefix$bench$suffix.out"
+	if [ "$rebuild" = yes -o ! -f "$file" ]; then
+		echo "$cmd $benchdir/$bench/$bench > $file"
+		$cmd $benchdir/$bench/$bench > $file || exit 2
 	fi
 done
 echo "SUCCESS: all is done !"
