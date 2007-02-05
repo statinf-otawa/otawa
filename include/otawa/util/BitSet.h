84f63b4e489b58bfe1bd3c200a69abaa4ce3e770
casse <devnull@localhost>
2007-02-05 12:17:27 +0000

    util::DFAEngine renamed to IterativeDFA. BitSet moved to dfa namespace.

--- include/otawa/util/BitSet.h
+++ include/otawa/util/BitSet.h
@@ -1,240 +1,24 @@
 /*
  *	$Id$
- *	Copyright (c) 2006, IRIT UPS.
+ *	Copyright (c) 2006-07, IRIT UPS.
  *
- *	util/BitSet.h -- BitSet class interface.
+ *	util::BitSet class interface
  */
 #ifndef OTAWA_UTIL_BITSET_H
 #define OTAWA_UTIL_BITSET_H
 
-#include <elm/util/BitVector.h>
+#include <otawa/dfa/BitSet.h>
+
+#warning "Deprecared header and classes. Use otawa/dfa/BitSet.h instead."
 
 namespace otawa {
 
 // BitSet class
-class BitSet {
-	elm::BitVector vec;
-	inline BitSet(const elm::BitVector& ivec);
+class BitSet: public dfa::BitSet {
 public:
-	inline BitSet(int size);
-	inline BitSet(const BitSet& set);
-	
-	inline bool isEmpty(void) const;
-	inline bool isFull(void) const;
-	inline void fill(void);
-	inline void empty(void);
-	
-	inline bool contains(int index) const;
-	inline void add(int index);
-	inline void remove(int index);
-
-	inline bool equals(const BitSet& set) const;
-	inline bool includes(const BitSet& set) const;
-	inline bool includesStrictly(const BitSet& set) const;
-	inline void complement(void);
-	inline void add(const BitSet& set);
-	inline void remove(const BitSet& set);
-	inline void mask(const BitSet& set);
-	inline int count(void) const;
-
-	inline BitSet doComp(void) const;	
-	inline BitSet doUnion(const BitSet& set) const;
-	inline BitSet doInter(const BitSet& set) const;
-	inline BitSet doDiff(const BitSet& set) const;
-	
-	inline BitSet& operator=(const BitSet& set);
-
-	inline BitSet& operator+=(int index);
-	inline BitSet& operator+=(const BitSet& set);
-	inline BitSet& operator-=(int index);
-	inline BitSet& operator-=(const BitSet& set);
-	inline BitSet operator+(const BitSet& set);
-	inline BitSet operator-(const BitSet& set);
-	
-	inline BitSet& operator|=(const BitSet& set);	
-	inline BitSet& operator&=(const BitSet& set);
-	inline BitSet operator|(const BitSet& set);
-	inline BitSet operator&(const BitSet& set);
-	
-	inline bool operator==(const BitSet& set) const;
-	inline bool operator!=(const BitSet& set) const;
-	inline bool operator>=(const BitSet& set) const;
-	inline bool operator<=(const BitSet& set) const;
-	inline bool operator>(const BitSet& set) const;
-	inline bool operator<(const BitSet& set) const;
-	
-	// Iterator class
-	class Iterator: public elm::BitVector::OneIterator {
-	public:
-		inline Iterator(const BitSet& set);
-	};
+	inline BitSet(int size): dfa::BitSet(size) { }
+	inline BitSet(const BitSet& set): dfa::BitSet(set) { }
 };
-
-// BitSet inlines
-inline BitSet::BitSet(const elm::BitVector& ivec): vec(ivec) {
-}
-
-inline BitSet::BitSet(int size): vec(size) {
-}
-
-inline BitSet::BitSet(const BitSet& set): vec(set.vec) {
-}
-	
-inline bool BitSet::isEmpty(void) const {
-	return vec.isEmpty();
-}
-
-inline bool BitSet::isFull(void) const {
-	return vec.countBits() == vec.size();
-}
-
-inline int BitSet::count(void) const {
-	return vec.countBits();
-}
-inline void BitSet::fill(void) {
-	vec.set();
-}
-
-inline void BitSet::empty(void) {
-	vec.clear();
-}
-	
-inline bool BitSet::contains(int index) const {
-	return vec.bit(index);
-}
-
-inline void BitSet::add(int index) {
-	vec.set(index);
-}
-
-inline void BitSet::remove(int index) {
-	vec.clear(index);
-}
-
-inline bool BitSet::equals(const BitSet& set) const {
-	return vec.equals(set.vec);
-}
-
-inline bool BitSet::includes(const BitSet& set) const {
-	return vec.includes(set.vec);
-}
-
-inline bool BitSet::includesStrictly(const BitSet& set) const {
-	return vec.includesStrictly(set.vec);
-}
-
-inline void BitSet::complement(void) {
-	vec.applyNot();
-}
-
-inline void BitSet::add(const BitSet& set) {
-	vec.applyOr(set.vec);
-}
-
-inline void BitSet::remove(const BitSet& set) {
-	vec.applyReset(set.vec);
-}
-
-inline void BitSet::mask(const BitSet& set) {
-	vec.applyAnd(set.vec);
-}
-
-inline BitSet BitSet::doComp(void) const {
-	return BitSet(vec.makeNot());
-}
-
-inline BitSet BitSet::doUnion(const BitSet& set) const {
-	return BitSet(vec.makeOr(set.vec));
-}
-
-inline BitSet BitSet::doInter(const BitSet& set) const {
-	return BitSet(vec.makeAnd(set.vec));
-}
-
-inline BitSet BitSet::doDiff(const BitSet& set) const {
-	return BitSet(vec.makeReset(set.vec));
-}
-
-inline BitSet& BitSet::operator=(const BitSet& set) {
-	vec = set.vec;
-	return *this;
-}
-
-inline BitSet& BitSet::operator+=(int index) {
-	add(index);
-	return *this;
-}
-
-inline BitSet& BitSet::operator+=(const BitSet& set) {
-	add(set);
-	return *this;
-}
-
-inline BitSet& BitSet::operator-=(int index) {
-	remove(index);
-	return *this;
-}
-
-inline BitSet& BitSet::operator-=(const BitSet& set) {
-	remove(set);
-	return *this;
-}
-
-inline BitSet BitSet::operator+(const BitSet& set) {
-	return doUnion(set);
-}
-
-inline BitSet BitSet::operator-(const BitSet& set) {
-	return doDiff(set);
-}
-	
-inline BitSet& BitSet::operator|=(const BitSet& set) {
-	add(set);
-	return *this;
-}
-	
-inline BitSet& BitSet::operator&=(const BitSet& set) {
-	mask(set);
-	return *this;
-}
-
-inline BitSet BitSet::operator|(const BitSet& set) {
-	return doUnion(set);
-}
-
-inline BitSet BitSet::operator&(const BitSet& set) {
-	return doInter(set);
-}
-	
-inline bool BitSet::operator==(const BitSet& set) const {
-	return equals(set);
-}
-
-inline bool BitSet::operator!=(const BitSet& set) const {
-	return !equals(set);
-}
-
-inline bool BitSet::operator>=(const BitSet& set) const {
-	return includes(set);
-}
-
-inline bool BitSet::operator<=(const BitSet& set) const {
-	return set.includes(*this);
-}
-
-inline bool BitSet::operator>(const BitSet& set) const {
-	return includesStrictly(set);
-}
-
-inline bool BitSet::operator<(const BitSet& set) const {
-	return set.includesStrictly(*this);
-}
-
-
-// BitVector::Iterator inlines
-inline BitSet::Iterator::Iterator(const BitSet& set)
-: elm::BitVector::OneIterator(set.vec) {
-}
 
 } // otawa
 
--- include/otawa/util/DFAEngine.h
+++ include/otawa/util/DFAEngine.h
@@ -1,171 +1,39 @@
 /*
  * $Id$
- * Copyright (c) 2005 IRIT-UPS
+ * Copyright (c) 2005-07 IRIT-UPS
  *
- * include/otawa/util/DFAEngine.h -- DFAEngine class interface.
+ * DFAEngine class interface
  */
 #ifndef OTAWA_UTIL_DFAENGINE_H
 #define OTAWA_UTIL_DFAENGINE_H
 
-#include <assert.h>
-#include <otawa/cfg.h>
-//#include <otawa/util/DFABitSet.h>
+#include <otawa/dfa/IterativeDFA.h>
+
+#warning "Deprecared header and classes. Use otawa/dfa/IterativeDFA.h instead."
 
 namespace otawa { namespace util {
 
 // DFAPredecessor
-class DFAPredecessor: public PreIterator<DFAPredecessor, BasicBlock *> {
-	BasicBlock::InIterator iter;
-	inline void look(void) {
-		while(iter && iter->kind() == Edge::CALL)
-			iter++;
-	}
+class DFAPredecessor: public dfa::Predecessor {
 public:
-	inline DFAPredecessor(BasicBlock *bb): iter(bb) {
-		look();
-	};
-	inline BasicBlock *item(void) const { return iter.item()->source(); };
-	inline bool ended(void) const { return iter.ended(); };
-	inline void next(void) { iter.next(); look(); };
+	inline DFAPredecessor(BasicBlock *bb): dfa::Predecessor(bb) { }
 };
 
 
 // DFASuccessor
-class DFASuccessor: public PreIterator<DFASuccessor, BasicBlock *> {
-	BasicBlock::OutIterator iter;
-	inline void look(void) {
-		while(iter && iter->kind() == Edge::CALL)
-			iter++;
-	}
+class DFASuccessor: public dfa::Successor {
 public:
-	inline DFASuccessor(BasicBlock *bb): iter(bb) {
-		look();
-	};
-	inline BasicBlock *item(void) const { return iter.item()->target(); };
-	inline bool ended(void) const { return iter.ended(); };
-	inline void next(void) { iter.next(); look(); };
+	inline DFASuccessor(BasicBlock *bb): dfa::Successor(bb) { }
 };
 
 
 // DFAEngine class
 template <class Problem, class Set, class Iter = DFAPredecessor>
-class DFAEngine {
-	Problem& prob;
-	CFG& _cfg;
-	int cnt;
-	Set **ins, **outs, **gens, **kills;
+class DFAEngine: public dfa::IterativeDFA<Problem, Set, Iter> {
 public:
-	inline DFAEngine(Problem& problem, CFG& cfg);
-	inline ~DFAEngine(void);
-	inline void compute(void);
-	inline Set *inSet(BasicBlock *bb);
-	inline Set *outSet(BasicBlock *bb); 
-	inline Set *genSet(BasicBlock *bb);
-	inline Set *killSet(BasicBlock *bb); 
+	inline DFAEngine(Problem& problem, CFG& cfg)
+		: dfa::IterativeDFA<Problem, Set, Iter>(problem, cfg) { }
 };
-
-
-// DFAEngine::DFAEngine inline
-template <class Problem, class Set, class Iter>
-inline DFAEngine<Problem, Set, Iter>::DFAEngine(Problem& problem, CFG& cfg)
-: _cfg(cfg), cnt(cfg.countBB()), prob(problem) {
-	ins = new Set *[cnt];
-	outs = new Set *[cnt];
-	gens = new Set *[cnt];
-	kills = new Set *[cnt];
-	for(elm::Iterator<BasicBlock *> bb(_cfg.bbs()); bb; bb++) {
-		int idx = INDEX(bb);
-		ins[idx] = prob.empty();
-		outs[idx] = prob.empty();
-		gens[idx] = prob.gen(bb);
-		kills[idx] = prob.kill(bb);
-	}
-}
-
-
-// DFAEngine::~DFAEngine() inline
-template <class Problem, class Set, class Iter>
-inline DFAEngine<Problem, Set, Iter>::~DFAEngine(void) {
-	for(int i = 0; i < cnt; i++) {
-		if(ins[i])
-			delete ins[i];
-		if(outs[i])
-			delete outs[i];
-		delete gens[i];
-		delete kills[i];
-	}
-	delete [] ins;
-	delete [] outs;
-	delete [] gens;
-	delete [] kills;
-}
-
-
-// DFAEngine::inSet() inline
-template <class Problem, class Set, class Iter>
-inline Set *DFAEngine<Problem, Set, Iter>::inSet(BasicBlock *bb) {
-	return ins[INDEX(bb)];
-}
-
-
-// DFAEngine::outSet() inline
-template <class Problem, class Set, class Iter>
-inline Set *DFAEngine<Problem, Set, Iter>::outSet(BasicBlock *bb) {
-	return outs[INDEX(bb)];
-}
-
-
-// DFAEngine::genSet() inline
-template <class Problem, class Set, class Iter>
-inline Set *DFAEngine<Problem, Set, Iter>::genSet(BasicBlock *bb) {
-	return gens[INDEX(bb)];
-}
-
-
-// DFAEngine::killSet() inline
-template <class Problem, class Set, class Iter>
-inline Set *DFAEngine<Problem, Set, Iter>::killSet(BasicBlock *bb) {
-	return kills[INDEX(bb)];
-}
-
-
-// DFAEngine::compute() inline
-template <class Problem, class Set, class Iter>
-inline void DFAEngine<Problem, Set, Iter>::compute(void) {
-	bool changed = true;
-	Set *comp = prob.empty(), *ex;
-	while(changed) {
-		changed = false;
-		for(CFG::BBIterator bb(&_cfg); bb; bb++) {
-			int idx = bb->number();
-			assert(idx >= 0);
-			
-			// IN = union OUT of predecessors
-			prob.reset(ins[idx]);
-			for(Iter pred(bb); pred; pred++)
-				if(pred) {
-					BasicBlock *bb_pred = pred;
-					int pred_idx = bb_pred->number();
-					assert(pred_idx >= 0);
-					prob.merge(ins[idx], outs[pred_idx]);
-				}
-			
-			// OUT = IN \ KILL U GEN
-			prob.set(comp, ins[idx]);
-			prob.diff(comp, kills[idx]);
-			prob.add(comp, gens[idx]);
-			
-			// Any modification ?
-			if(!prob.equals(comp, outs[idx])) {
-				ex = outs[idx];
-				outs[idx] = comp;
-				comp = ex;
-				changed = true;
-			}
-			prob.reset(comp);
-		}
-	}
-}
 
 } } // otawa::util
 
