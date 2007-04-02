2b16140868fd10498139ab2984879175b740453a
casse <devnull@localhost>
2007-04-02 12:50:01 +0000

    No more items.

--- include/otawa/prog/ProgItem.h
+++ include/otawa/prog/ProgItem.h
@@ -4,8 +4,8 @@
  *
  *	ProgItem class interface
  */
-#ifndef OTAWA_PROG_PROGITEM_H
-#define OTAWA_PROG_PROGITEM_H
+#ifndef OTAWA_PROG_PROG_ITEM_H
+#define OTAWA_PROG_PROG_ITEM_H
 
 #include <assert.h>
 #include <elm/string.h>
@@ -16,44 +16,27 @@
 namespace otawa {
 
 // Extern classes
-class CodeItem;
 class Inst;
-class DataItem;
 class Segment;
 
 // ProgItem class
-class ProgItem: public PropList, public inhstruct::DLNode {
-public:
-	typedef enum {
-		blank = 0,
-		code,
-		data
-	} kind_t;
+class ProgItem: public PropList, protected inhstruct::DLNode {
 
-	ProgItem(address_t address, size_t size);
-	inline kind_t kind(void) const { return _kind; }
-	inline address_t address(void) { return _address; }
-	inline address_t topAddress(void) const { return _address + _size; }
-	inline size_t size(void) { return _size; }
-	inline bool isBlank(void) { return _kind == blank; }
-	inline bool isCode(void) { return _kind == code; }
-	inline bool isData(void) { return _kind == data; }
-	inline CodeItem *toCode(void)
-		{ if(_kind == code) return (CodeItem *)this; else return 0; }
-	inline DataItem *toData(void)
-		{ if(_kind == data) return (DataItem *)this; else return 0; }
+public:
+	ProgItem *next(void) const;
+	ProgItem *previous(void) const;
+	virtual address_t address(void) const = 0;
+	virtual size_t size(void) const = 0;
+	inline address_t topAddress(void) const { return address() + size(); }
+	virtual Inst *toInst(void);
 
 protected:
-	ProgItem(kind_t kind, address_t address, size_t size);
-	virtual ~ProgItem(void);
-
-private:
 	friend class Segment;
-	kind_t _kind;
-	address_t _address;
-	size_t _size;
+	virtual ~ProgItem(void);
+	inline void insertPseudo(ProgItem *pos)
+		{ pos->insertBefore(this); }
 };
 
 } // otawa
 
-#endif // OTAWA_PROG_PROGITEM_H
+#endif // OTAWA_PROG_PROG_ITEM_H
