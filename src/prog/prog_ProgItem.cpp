4d4d956ce5b37ca44a1437d57342b1414bc78e06
casse <devnull@localhost>
2007-04-02 12:51:01 +0000

    No more items.

--- src/prog/prog_ProgItem.cpp
+++ src/prog/prog_ProgItem.cpp
@@ -10,44 +10,10 @@
 namespace otawa {
 	
 /**
- * @class ProgObject
- * This is the base class of all objects representing program parts.
- * It provides facilities for storing and retrieving bound special properties.
- */
-
-/**
  * @class ProgItem
  * Base class of the components of a program file segment.
  */
 
-
-/**
- * Build a blank progam item.
- * @param address	Program item address.
- * @param size		Program item size.
- */
-ProgItem::ProgItem(address_t address, size_t size)
-:	_kind(blank),
-	_address(address),
-	_size(size)
-{
-	assert(size);
-}
-
-
-/**
- * Build a program item.
- * @param kind		Kind of program item.
- * @param address	Program item address.
- * @param size		Program item size.
- */
-ProgItem::ProgItem(kind_t kind, address_t address, size_t size)
-:	_kind(kind),
-	_address(address),
-	_size(size)
-{
-	assert(size);
-}
 
 /**
  * @fn ProgItem::~ProgItem(void);
@@ -56,14 +22,42 @@ ProgItem::ProgItem(kind_t kind, address_t address, size_t size)
 ProgItem::~ProgItem(void) {
 }
  
+
+/**
+ * Get the next program item.
+ * @return	Next program item, null at the end of the list.
+ */
+ProgItem *ProgItem::next(void) const {
+	ProgItem *result = (ProgItem *)inhstruct::DLNode::next(); 
+	if(result->atEnd())
+		return 0;
+	else
+		return result;
+}
+
+
+/**
+ * Get the previous program item.
+ * @return	Previous program item, null at the begin of the list.
+ */
+ProgItem *ProgItem::previous(void) const {
+	ProgItem *result = (ProgItem *)inhstruct::DLNode::previous(); 
+	if(result->atBegin())
+		return 0;
+	else
+		return result;
+}
+
+
 /**
  * @fn address_t ProgItem::address(void) const;
- * Get the address of the item if some has been assigned.
+ * Get the address of the item .
  * @return Address of the item or address 0 if none is assigned.
  * @note In workstation systems, it is commonly accepted that the address
  * 0 is ever invalid because it is the usual value of NULL in C. It should also
  * work the same for embedded systems.
  */
+ 
  
  /**
   * @fn size_t ProgItem::size(void) const;
@@ -71,22 +65,21 @@ ProgItem::~ProgItem(void) {
   * @return	Size of the item.
   */
 
-/**
- * @fn CodeItem *ProgItem::toCode(void);
- *	Get the code program item if it is, null else.
- *	@return Code program item or null.
- */
 
 /**
- * @fn DataItem *ProgItem::toData(void);
- *	Get the data program item if it is, null else.
- *	@return Data program item or null.
+ * @fn address_t ProgItem::topAddress(void) const;
+ * Compute the address of the item immediately following the current item.
+ * @return	Top address of the item.
  */
 
+
 /**
- * @fn bool ProgItem::isBlank(void);
- * Test if the code item is blank.
- * @return True if it is blank, false else.
+ * Return the instruction matching the current item.
+ * @return	Matching instruction or null if the current program item is not
+ * an instruction.
  */
+Inst *ProgItem::toInst(void) {
+	return 0;
+}
 
 } // otawa
