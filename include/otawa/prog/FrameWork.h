fe54706a5c1fcc94e393f5d7d5e4f75a060bd683
casse <devnull@localhost>
2007-05-03 08:51:36 +0000

    FrameWork class renamed to WorkSpace.

--- include/otawa/prog/FrameWork.h
+++ include/otawa/prog/FrameWork.h
@@ -1,87 +1,18 @@
 /*
  *	$Id$
- *	Copyright (c) 2003-06, IRIT UPS.
+ *	Copyright (c) 2003-07, IRIT UPS.
  *
  *	otawa/prog/FrameWork.h -- interface for FrameWork class.
  */
 #ifndef OTAWA_PROG_FRAMEWORK_H
 #define OTAWA_PROG_FRAMEWORK_H
 
-#include <elm/Collection.h>
-#include <elm/system/Path.h>
-#include <otawa/properties.h>
-#include <otawa/prog/Process.h>
+#warning "FrameWork class is deprecated. Use WorkSpace instead."
 
-namespace elm { namespace xom {
-	class Element;
-} } // elm::xom
+#include <otawa/prog/WorkSpace.h>
 
 namespace otawa {
-
-using namespace elm;
-using namespace elm::genstruct;
-
-// Classes
-class AbstractFeature;
-class AST;
-class ASTInfo;
-class CFG;
-class CFGInfo;
-class File;
-class Inst;
-class Loader;
-class Manager;
-namespace hard {
-	class Platform;
-	class Processor;
-}
-namespace ilp {
-	class System;
-}
-namespace sim {
-	class Simulator;
-}
-
-// FrameWork class
-class FrameWork: public PropList {
-	Process *proc;
-	Vector<const AbstractFeature *> features;
-	
-protected:
-	virtual Property *getDeep(const AbstractIdentifier *id)
-		{ return proc->getProp(id); };
-public:
-	FrameWork(Process *_proc);
-	~FrameWork(void);
-	inline Process *process(void) const { return proc; };
-	
-	// Process overload
-	virtual hard::Platform *platform(void) { return proc->platform(); };
-	virtual Manager *manager(void) { return proc->manager(); };
-	virtual Inst *start(void) { return proc->start(); };
-	virtual Inst *findInstAt(address_t addr) { return proc->findInstAt(addr); };
-	virtual const hard::CacheConfiguration& cache(void);
-	
-	// CFG Management
-	CFGInfo *getCFGInfo(void);
-	CFG *getStartCFG(void);
-	
-	// AST Management
-	ASTInfo *getASTInfo(void);
-	
-	// ILP support
-	ilp::System *newILPSystem(bool max = true);
-
-	// Configuration services
-	elm::xom::Element *config(void);
-	void loadConfig(const elm::system::Path& path);
-	
-	// Feature management
-	void provide(const AbstractFeature& feature);
-	bool isProvided(const AbstractFeature& feature);
-	void remove(const AbstractFeature& feature);
-};
-
+	typedef class WorkSpace FrameWork;
 };	// otawa
 
 #endif	// OTAWA_PROG_FRAMEWORK_H
