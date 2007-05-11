30e3894d072c61b59728440b394ea02b6935f997
casse <devnull@localhost>
2007-05-11 15:29:20 +0000

    Added new_gliss loader kit (GLISS + GEL).

---
+++ include/otawa/loader/gliss.h
@@ -0,0 +1,19 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2007, IRIT UPS.
+ *
+ *	interface to GLISS simulation
+ */
+#ifndef OTAWA_LOADER_GLISS_H
+#define OTAWA_LOADER_GLISS_H
+
+#include <otawa/prop/Identifier.h>
+
+namespace otawa { namespace gliss {
+
+extern Identifier<state_t *> GLISS_STATE;
+
+} } // otawa::gliss
+
+#endif // OTAWA_LOADER_GLISS_H
+
