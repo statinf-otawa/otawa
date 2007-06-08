8c82016499158c3b0734138d15909523af0f8387
ballabri <devnull@localhost>
2007-06-08 16:25:17 +0000

    Added LoopUnroller + Virtualizer processors

---
+++ include/otawa/cfg/Virtualizer.h
@@ -0,0 +1,32 @@
+#ifndef CFG_VIRTUALIZER_H_
+#define CFG_VIRTUALIZER_H_
+
+#include <otawa/proc/Processor.h>
+#include <otawa/proc/Feature.h>
+
+namespace otawa {
+
+
+
+class Virtualizer: public Processor {
+
+	public:
+	Virtualizer(void);
+	virtual void processWorkSpace(WorkSpace*);
+	virtual void configure(const PropList& props);
+	
+	
+	private:	
+	void virtualize(struct call_t*, CFG *cfg, VirtualCFG *vcfg, BasicBlock *entry, BasicBlock *exit);
+	bool isInlined();
+	bool virtual_inlining;
+	CFG *entry;
+};
+
+extern Feature<Virtualizer> VIRTUALIZED_CFG_FEATURE;
+extern Identifier<bool> VIRTUAL_INLINING;
+
+}
+
+
+#endif 
