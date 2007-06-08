a1919110dce130407fbe1eccc5ad4d3a8c986fa8
ballabri <devnull@localhost>
2007-06-08 16:26:18 +0000

    Added FirstLastBuilder (to detect first and last lblocks of a basic block), and LinkedBlocksDetector (which detects non-conflicting l-blocks with category FIRST MISS)

---
+++ src/prog/cache_FirstLastBuilder.cpp
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <elm/io.h>
+#include <elm/genstruct/Vector.h>
+#include <elm/genstruct/Table.h> 
+#include <otawa/cache/LBlockSet.h>
+#include <otawa/util/LBlockBuilder.h>
+#include <otawa/ilp.h>
+#include <otawa/ipet.h>
+#include <otawa/util/Dominance.h>
+
+#include <otawa/cfg.h>
+#include <otawa/util/LoopInfoBuilder.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/hard/Platform.h>
+
+
+#include <otawa/cache/FirstLastBuilder.h>
+
+using namespace otawa;
+using namespace otawa::ilp;
+using namespace otawa::ipet;
+using namespace elm;
+
+
+
+
+namespace otawa {
+
+/**
+ * 
+ * This feature represents the availability of the LAST_LBLOCK and LBLOCK_ISFIRST properties.
+ * 
+ * @par Properties
+ * @li @ref LAST_LBLOCK
+ * @li @ref LBLOCK_ISFIRST
+ */
+Feature<FirstLastBuilder> ICACHE_FIRSTLAST_FEATURE("otawa.cache.firstlast_feature");
+
+ 
+/**
+ * This property gives the ID of the last lblock of a BasicBlock, for each line.
+ *
+ * @par Hooks
+ * @li @ref BasicBlock 
+ */
+Identifier<LBlock**> LAST_LBLOCK("otawa.cache.last_lblock", NULL, otawa::NS);
+ 
+ /**
+ * This property tells if the lblock is the first of its BasicBlock (for its cache line)
+ * This information is useful, because if it's false, then, the lblock is always a miss.
+ *
+ * @par Hooks
+ * @li @ref LBlock 
+ */
+Identifier<bool> LBLOCK_ISFIRST("otawa.cache.lblock_isfirst", false, otawa::NS);
+ 
+/**
+ * @class ACSBuilder
+ *
+ * This processor produces the Abstract Cache States (ACS), for the May and Persistence problems.
+ *
+ * @par Configuration
+ * none
+ *
+ * @par Required features
+ * @li @ref DOMINANCE_FEATURE
+ * @li @ref LOOP_HEADERS_FEATURE
+ * @li @ref LOOP_INFO_FEATURE
+ * @li @ref COLLECTED_LBLOCKS_FEATURE
+ *
+ * @par Provided features
+ * @li @ref ICACHE_FIRSTLAST_FEATURE
+ * 
+ * @par Statistics
+ * none
+ */
+
+FirstLastBuilder::FirstLastBuilder(void) : CFGProcessor("otawa::FirstLastBuilder", Version(1, 0, 0)) {
+	require(DOMINANCE_FEATURE);
+	require(LOOP_HEADERS_FEATURE);
+	require(LOOP_INFO_FEATURE);
+	require(COLLECTED_LBLOCKS_FEATURE);
+	provide(ICACHE_FIRSTLAST_FEATURE);
+}
+
+void FirstLastBuilder::processCFG(WorkSpace *fw, CFG *cfg) {
+	int i;
+	int *max_addr, *min_addr;
+	LBlock **min_lblock, **max_lblock;
+
+		
+	LBlockSet **lbsets = LBLOCKS(fw);
+	const hard::Cache *cache = fw->platform()->cache().instCache();
+		
+	for (CFG::BBIterator bb(cfg); bb; bb++)
+			LAST_LBLOCK(bb) = new LBlock*[cache->lineCount()];
+	
+	max_addr = new int[cache->lineCount()];
+	min_addr = new int[cache->lineCount()];
+	max_lblock = new LBlock*[cache->lineCount()];
+	min_lblock = new LBlock*[cache->lineCount()];
+	
+	/*
+	 * Detects the first/last lblock for each basicblock (for the current cache line)
+	 * This information is used for the ACS creation
+	 */
+	for (CFG::BBIterator bb(cfg); bb; bb++) {
+		for (int line = 0; line < cache->lineCount(); line++) {
+			max_lblock[line] = NULL;
+			min_lblock[line] = NULL;
+		}			
+		if (BB_LBLOCKS(bb) != NULL) { 
+			const genstruct::AllocatedTable<LBlock*> &lblocks = *BB_LBLOCKS(bb);
+		
+			for (i = 0; i < lblocks.count(); i++) { 
+				int line = cache->line(lblocks[i]->address());
+				
+				
+				if ((max_lblock[line] == NULL) || (lblocks[i]->address() > max_addr[line])) {
+					max_addr[line] = lblocks[i]->address();
+					max_lblock[line] = lblocks[i];
+				}
+				if ((min_lblock[line] == NULL) || (lblocks[i]->address() < min_addr[line])) {
+					min_addr[line] = lblocks[i]->address();
+					min_lblock[line] = lblocks[i];
+				}
+			}
+		}
+		for (int line = 0; line < cache->lineCount(); line++) {
+			/* XXX TODO: pas cohérent */
+			LAST_LBLOCK(bb)[line] = max_lblock[line];
+			if (min_lblock[line] != NULL)
+				LBLOCK_ISFIRST(min_lblock[line]) = true;
+		}
+	}
+	
+	delete [] max_addr;	
+	delete [] min_addr;
+	delete [] max_lblock;
+	delete [] min_lblock;	
+}
+
+}
---
+++ src/prog/cache_LinkedBlocksDetector.cpp
@@ -0,0 +1,131 @@
+
+#include <stdio.h>
+#include <elm/io.h>
+#include <otawa/cache/LBlockSet.h>
+#include <otawa/util/LBlockBuilder.h>
+#include <otawa/ilp.h>
+#include <otawa/ipet.h>
+#include <otawa/util/Dominance.h>
+#include <otawa/cfg.h>
+#include <otawa/util/LoopInfoBuilder.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/hard/Platform.h>
+#include <otawa/ipet/TrivialInstCacheManager.h>
+#include <elm/util/Pair.h>
+
+#include <otawa/cache/cat2/ACSBuilder.h>
+#include <otawa/cache/cat2/CAT2Builder.h>
+#include <otawa/cache/cat2/LinkedBlocksDetector.h>
+
+using namespace otawa;
+using namespace otawa::ilp;
+using namespace otawa::ipet;
+
+namespace otawa {
+	
+  
+/**
+ * @class LinkedBlocksDetector
+ *
+ *
+ * @par Configuration
+ * none
+ *
+ * @par Required features
+ * @li @ref ASSIGNED_VARS_FEATURE
+ * @li @ref COLLECTED_LBLOCKS_FEATURE
+ * @li @ref LOOP_INFO_FEATURE
+ * @li @ref ICACHE_CATEGORY_FEATURE
+ *
+ * @par Provided features
+ * 
+ * @par Statistics
+ * none
+ */
+
+
+
+LinkedBlocksDetector::LinkedBlocksDetector(void) : Processor("otawa::LinkedBlocksDetector", Version(1, 0, 0)), _explicit(false) {
+	require(ASSIGNED_VARS_FEATURE);
+	require(ICACHE_CATEGORY_FEATURE);
+	require(LOOP_INFO_FEATURE);
+	require(COLLECTED_LBLOCKS_FEATURE);
+}
+
+void LinkedBlocksDetector::configure(const PropList& props) {
+        Processor::configure(props);
+	_explicit = EXPLICIT(props);
+}
+                
+void LinkedBlocksDetector::processWorkSpace(otawa::WorkSpace *fw) {
+	const hard::Cache *cache = fw->platform()->cache().instCache();
+	ilp::System *system = getSystem(fw, ENTRY_CFG(fw));
+	int penalty = cache->missPenalty();
+	LBlockSet **lbsets = LBLOCKS(fw);
+	
+	
+	for (int i = 0 ; i < cache->lineCount(); i++) {
+		genstruct::Vector<LinkedBlockList*> blockList;
+		int count = lbsets[i]->cacheBlockCount();
+		for (int j = 0; j < count; j++)
+			blockList.add(NULL);
+		for (LBlockSet::Iterator lblock(*lbsets[i]); lblock; lblock++) {
+			if ((lblock->id() == 0) || (lblock->id() == (lbsets[i]->count() - 1)))
+				continue; /* Skip first / last l-blocks */
+			if (CATEGORY(lblock) == FIRST_MISS) {
+				BasicBlock *header = CATEGORY_HEADER(lblock);
+				int cbid = lblock->cacheblock();
+				
+				if (blockList[cbid] == NULL)
+					blockList[cbid] = new LinkedBlockList(order);
+				blockList[cbid]->add(lblock);	
+			}
+		}
+		
+		cout << "[LBD] Linked blocks detected for line: " << i << "\n";
+		for (int j = 0; j < count; j++) {
+			if (blockList[j] != NULL) {
+				cout << "[LBD] Cache block " << j << "\n";
+						
+				Vector<LBlock*> equiv;
+				BasicBlock *old_header = NULL;
+				equiv.clear();
+				for (LinkedBlockList::Iterator iter(*blockList[j]); iter; iter++) {	
+					cout << "[LBD] lblock " << iter->id() << " ( " << iter->bb()->number() << ") w/ header " << CATEGORY_HEADER(*iter)->number() <<  "\n";				
+					/* We want to build another "equiv" set from scratch whenever the firstmiss-header changes */ 
+					BasicBlock *header = CATEGORY_HEADER(*iter);
+					if ((old_header) && (old_header != header)) {
+						recordBlocks(&equiv);
+						equiv.clear();
+					}
+			
+					equiv.add(*iter);
+					old_header = header;
+				}
+				recordBlocks(&equiv);
+			}			
+		} 
+		
+		for (int j = 0; j < blockList.length(); j++) {
+			if (blockList[j] != NULL)
+				delete blockList[j];
+		}
+	}
+}
+
+void LinkedBlocksDetector::recordBlocks(Vector<LBlock*> *equiv) {
+	if (equiv->length() == 1)
+		return;		
+	genstruct::Vector<LBlock*> *copy = new genstruct::Vector<LBlock*>(*equiv);
+	cout << "[LBD] Equiv. LBlocks: ";
+	for (genstruct::Vector<LBlock*>::Iterator lblock(*equiv); lblock; lblock++) {
+		cout << lblock->id() << "(" << lblock->bb()->number() << ") ";
+		assert(CATEGORY(lblock) == FIRST_MISS);
+		LINKED_BLOCKS(lblock) = copy;   		
+	}
+	cout << "\n";
+}
+
+Identifier<genstruct::Vector<LBlock*> *> LINKED_BLOCKS("otawa.cache.linked_blocks", NULL);
+
+}
