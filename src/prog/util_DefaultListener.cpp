b8dabdf4930b8e9261092ca2eaffbcd238e4fb3f
ballabri <devnull@localhost>
2007-06-11 09:07:38 +0000

    Added some Doxygen documentation.

--- src/prog/util_DefaultListener.cpp
+++ src/prog/util_DefaultListener.cpp
@@ -1,6 +1,31 @@
 
 /**
- * @class UnrollingListener
+ * @class DefaultListener
  *
+ * This listener gathers in an array the LUB of the in-states for all analyzed basic blocks.
+ * 
  */
  
+/**
+ * @fn void DefaultListener::blockInterpreted (const DefaultFixPoint< DefaultListener > *fp, BasicBlock *bb, const typename Problem::Domain &in, const typename Problem::Domain &out, CFG *cur_cfg) const
+ * This is called whenever a block is processed. In the DefaultListener, we do the LUB of all the in-states
+ * of each block.
+ * @param fp The FixPoint object
+ * @param bb The Basic Block
+ * @param in In-state
+ * @param out Out-state (not used in this class)
+ * @param cur_cfg Current CFG   
+ */
+ 
+/**
+ * @fn void DefaultListener::fixPointReached (const DefaultFixPoint< DefaultListener > *fp, BasicBlock *bb)
+ * This is called whenever a loop's fixpoint is reached.
+ * @param fp The FixPoint object
+ * @param bb The loop header. 
+ */
+
+/**
+  * Problem & DefaultListener::getProb ()
+  * This function is for getting the Problem associated with this listener.
+  * @return The problem.
+ */
