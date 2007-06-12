bddad24316ea05982693c476463ddc61f13644ea
casse <devnull@localhost>
2007-06-12 22:41:04 +0000

    Identifier names normalized.

--- src/odisplay/display_graphviz.cpp
+++ src/odisplay/display_graphviz.cpp
@@ -15,17 +15,17 @@ namespace otawa { namespace display {
  * Identifier for the dot layout. Must be one of @ref layout_t.
  * Default is LAYOUT_DOT (standard directional graph, from up to down)
  */
-Identifier<int> GRAPHVIZ_LAYOUT("display.graphviz.layout", LAYOUT_DOT);
+Identifier<int> GRAPHVIZ_LAYOUT("otawa::display::graphviz_layout", LAYOUT_DOT);
 /**
  * Identifier for the output. Must be one of @ref output_t.
  * Default is OUTOUT_PS (PostScript format)
  */
-Identifier<int> GRAPHVIZ_OUTPUT("display.graphviz.output", OUTPUT_PS);
+Identifier<int> GRAPHVIZ_OUTPUT("otawa::display::graphviz_output", OUTPUT_PS);
 /**
  * Identifier for the file that will be created.
  * Default is "odisplay.ps"
  */
-Identifier<elm::CString> GRAPHVIZ_FILE("display.graphviz.file", "odisplay.ps");
+Identifier<elm::CString> GRAPHVIZ_FILE("otawa::display::graphviz_file", "odisplay.ps");
 
 
 
