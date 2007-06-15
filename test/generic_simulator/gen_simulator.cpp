5f10baa980c7ea53adaf93470149e75472cf12c2
casse <devnull@localhost>
2007-06-15 13:29:13 +0000

    OTAWA hides completely the GLISS layer by providing simulation facilities: Process::newState() returns a SimState() that contains functions for simulation.

--- test/generic_simulator/gen_simulator.cpp
+++ test/generic_simulator/gen_simulator.cpp
@@ -7,17 +7,10 @@
 
 #include <cstdlib>
 #include <elm/io.h>
-#include <elm/system/Path.h>
 #include <otawa/otawa.h>
 #include <otawa/ipet.h>
-#include <otawa/proc/ProcessorException.h>
-#include <otawa/hard/CacheConfiguration.h>
-#include <otawa/ilp.h>
 #include <otawa/gensim/GenericSimulator.h>
-#include <FullSimulationDriver.h>
-#include <otawa/gensim/debug.h>
-#include <otawa/gliss.h>
-#include <otawa/sim/BasicBlockDriver.h>
+#include "FullSimulationDriver.h"
 
 
 using namespace elm;
@@ -31,13 +24,20 @@ int main(int argc, char **argv, char **envp) {
 	
 	try {
 		
+		// Prepare the log output
 		elm::io::OutFileStream logStream("log.txt");
 		if(!logStream.isReady())
 			throw IOException("cannot open log file !");
 		elm::io::Output logFile(logStream);
 	
-	
-	
+		// Check arguments
+		if(argc < 2) {
+			elm::cerr << "ERROR: no argument.\n"
+				 << "Syntax is : " << argv[0] << " <executable>\n";
+			exit(2);
+		}
+			
+		// Load the file
 		Manager manager;
 		PropList loader_props;
 		char **new_argv = new char *[argc];
@@ -47,53 +47,33 @@ int main(int argc, char **argv, char **envp) {
 		ARGC(loader_props) = argc - 1;
 		ARGV(loader_props) = new_argv;
 		ENVP(loader_props) = envp;
-		
-		// Load program
-		if(argc < 2) {
-			elm::cerr << "ERROR: no argument.\n"
-				 << "Syntax is : " << argv[0] << " <executable>\n";
-			exit(2);
-		}
-		
 		//TODO: move this parameter on command line
 		elm::system::Path xml_processor_file("../../examples/piconsens/deg4.xml");
 		PROCESSOR_PATH(loader_props) = xml_processor_file;
-		FrameWork *fw = manager.load(argv[1], loader_props);
-		
+		WorkSpace *ws = manager.load(argv[1], loader_props);
 		
 		// Find _start CFG
 		elm::cout << "Looking for the _start CFG\n";
-		CFG *cfg = fw->getCFGInfo()->findCFG("_start");
+		CFG *cfg = ws->getCFGInfo()->findCFG("_start");
 		if(cfg == 0) {
 			elm::cerr << "ERROR: cannot find _start !\n";
 			return 1;
 		}
 		else
 			elm::cout << "_start found at 0x" << cfg->address() << '\n';
-
-		::address_t addr_start = cfg->address();
-		
-		// Removing __eabi call if available
-		for(CFG::BBIterator bb(cfg); bb; bb++)
-			for(BasicBlock::OutIterator edge(bb); edge; edge++)
-				if(edge->kind() == otawa::Edge::CALL
-				&& edge->target()
-				&& edge->calledCFG()->label() == "__eabi") {
-					delete(*edge);
-					break;
-				}
+		otawa::address_t addr_start = cfg->address();
 		
 		// Prepare processor configuration
 		PropList props;
-		props.set(EXPLICIT, true);
+		EXPLICIT(props) = true;
 		
 		// instantiate generic simulator
 		gensim::GenericSimulator simulator;
-		sim::State *simulator_state = simulator.instantiate(fw, props);
+		sim::State *simulator_state = simulator.instantiate(ws, props);
 
 		int start_cycle = simulator_state->cycle();
 		// create the driver which will go through all the program to simulate
-		sim::FullSimulationDriver driver(fw, fw->findInstAt(addr_start), otawa::gliss::GLISS_STATE(fw));
+		sim::FullSimulationDriver driver(ws, ws->findInstAt(addr_start));
 		// start and run the simulation until its end
 		simulator_state->run(driver);
 		int finish_cycle = simulator_state->cycle();
