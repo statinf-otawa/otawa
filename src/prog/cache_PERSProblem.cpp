b7ec66ac1f552d83477623173ceba3cf46511c28
ballabri <devnull@localhost>
2007-06-25 13:13:52 +0000

    better bottom handling

--- src/prog/cache_PERSProblem.cpp
+++ src/prog/cache_PERSProblem.cpp
@@ -37,7 +37,7 @@ namespace otawa {
 PERSProblem::PERSProblem(const int _size, LBlockSet *_lbset, WorkSpace *_fw, const hard::Cache *_cache, const int _A) 
 	: callstate(_size, _A), line(lbset->line()), cache(_cache), bot(_size, _A), ent(_size, _A), lbset(_lbset), fw(_fw)
 	{
-		bot.empty();
+		bot.setToBottom();
 		ent.empty();
 #ifdef PERFTEST
 		bot.enterContext();
