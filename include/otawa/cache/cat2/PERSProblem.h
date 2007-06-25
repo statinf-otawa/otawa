086e1aa6914691f22b5e63e39f099574418bb7fa
ballabri <devnull@localhost>
2007-06-25 13:07:11 +0000

    Corrected persistence lub

--- include/otawa/cache/cat2/PERSProblem.h
+++ include/otawa/cache/cat2/PERSProblem.h
@@ -139,12 +139,14 @@ class PERSProblem {
 	
 	class Domain {
 			int A, size;
+			bool isBottom;
 		public:
 
 			inline Domain(const int _size, const int _A)
-			: A (_A), size(_size)
+			: A (_A), size(_size), whole(_size, _A)
 			{
-
+				isBottom = true;
+				whole.empty();
 			}
 
 			inline ~Domain() {
@@ -152,92 +154,160 @@ class PERSProblem {
 					delete data[i];
 			}
 			
-			inline Domain(const Domain &source) : A(source.A), size(source.size) {
+			inline Domain(const Domain &source) : A(source.A), size(source.size), whole(source.whole), isBottom(source.isBottom) {
+				
 				for (int i = 0; i < source.data.length(); i++)
 					data.add(new Item(*source.data[i]));
 			} 
 			
 			inline Domain& operator=(const Domain &src) {
 				empty(); /* XXX !!gruik!! */
+				whole = src.whole;
+				isBottom = src.isBottom;
 				for (int i = 0; i < src.data.length(); i++)
 					data.add(new Item(*src.data[i]));
 			}	
-
+			
 			inline void lub(const Domain &dom) {
+				/*
+				 * Rules for the LUB:
+				 * 1. lub(anything,bottom) == anything
+				 * 2. lub(dom1,dom2) where dom1 and dom2 has the same size, do the LUB of each corresponding item. 
+				 * 3. lub(dom1,dom2) where dom1 has less items than dom2: we discard  items of dom2 (starting from outer-most loop)
+				 * until it has the same size as dom1, then apply rule 2.
+				 */
 				
-				/* X lub bottom == X */
-				if (dom.data.length() == 0)
-					return;
-				if (data.length() == 0) {
+				if (isBottom && !dom.isBottom) {
 					for (int i = 0; i < dom.data.length(); i++)
 						data.add(new Item(*dom.data[i]));
-					return;
+					whole = dom.whole;
+					isBottom = false;
 				}
 				
-				ASSERT(dom.data.length() == data.length());
-				/* otherwise, LUB all the items */
-				for (int i = 0; i < dom.data.length(); i++)
-					data[i]->lub(*dom.data[i]);
-			}
+				
+				if (dom.isBottom)
+					return;
+				
+				int length = (data.length() < dom.data.length()) ? data.length() : dom.data.length();
+				
+				for (int i = data.length() - 1, j = dom.data.length() - 1, k = 0; k < length; i--, j--, k++) {
+					data[i]->lub(*dom.data[j]);
+				}
+				
 			
-			inline bool equals(const Domain &dom) const {				
+				for (int i = 0; i < data.length() - length; i++) {
+					
+					data.remove(0);
+				}
+				
+				whole.lub(dom.whole);
+				
+			}
+
+
+			inline void lub(const Item &item) {
+				/* Special LUB: do the lub of each Item in current domain with
+				 * item passed as parameter. Used for the partial analysis
+				 */
+				 for (int i = 0; i < data.length(); i++)
+				 	data[i]->lub(item);
+				 
+				 whole.lub(item);
+				 
+			}
+
+			
+			inline bool equals(const Domain &dom) const {
+				ASSERT(!isBottom && !dom.isBottom);			
 				for (int i = 0; i < dom.data.length(); i++) {
 					if (!data[i]->equals(*dom.data[i]))
 						return false;
 				}
-				return true;
+				return (whole.equals(dom.whole) && (isBottom == dom.isBottom));
 			}
 			
 			inline void empty() {
 				for (int i = 0; i < data.length(); i++)
 					delete data[i];
-				data.clear();				
+				data.clear();
+				whole.empty();
+				isBottom = false;		
+			}
+			
+			inline void setToBottom() {
+				for (int i = 0; i < data.length(); i++)
+					delete data[i];
+				data.clear();
+				whole.empty();
+				isBottom = true;		
+			}
+			
+			inline Item &getWhole() {
+				return whole;
 			}
 			
 			inline bool contains(const int id, const int index) {
+				ASSERT(!isBottom);
 				return(data[index]->contains(id));
 			}
 			
 			
 			inline void inject(MUSTProblem::Domain *must, const int id) {
+				ASSERT(!isBottom);
 				for (int i = 0; i < data.length(); i++)
-					data[i]->inject(must, id);			
+					data[i]->inject(must, id);		
+				whole.inject(must, id);	
 			}
 			
 			inline bool isWiped(const int id, const int index) {
+				ASSERT(!isBottom);
 				return(data[index]->isWiped(id));
 				
 			}
 			
 			inline int getAge(const int id, const int index) const {
+				ASSERT(!isBottom);
 				return(data[index]->getAge(id));
 			}
 			
 			inline bool isPersistent(const int id, const int index) {
+				ASSERT(!isBottom);
 				return(data[index]->isPersistent(id));
 		
 			}
 			inline void addDamage(const int id, const int index, int damage) {
+				ASSERT(!isBottom);
 				data[index]->addDamage(id, damage);
 			}
 			
 			inline void addDamage(const int id, int damage) {
+				ASSERT(!isBottom);
 				for (int n = 0; n < data.length(); n++)
 					data[n]->addDamage(id, damage);
+				whole.addDamage(id, damage);
 			}
 			
 			inline void refresh(const int id, const int index, int newage) {
+				ASSERT(!isBottom);
 				data[index]->refresh(id, newage);
 			}
 			
 			inline void refresh(const int id, int newage) {
+				ASSERT(!isBottom);
 				for (int n = 0; n < data.length(); n++)
 					data[n]->refresh(id, newage);
+				whole.refresh(id, newage);
 			}
 			
 			inline void print(elm::io::Output &output) const {
 				bool first = true;
-				output << "(";
+				if (isBottom) {
+					output << "BOTTOM";
+					return;
+				}
+				output << "(W=";
+				whole.print(output); 
+				output << ", ";
 				for (int i = 0; i < data.length(); i++) {
 					if (!first)
 						output << "|";
@@ -248,6 +318,7 @@ class PERSProblem {
 			}
 			
 			inline void enterContext() {
+				ASSERT(!isBottom);
 				Item item(size, A);
 				item.empty();
 				data.push(new Item(item));
@@ -255,19 +326,25 @@ class PERSProblem {
 			}
 			
 			inline void leaveContext() {
+				ASSERT(!isBottom);
 				Item *ptr = data.pop();
 				delete ptr;
 			}
+			
 			inline int length() {
+				ASSERT(!isBottom);
 				return data.length();
 			}
 			
 			inline Item& getItem(const int idx) const {
+				ASSERT(!isBottom);
 				return(*data.get(idx));
 			}
 			
 					
 		private:
+		
+			Item whole;
 			genstruct::Vector<Item*> data;
 
 	};
