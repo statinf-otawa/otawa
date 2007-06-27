980d6131d9683e7aa99f75cb577c775766cefd80
ballabri <devnull@localhost>
2007-06-27 12:31:01 +0000

    Fixed a bunch of #include problems

--- include/otawa/cache/cat2/ACSBuilder.h
+++ include/otawa/cache/cat2/ACSBuilder.h
@@ -1,7 +1,32 @@
+/*
+ *	$Id$
+ *	This is the Abstract Cache State builder.
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *	02110-1301  USA
+ */
 #ifndef CACHE_ACSBUILDER_H_
 #define CACHE_ACSBUILDER_H_
 
+#include <otawa/prop/Identifier.h>
 #include <otawa/proc/Processor.h>
+#include <otawa/hard/Cache.h>
+#include <otawa/cache/LBlockSet.h>
 #include <otawa/cache/cat2/MUSTProblem.h>
 #include <otawa/cache/cat2/PERSProblem.h>
 
