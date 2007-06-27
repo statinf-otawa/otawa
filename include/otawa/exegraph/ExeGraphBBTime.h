567893368e0e561b2958407239559b9fb34bf95e
casse <devnull@localhost>
2007-06-27 09:35:25 +0000

    New version of ExeGraph.

--- include/otawa/exegraph/ExeGraphBBTime.h
+++ include/otawa/exegraph/ExeGraphBBTime.h
@@ -1,281 +1,115 @@
 /*
  *	$Id$
- *	Copyright (c) 2005, IRIT UPS.
+ *	exegraph module interface
  *
- *	otawa/ipet/TrivialBBTime.h -- TrivialBBTime class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#ifndef OTAWA_EXEGRAPHBBTIME_H
-#define OTAWA_EXEGRAPHBBTIME_H
+#ifndef EXECUTIONGRAPH_BBTIME_H
+#define EXECUTIONGRAPH_BBTIME_H
 
 #include <assert.h>
 #include <otawa/proc/BBProcessor.h>
-#include <otawa/exegraph/ExecutionGraph.h>
-#include <otawa/exegraph/Microprocessor.h>
-#include <elm/inhstruct/DLList.h>
+#include "ExeGraph.h"
 #include <elm/genstruct/DLList.h>
+#include <otawa/cfg/BasicBlock.h>
+#include <otawa/ipet.h>
+//#include <LiExeGraph.h>
+
+
+
+#define DO_LOG
+#if defined(NDEBUG) || !defined(DO_LOG)
+#	define LOG(c)
+#else
+#	define LOG(c) c
+#	define OUT	elm::cerr
+#endif
+
 
 namespace otawa {
 	
 using namespace elm::genstruct; 
 	
-class PrefixCost {
-	private:
-		elm::genstruct::DLList<int> _costs;
-		elm::genstruct::DLList<BasicBlock *> _reverse_prefix;
-	public:
-		inline PrefixCost(elm::genstruct::DLList<BasicBlock *> * prefix);
-		inline ~PrefixCost();
-		inline void addCost(int cost);
-		inline elm::genstruct::DLList<BasicBlock *> * reversePrefix();
-		inline bool isPrefix(elm::genstruct::DLList<BasicBlock *> * prefix);
-		class CostIterator: public elm::genstruct::DLList<int>::Iterator {
-				public:
-					inline CostIterator(const PrefixCost *prefix_cost);
-			};
-		
-};
- 
-inline PrefixCost::PrefixCost(elm::genstruct::DLList<BasicBlock *> * prefix) {
-	if (prefix) {
-		for (elm::genstruct::DLList<BasicBlock *>::Iterator bb(*prefix) ; bb ; bb++) {
-			_reverse_prefix.addLast(bb);
-		}
-	}
-}
-
-inline PrefixCost::~PrefixCost() {
-	_costs.clear();
-	_reverse_prefix.clear();
-}
-
-inline void PrefixCost::addCost(int cost) {
-	_costs.addLast(cost);
-}
-
-
-inline elm::genstruct::DLList<BasicBlock *> * PrefixCost::reversePrefix() {
-	return &_reverse_prefix;
-}
-
-inline bool PrefixCost::isPrefix(elm::genstruct::DLList<BasicBlock *> * prefix) {
-	if (prefix->isEmpty()) {
-		if (_reverse_prefix.isEmpty())
-			return true;
-		else
-			return false;
-	}
-	else {
-		if (_reverse_prefix.isEmpty())
-			return false;
-	}
-	
-	elm::genstruct::DLList<BasicBlock *>::Iterator bb1(*prefix), bb2(_reverse_prefix);
-	bb1.first();
-	bb2.first();
-	while (!bb1.ended() && !bb2.ended()) {
-		if (bb1.item() != bb2.item()) 
-			return false;
-		bb1.next();
-		bb2.next();
-	}
-	if (bb1.ended() != bb2.ended())
-		return false;
-	return true;
-}
-
-inline PrefixCost::CostIterator::CostIterator(const PrefixCost *prefix_cost):
-	elm::genstruct::DLList<int>::Iterator(prefix_cost->_costs) {
-}
-
-
-// --------------------------------------------------------------------
-// Block sequence class
-// --------------------------------------------------------------------
-
-class BlockSequence {
-	private:
-		elm::genstruct::DLList<BasicBlock *> block_list;
-		int _offset; // start point of the sequence (in the first block)
-		int inst_count;
-	public:
-		inline BlockSequence();
-		inline BlockSequence(BlockSequence *seq);
-		inline void addBlockFirst(BasicBlock *bb, int off);
-		inline void addBlockLast(BasicBlock *bb, int off);
-		inline bool isEmpty(void);
-		inline BasicBlock *first(void);
-		inline BasicBlock *last(void);
-		inline int instCount(void);
-		inline elm::genstruct::DLList<BasicBlock *> * blockList(void);
-		inline void dump(elm::io::Output& out_stream);
-		inline int offset();
-};
-
-inline BlockSequence::BlockSequence() 
-: inst_count(0) {
-}
-
-inline BlockSequence::BlockSequence(BlockSequence *seq) {
-	for (elm::genstruct::DLList<BasicBlock *>::Iterator bb(seq->block_list) ; bb ; bb++) {
-		block_list.addLast(bb);
-	}
-	inst_count = seq->instCount();
-}
-
-inline void BlockSequence::addBlockFirst(BasicBlock *bb, int off) {
-	block_list.addFirst(bb);
-	_offset = off;
-	inst_count = inst_count +  bb->countInstructions() - off;
-}
-
-inline void BlockSequence::addBlockLast(BasicBlock *bb, int off) {
-	block_list.addLast(bb);
-	_offset = off;
-	inst_count = inst_count +  bb->countInstructions() - off;
-}
-
-inline bool BlockSequence::isEmpty(void) {
-	return block_list.isEmpty();
-}
-
-inline BasicBlock * BlockSequence::first(void) {
-	return block_list.first();
-}
-
-inline BasicBlock * BlockSequence::last(void) {
-	return block_list.last();
-}
-
-inline int BlockSequence::instCount(void) {
-	return inst_count;
-}
-
-inline elm::genstruct::DLList<BasicBlock *> * BlockSequence::blockList(void) {
-	return &block_list;
-}
-
-inline void BlockSequence::dump(elm::io::Output& out_stream) {
-	for (elm::genstruct::DLList<BasicBlock *>::Iterator bb(this->block_list) ; bb ; bb++) {
-		out_stream << "b" << bb->number();
-		out_stream << " - ";
-	}
-	out_stream << " [offset=" << _offset << "] [inst_count=" << inst_count << "]";
-}
-
-inline int BlockSequence::offset() {
-	return _offset;
-}
-
-
-// --------------------------------------------------------------------
-// ExeGraphBBTime class
-// --------------------------------------------------------------------
-
+template <class G>
 class ExeGraphBBTime: public BBProcessor {
-	private:
+	protected:
+		typedef typename G::Node ExeNode;
 		FrameWork *fw;
 		PropList *properties;
-		elm::io::Output dumpFile;
-		Microprocessor *microprocessor;
-		bool delta, do_context;
-		elm::genstruct::DLList<PrefixCost *> costs;
-		bool built;
-	
+		Microprocessor<ExeNode> *microprocessor;
 	public:
-		ExeGraphBBTime(void);
-	
-		void buildExecutionGraph(FrameWork *fw, 
-								ExecutionGraph& graph, 
-								elm::genstruct::DLList<Inst *> &prologue, 
-								elm::genstruct::DLList<Inst *> &body, 
-								elm::genstruct::DLList<Inst *> &epilogue);
-		void buildPrologueList(	BasicBlock * bb,
-								elm::genstruct::DLList<ExecutionGraphInstruction *> * prologue, 
-								int capacity, 
-								elm::genstruct::DLList<elm::genstruct::DLList<ExecutionGraphInstruction *> *> * prologue_list);
-		void buildEpilogueList(BasicBlock * bb,
-								elm::genstruct::DLList<ExecutionGraphInstruction *> * epilogue, 
-								int capacity, 
-								elm::genstruct::DLList<elm::genstruct::DLList<ExecutionGraphInstruction *> *>* epilogue_list,
-								int start_index);
-		void buildPrefixList(
-								BasicBlock * bb,
-								elm::genstruct::DLList<ExecutionGraphInstruction *> * prefix, 
-								int max_size, 
-								int current_size,
-								elm::genstruct::DLList<elm::genstruct::DLList<ExecutionGraphInstruction *> *> * prefix_list);
-		int analyzeExecutionGraph(ExecutionGraph& graph);
-
+		ExeGraphBBTime(const PropList& props = PropList::EMPTY);
 		// BBProcessor overload
-		int processSequence( FrameWork *fw,
-			elm::genstruct::DLList<ExecutionGraphInstruction *> * prologue,
-			elm::genstruct::DLList<ExecutionGraphInstruction *> * body,
-			elm::genstruct::DLList<ExecutionGraphInstruction *> * epilogue,
-			int capacity ) ;
-
-	// Configuration Properties
-	static Identifier<Microprocessor *> PROCESSOR;
-	static Identifier<elm::io::Output *>  LOG_OUTPUT;
-	static Identifier<bool> DELTA;
-	static Identifier<bool> CONTEXT;
-
-	// statistics
-	typedef struct stat_t {
-		double total_span_sum;
-		double total_vals_sum;
-		int bb_cnt;
-		int bb_span_sum;
-		int bb_vals_sum;
-		int seq_cnt;
-		inline stat_t(void): total_span_sum(0), total_vals_sum(0),
-		bb_cnt(0),bb_span_sum(0), bb_vals_sum(0), seq_cnt(0) { };
-	} stat_t;
-	static Identifier<Vector <stat_t> *> PREFIX_STATS;
-
-protected:
-	virtual void configure(const PropList& props = PropList::EMPTY);
-	virtual void setup(FrameWork *fw);
-	virtual void processBB(FrameWork *fw, CFG *cfg, BasicBlock *bb);
-	virtual void cleanup(FrameWork *fw);
-		
-private:
-		void recordDelta(
-			DLList<ExecutionGraphInstruction *> *insts,
-			int cost,
-			BasicBlock *bb);
-
-		typedef struct node_stat_t {
-			struct node_stat_t *children, *sibling;
-			int min, max;
-			BasicBlock *bb;
-			Vector<int> vals;
-			inline node_stat_t(BasicBlock *_bb, int cost)
-			: bb(_bb), children(0), sibling(0), min(cost), max(cost) { }
-		} node_stat_t;
-
-		Vector<stat_t>& exe_stats;
-		node_stat_t stat_root;
-
-		void initPrefixStats(BasicBlock *bb);		
-		void recordPrefixStats(
-			DLList<ExecutionGraphInstruction *> *insts,
-			int cost);
-		node_stat_t *recordPrefixNode(
-			node_stat_t *parent,
-			BasicBlock *bb,
-			int cost);
-		void recordPrefixNodeStats(node_stat_t *node, int cost);
-		void collectPrefixStats(int depth = 0, node_stat_t *node = 0);
-		
-		typedef struct context_t {
-			BasicBlock *bb;
-			struct context_t *prev;
-			inline context_t(BasicBlock *_bb, context_t *_prev)
-			: bb(_bb), prev(_prev) { }
-		} context_t;
-		void buildContext(int cost, node_stat_t *stat = 0, context_t *ctx = 0); 
+		void processWorkSpace(FrameWork *fw);
+		void processBB(FrameWork *fw, CFG *cfg, BasicBlock *bb) {}
+		// Configuration Properties
+		static Identifier<Microprocessor<ExeNode> *> PROCESSOR;
+		static Identifier<elm::io::Output *>  LOG_OUTPUT;
 };
+
+template <class G>
+Identifier<Microprocessor<typename G::Node> *> 
+	ExeGraphBBTime<G>::PROCESSOR("otawa.ExeGraphBBTime.proc.", NULL);
+
+
+template <class G>
+ExeGraphBBTime<G>::ExeGraphBBTime(const PropList& props) 
+:	BBProcessor(),
+	microprocessor(PROCESSOR(props)),
+	fw(0)
+{
+  provide(ipet::BB_TIME_FEATURE);
+}
+
+template <class G>
+void ExeGraphBBTime<G>::processWorkSpace(FrameWork *fw) {
+	bool built = false, reset = false;
+	this->fw = fw;
+	
+	// If required, find the microprocessor
+	if(!microprocessor) {
+		microprocessor = PROCESSOR(fw);
+		if(microprocessor) 
+			reset = true;
+		else {
+			const hard::Processor *proc = fw->platform()->processor();
+			if(!proc)
+				throw ProcessorException(*this, "no processor to work with");
+			else {
+				reset = true;
+				built = true;
+				microprocessor = new Microprocessor<ExeNode>(proc);
+			}
+		}
+	}
+	elm::cerr << "=> microprocessor = " << microprocessor << io::endl;
+	
+	// Perform the actual process
+	BBProcessor::processWorkSpace(fw);
+	
+	// Cleanup if required
+	if(built)
+		delete microprocessor;
+	if(reset)
+		microprocessor = 0;
+}
+
 
 } //otawa
 
-#endif // OTAWA_EXEGRAPHBBTIME_H
+#endif // OTAWA_ExeGraphBBTime_H
