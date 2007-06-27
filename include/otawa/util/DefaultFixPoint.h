2fa56a9fdfd2afed556bb262e697f7daf0d876f3
ballabri <devnull@localhost>
2007-06-27 12:31:00 +0000

    Fixed a bunch of #include problems

--- include/otawa/util/DefaultFixPoint.h
+++ include/otawa/util/DefaultFixPoint.h
@@ -1,10 +1,36 @@
+/*
+ *	$Id$
+ *	Default FixPoint implementation (no loop unrolling)
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *	02110-1301  USA
+ */
+
 #ifndef UTIL_DEFAULTFIXPOINT_H_
 #define UTIL_DEFAULTFIXPOINT_H_
 
 
 #include <otawa/util/HalfAbsInt.h>
-
-
+#include <otawa/cfg/CFG.h>
+#include <otawa/cfg/BasicBlock.h>
+#include <otawa/cfg/Edge.h>
+#include <otawa/prop/PropList.h>
+#include <elm/genstruct/Vector.h>
 
 namespace otawa {
 
