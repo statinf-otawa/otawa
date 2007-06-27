1c306b72ed4a77fb936a0396c6327a232758c2cd
ballabri <devnull@localhost>
2007-06-27 12:31:03 +0000

    Fixed a bunch of #include problems

--- include/otawa/cfg/Virtualizer.h
+++ include/otawa/cfg/Virtualizer.h
@@ -1,9 +1,38 @@
+/*
+ *	$Id$
+ *	Virtualizer processor (function inliner)
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *	02110-1301  USA
+ */
+
 #ifndef CFG_VIRTUALIZER_H_
 #define CFG_VIRTUALIZER_H_
 
 #include <otawa/proc/Processor.h>
 #include <otawa/proc/Feature.h>
-
+#include <otawa/prop/Identifier.h>
+#include <elm/genstruct/HashTable.h>
+#include <otawa/cfg/CFG.h>
+#include <otawa/cfg/VirtualCFG.h>
+#include <otawa/cfg/BasicBlock.h>
+#include <otawa/prog/WorkSpace.h>
+#include <otawa/prop/PropList.h>
 namespace otawa {
 
 
