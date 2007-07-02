429a25b0f90ee2fd9d658e3a46d216363242e0b9
casse <devnull@localhost>
2007-07-02 09:05:42 +0000

    Distribution generation files.

--- dist/make-dist.sh
+++ dist/make-dist.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
+root=`dirname $0`
 
 # Select version
-dist=default
+dist=default.otawa
 if [ "$1" != "" ]; then
 	dist="$1"
 fi
-path="$dist.otawa"
+path="$dist"
 if [ ! -f $path ]; then
 	echo "ERROR: distribution $path does not exist !"
 	exit 1
 fi
 
 # Let's go
-out="build-$dist.sh"
 line=`grep -n "###configuration###" build.sh | cut -f 1 -d ":"`
-head -$line build.sh > $out
-cat $path >> $out
-echo "config=no" >> $out
-size=`wc -l < build.sh`
+head -$line build.sh
+cat $path
+echo "config=no"
+size=`wc -l < $root/build.sh`
 tail=`expr $size - $line - 2`
-tail -$tail build.sh >> $out
-chmod +x $out
+tail -$tail build.sh
