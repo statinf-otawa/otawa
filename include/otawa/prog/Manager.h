963a0fb242bf845bb7db748e0944ca439de0c950
casse <devnull@localhost>
2007-07-27 07:49:33 +0000

    Added the identifier ILP_PLUGIN_NAME.

--- include/otawa/prog/Manager.h
+++ include/otawa/prog/Manager.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2003, IRIT UPS.
+ *	Manager class interface
  *
- *	manager.h -- manager classes interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2003-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_MANAGER_H
 #define OTAWA_MANAGER_H
@@ -91,6 +106,7 @@ extern Identifier<hard::CacheConfiguration *> CACHE_CONFIG;
 extern Identifier<elm::system::Path> PROCESSOR_PATH;
 extern Identifier<elm::xom::Element *> PROCESSOR_ELEMENT;
 extern Identifier<hard::Processor *> PROCESSOR;
+extern Identifier<cstring> ILP_PLUGIN_NAME;
 
 } // otawa
 
