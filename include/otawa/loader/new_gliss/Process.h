094ffe18f438c72c6422e6a2ef73ff5f23defd26
casse <devnull@localhost>
2007-09-26 11:59:27 +0000

    Added support for MEMORY_ACCESS_FEATURE.

--- include/otawa/loader/new_gliss/Process.h
+++ include/otawa/loader/new_gliss/Process.h
@@ -26,16 +26,30 @@ public:
 	virtual hard::Platform *platform(void);
 	virtual Inst *start(void);
 	virtual File *loadFile(elm::CString path);
+	virtual void get(Address at, signed char& val);
+	virtual void get(Address at, unsigned char& val);
+	virtual void get(Address at, signed short& val);
+	virtual void get(Address at, unsigned short& val);
+	virtual void get(Address at, signed long& val);
+	virtual void get(Address at, unsigned long& val);
+	virtual void get(Address at, signed long long& val);
+	virtual void get(Address at, unsigned long long& val);
+	virtual void get(Address at, Address& val);
+	virtual void get(Address at, string& str);
+	virtual void get(Address at, char *buf, int size);
 
+	
 protected:
 	friend class Segment;
 	virtual Inst *decode(address_t addr) = 0;
 	virtual void *gelFile(void) = 0;
+	virtual void *memory(void) = 0;
 	
 public:
 	Inst *_start; 
 	hard::Platform *_platform;
 	void *_state;
+	void *_memory;
 	int argc;
 	char **argv, **envp;
 	bool no_stack;
--- include/otawa/loader/old_gliss/Process.h
+++ include/otawa/loader/old_gliss/Process.h
@@ -30,15 +30,28 @@ public:
 	virtual Inst *start(void);
 	virtual File *loadFile(elm::CString path);
 	virtual Loader *loader(void) const;
+	virtual void get(Address at, signed char& val);
+	virtual void get(Address at, unsigned char& val);
+	virtual void get(Address at, signed short& val);
+	virtual void get(Address at, unsigned short& val);
+	virtual void get(Address at, signed long& val);
+	virtual void get(Address at, unsigned long& val);
+	virtual void get(Address at, signed long long& val);
+	virtual void get(Address at, unsigned long long& val);
+	virtual void get(Address at, Address& val);
+	virtual void get(Address at, string& str);
+	virtual void get(Address at, char *buf, int size);
 
 protected:
 	friend class Segment;
 	virtual Inst *decode(address_t addr) = 0;
-	
+	virtual void *memory(void) = 0;
+
 public:
 	Inst *_start; 
 	hard::Platform *_platform;
 	void *_state;
+	void *_memory;
 	int argc;
 	char **argv, **envp;
 	Loader *_loader;
