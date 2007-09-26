de1aa9059464afcb963799207f4c5573cb5371c4
casse <devnull@localhost>
2007-09-26 12:01:20 +0000

    Added support:   - processor-relative features,   - ACCESS_MEMORY_FEATURE.

--- include/otawa/prog/Process.h
+++ include/otawa/prog/Process.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2003-07, IRIT UPS.
- *
  *	Process class interface
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2003-7, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_PROGRAM_PROCESS_H
 #define OTAWA_PROGRAM_PROCESS_H
@@ -12,6 +27,7 @@
 #include <elm/genstruct/Vector.h>
 #include <otawa/instruction.h>
 #include <otawa/program.h>
+#include <otawa/proc/Feature.h>
 
 namespace elm { namespace xom {
 	class Element;
@@ -53,11 +69,16 @@ private:
 // Process class
 class Process: public PropList {
 	Vector<File *> files;
+	Vector<AbstractFeature *> provided;
 	File *prog;
 	Manager *man;
+	void link(WorkSpace *ws);
+	void unlink(WorkSpace *ws);
 
 protected:
+	friend class WorkSpace;
 	void addFile(File *file);
+	void provide(AbstractFeature& feature);
 
 public:
 	Process(Manager *manager, const PropList& props = EMPTY, File *program = 0);
@@ -71,12 +92,28 @@ public:
 	virtual Inst *findInstAt(address_t addr);
 	virtual address_t findLabel(String& label);
 	virtual Inst *findInstAt(String& label);
-	inline File *program(void) const;
+	inline File *program(void) const { return prog; }
 	virtual int instSize(void) const = 0;
 	virtual Processor *decoder(void);
 	virtual Loader *loader(void) const;
 	Symbol *findSymbol(const string& name); 	
 
+	// Memory access
+	virtual void get(Address at, signed char& val);
+	virtual void get(Address at, unsigned char& val);
+	virtual void get(Address at, signed short& val);
+	virtual void get(Address at, unsigned short& val);
+	virtual void get(Address at, signed long& val);
+	virtual void get(Address at, unsigned long& val);
+	virtual void get(Address at, signed long long& val);
+	virtual void get(Address at, unsigned long long& val);
+	virtual void get(Address at, Address& val);
+	virtual void get(Address at, float& val);
+	virtual void get(Address at, double& val);
+	virtual void get(Address at, long double& val);
+	virtual void get(Address at, string& str);
+	virtual void get(Address at, char *buf, int size);
+	
 	// Simulation management
 	virtual SimState *newState(void);
 	virtual sim::Simulator *simulator(void);
@@ -96,10 +133,49 @@ public:
 
 };
 
-// Inlines
-inline File *Process::program(void) const {
-	return prog;
-}
+
+// Process display
+elm::io::Output& operator<<(elm::io::Output& out, Process *proc); 
+
+
+// ProcessException class
+class ProcessException: public Exception {
+public:
+	inline ProcessException(Process *process): proc(process)
+		{ ASSERTP(process, "null process passed"); }
+	inline Process *process(void) const { return proc; }
+private:
+	Process *proc;
+};
+
+
+// UnsupportedFeatureException class
+class UnsupportedFeatureException: public ProcessException {
+public:
+ 	inline UnsupportedFeatureException(
+ 		Process *proc,const AbstractFeature& feature)
+ 		: ProcessException(proc), f(feature) { }
+ 	inline const AbstractFeature& feature(void) const { return f; }
+ 	virtual String 	message(void); 
+private:
+	const AbstractFeature& f;
+};
+
+// OutOfSegmentException class
+class OutOfSegmentException: public ProcessException { 
+public:
+	OutOfSegmentException(Process *proc, Address address)
+		: ProcessException(proc), addr(address) { }
+	inline Address address(void) const { return addr; }
+ 	virtual String 	message(void); 	
+private:
+	Address addr;
+};
+
+// Features
+extern Feature<NoProcessor> MEMORY_ACCESS_FEATURE;
+extern Feature<NoProcessor> FLOAT_MEMORY_ACCESS_FEATURE;
+extern Feature<NoProcessor> STACK_USAGE_FEATURE;
 
 } // otawa
 
