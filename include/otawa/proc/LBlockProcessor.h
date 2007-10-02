d5f57c699a7466f425652cb9b273a16cd6670887
ballabri <devnull@localhost>
2007-10-02 14:17:13 +0000

    add lblockprocessor

---
+++ include/otawa/proc/LBlockProcessor.h
@@ -0,0 +1,35 @@
+/*
+ *	$Id$
+ *	Copyright (c) 2005-06, IRIT UPS.
+ *
+ *	otawa/proc/LBlockProcessor.h -- LBlockProcessor class interface.
+ */
+#ifndef OTAWA_PROC_LBLOCKPROCESSOR_H
+#define OTAWA_PROC_LBLOCKPROCESSOR_H
+
+#include <otawa/proc/Processor.h>
+#include <elm/genstruct/SLList.h>
+
+namespace otawa {
+	
+// Extern class
+class LBlock;
+class BasicBlock;
+	
+// Processor class
+class LBlockProcessor: public Processor {
+	elm::CString name;
+	CFG *last;
+	void init(const PropList& props);
+protected:
+	virtual void processWorkSpace(WorkSpace *fw);
+	virtual void processLBlock(WorkSpace *fw, LBlock *cfg) = 0;
+public:
+	LBlockProcessor(void);
+	LBlockProcessor(elm::String name, elm::Version version);
+	virtual void configure(const PropList& props);	
+};
+
+} // otawa
+
+#endif // OTAWA_PROC_LBLOCKPROCESSOR_H
