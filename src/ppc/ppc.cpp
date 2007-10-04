e308f3744d0cc9da6b3ed78d77d04af2ca843eb7
casse <devnull@localhost>
2007-10-04 08:14:36 +0000

    Added REGISTER_USAGE_FEATURE support.

--- src/ppc/ppc.cpp
+++ src/ppc/ppc.cpp
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2007, IRIT UPS <casse@irit.fr>
+ *	PowerPC OTAWA plugin
  *
- *	PowerPC plugin implementation
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <elm/assert.h>
@@ -276,6 +291,7 @@ Process::Process(
 	hard::Platform *pf,
 	const PropList& props
 ): otawa::loader::new_gliss::Process(manager, pf, props) {
+	provide(REGISTER_USAGE_FEATURE);
 	/*if(Processor::VERBOSE(props)) {
 		cerr << "PPC: sizeof(Inst) = " << sizeof(Inst) << io::endl;
 		cerr << "PPC: sizeof(BranchInst) = " << sizeof(BranchInst) << io::endl;
