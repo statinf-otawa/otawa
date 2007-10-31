c49b5a94f1028b2205a8f6c793d702ada8c15f8e
ballabri <devnull@localhost>
2007-10-31 13:56:59 +0000

    Separate feature dependency information.

--- src/prog/cfg_VirtualCFG.cpp
+++ src/prog/cfg_VirtualCFG.cpp
@@ -202,9 +202,11 @@ VirtualCFG::VirtualCFG(CFG *cfg, bool inlined): _cfg(cfg) {
 /**
  * Build a new empty VirtualCFG
  */
-VirtualCFG::VirtualCFG() {
-  /*_bbs.add(&_entry);
-  _bbs.add(&_exit);*/
+VirtualCFG::VirtualCFG(bool addEntryExit) {
+  if (addEntryExit) {
+    _bbs.add(&_entry);
+    _bbs.add(&_exit);
+  }
   _entry._cfg = this;
   _exit._cfg = this;
   flags |= FLAG_Scanned;
