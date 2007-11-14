4879fb8175f88553f7c4231379b94f5c8cee6209
casse <devnull@localhost>
2007-11-14 19:40:50 +0000

    Added the list of feature to the documentation.

---
+++ src/prog/feature.h
@@ -0,0 +1,78 @@
+/*
+ *	$Id$
+ *	Feature documentation
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * @page features "Available Features"
+ * 
+ * @par Program Features
+ * 
+ * @li @ref FLOW_FACTS_FEATURE
+ * 
+ * @par CFG Features
+ * 
+ * @li @ref CFG_INFO_FEATURE
+ * @li @ref COLLECTED_CFG_FEATURE
+ * @li @ref CONTEXT_TREE_FEATURE
+ * @li @ref DOMINANCE_FEATURE
+ * @li @ref LOOP_HEADERS_FEATURE
+ * @li @ref LOOP_INFO_FEATURE
+ * @li @ref NORMALIZED_CFGS_FEATURE
+ * @li @ref PCG_FEATURE
+ * @li @ref UNROLLED_LOOPS_FEATURE
+ * @li @ref VIRTUALIZED_CFG_FEATURE
+ * 
+ * @par IPET Features
+ * 
+ * @li @ref CONTROL_CONSTRAINTS_FEATURE
+ * @li @ref FLOW_FACTS_CONSTRAINTS_FEATURE
+ * @li @ref ILP_SYSTEM_FEATURE
+ * @li @ref OBJECT_FUNCTION_FEATURE
+ * @li @ref WCET_COUNT_RECORDED_FEATURE
+ * @li @ref WCET_FEATURE
+ * 
+ * Pipeline timings features :
+ * @li @ref ASSIGNED_VARS_FEATURE
+ * @li @ref BB_TIME_FEATURE
+ * @li @ref DELTA_SEQUENCES_FEATURE
+ * @li @ref EDGE_TIME_FEATURE
+ * @li @ref ipet::FLOW_FACTS_FEATURE
+ * @li @ref INTERBLOCK_SUPPORT_FEATURE
+ * 
+ * Cache specific features :
+ * @li @ref CCG_FEATURE
+ * @li @ref COLLECTED_LBLOCKS_FEATURE
+ * @li @ref DATA_CACHE_SUPPORT_FEATURE
+ * @li @ref ICACHE_ACS_FEATURE
+ * @li @ref ICACHE_ACS_MAY_FEATURE
+ * @li @ref ICACHE_CATEGORY_FEATURE
+ * @li @ref ICACHE_CATEGORY2_FEATURE
+ * @li @ref ICACHE_FIRSTLAST_FEATURE
+ * @li @ref INST_CACHE_SUPPORT_FEATURE
+ * 
+ * @par Loader Features
+ * 
+ * @li @ref FLOAT_MEMORY_ACCESS_FEATURE
+ * @li @ref MEMORY_ACCESS_FEATURE
+ * @li @ref REGISTER_USAGE_FEATURE
+ * @li @ref STACK_USAGE_FEATURE
+ */
+
