fa52eb481d9365f5ef62548063d3ca65094723a9
casse <devnull@localhost>
2007-11-21 09:15:29 +0000

    No more independent variables.

--- include/otawa/ilp/Var.h
+++ include/otawa/ilp/Var.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2005, IRIT UPS.
+ *	Var class interface
  *
- *	ilp/Var.h -- interface to the Var class.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_ILP_VAR_H
 #define OTAWA_ILP_VAR_H
@@ -15,36 +30,29 @@ using namespace elm;
 
 // Var class
 class Var {
-	elm::String _name;
 public:
-	inline Var(void);
-	inline Var(const char *name);
-	inline Var(const elm::String& name);
-	inline elm::String& name(void);
-	inline String makeName(void) {
-		if(_name) return _name;
-		else return _ << "0x" << (void *)this;
-	}
+	virtual ~Var(void);
+	inline elm::String& name(void) { return _name; }
+	virtual void print(io::Output& out);
+
+protected:
+	inline Var(void) { }
+	inline Var(cstring name): _name(name) { }
+	inline Var(const string& name): _name(name) { }
+
+private:
+	string _name;
 };
-
-// Inlines
-inline Var::Var(void) {
-}
-
-inline Var::Var(const char *name): _name(name) {
-}
-
-inline Var::Var(const elm::String& name): _name(name) {
-}
-
-inline elm::String& Var::name(void) {
-	return _name;
-}
 
 // Output
 inline io::Output& operator<<(io::Output& out, Var *var) {
-	out << "ilp::Var(" << var->makeName() << ')';
+	out << "ilp::Var("; var->print(out); out << ')';
 	return out;
+}
+
+inline io::Output& operator<<(io::Output& out, Var& var) {
+	out << "ilp::Var("; var.print(out); out << ')';
+	return out;	
 }
 
 } } // otawa::ilp
