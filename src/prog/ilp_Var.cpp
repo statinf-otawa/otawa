580992cd6d5f5d9906249f72071ddea651c7a313
casse <devnull@localhost>
2007-11-21 09:17:56 +0000

    No more independent variables.

--- src/prog/ilp_Var.cpp
+++ src/prog/ilp_Var.cpp
@@ -1,10 +1,24 @@
 /*
  *	$Id$
- *	Copyright (c) 2005, IRIT UPS.
+ *	Var class implementation
  *
- *	src/ilp_Var.h -- implementation of the Var class.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-07, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
 #include <otawa/ilp/Var.h>
 
 namespace otawa { namespace ilp {
@@ -25,14 +39,14 @@ namespace otawa { namespace ilp {
 
 
 /**
- * @fn Var::Var(const char *name);
+ * @fn Var::Var(cstring *name);
  * Build a variable with the given name.
  * @param name	Name of the variable.
  */
 
 
 /**
- * @fn Var::Var(String& name);
+ * @fn Var::Var(const string& name);
  * Build a variable with the given name.
  * @param name	Name of the variable.
  */
@@ -44,5 +58,22 @@ namespace otawa { namespace ilp {
  * @return	Variable name.
  */
 
+
+/**
+ */
+Var::~Var(void) {
+}
+
+
+/**
+ * Print the name of the variable.
+ * @param out	Output to use.
+ */
+void Var::print(io::Output& out) {
+	if(_name)
+		out << _name;
+	else
+		out << '_' << io::hex((int)this);
+}
 
 } } // otawa::ilp
