f5a298fd5fc511b0c7102fb9860933f717c72723
casse <devnull@localhost>
2008-01-12 10:42:26 +0000

    bpred classes moved in otawa::ipet namespace.

--- src/bpred/BPredProcessor_Bimodal.cpp
+++ src/bpred/BPredProcessor_Bimodal.cpp
@@ -1,4 +1,26 @@
-#include <otawa/ipet/bpred/BPredProcessor.h>
+/*
+ *	$Id$
+ *	BPredProcessor class implementation
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2008, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <otawa/ipet/BPredProcessor.h>
 #include "BCGDrawer.h"
 #include "BSets.h"
 #include "BCG.h"
@@ -13,7 +35,6 @@ using namespace elm::genstruct;
 
 namespace otawa {
 namespace ipet {
-namespace bpred {
 
 
 /////// CREATION DE CONTRAINTES ET VARIABLES
@@ -876,6 +897,5 @@ void BPredProcessor::processCFG__Bimodal(WorkSpace *fw, CFG *cfg) {
 	
 }
 
-} // ::bpred
 } // ::ipet
 } // ::otawa
