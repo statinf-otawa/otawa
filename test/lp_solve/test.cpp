fdd8c1259cfa56d194db776f567358702adcd9db
casse <devnull@localhost>
2008-03-26 15:27:04 +0000

    -Wall compliance

--- test/lp_solve/test.cpp
+++ test/lp_solve/test.cpp
@@ -27,24 +27,27 @@ void check(char *msg, int req, int fnd) {
 extern otawa::ilp::ILPPlugin& lp_solve5_plugin;
 
 int main(void) {
-	Var v1, v2, v3, v4;
 	System *sys = lp_solve5_plugin.newSystem();
+	Var *v1 = sys->newVar(),
+		*v2 = sys->newVar(),
+		*v3 = sys->newVar(),
+		*v4 = sys->newVar();
 	
 	// Build the system
 	Constraint *cons = sys->newConstraint(ilp::Constraint::LE, 4);
-	cons->add(3, &v1);
-	cons->add(2, &v2);
-	cons->add(2, &v3);
-	cons->add(1, &v4);
+	cons->add(3, v1);
+	cons->add(2, v2);
+	cons->add(2, v3);
+	cons->add(1, v4);
 	cons = sys->newConstraint(ilp::Constraint::GE, 3);
-	cons->add(0, &v1);
-	cons->add(4, &v2);
-	cons->add(3, &v3);
-	cons->add(1, &v4);
-	sys->addObjectFunction(2,&v1);
-	sys->addObjectFunction(3,&v2);
-	sys->addObjectFunction(-2,&v3);
-	sys->addObjectFunction(3,&v4);
+	cons->add(0, v1);
+	cons->add(4, v2);
+	cons->add(3, v3);
+	cons->add(1, v4);
+	sys->addObjectFunction(2,v1);
+	sys->addObjectFunction(3,v2);
+	sys->addObjectFunction(-2,v3);
+	sys->addObjectFunction(3,v4);
 	
 	// Solve it
 	sys->dump();
@@ -52,10 +55,10 @@ int main(void) {
 	
 	// Display result
 	check("Result", 12, lround(sys->value()));
-	check("v1", 0, lround(sys->valueOf(&v1)));
-	check("v2", 0, lround(sys->valueOf(&v2)));
-	check("v3", 0, lround(sys->valueOf(&v3)));
-	check("v4", 4, lround(sys->valueOf(&v4)));
+	check("v1", 0, lround(sys->valueOf(v1)));
+	check("v2", 0, lround(sys->valueOf(v2)));
+	check("v3", 0, lround(sys->valueOf(v3)));
+	check("v4", 4, lround(sys->valueOf(v4)));
 	
 	return 0;
 }
