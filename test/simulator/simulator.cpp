0c02afa58d384d0b0fe87f46bdc69cfe49bceeea
casse <devnull@localhost>
2008-04-07 15:21:28 +0000

    Generic simulator conversion.

--- test/simulator/simulator.cpp
+++ test/simulator/simulator.cpp
@@ -1,12 +1,10 @@
 /*
  *	$Id$
- *	Copyright (c) 2003, IRIT UPS.
+ *	Copyright (c) 2006-08, IRIT UPS.
  *
  *	test/ipet/exegraph.cpp -- test for Execution Graph modeling feature.
  */
 
-#include <systemc>
-#include <stdlib.h>
 #include <elm/io.h>
 #include <otawa/otawa.h>
 #include <otawa/ipet.h>
@@ -14,112 +12,134 @@
 #include <otawa/hard/CacheConfiguration.h>
 #include <otawa/ilp.h>
 #include <otawa/sim/BasicBlockDriver.h>
-#include <ARMSimulator.h>
+#include <otawa/gensim/GenericSimulator.h>
+#include <otawa/sim/Driver.h>
 
 using namespace elm;
 using namespace elm::io;
 using namespace otawa;
 using namespace otawa::ipet;
 using namespace std;
-using namespace sc_core;
-using namespace sc_dt;
 
-// void sc_main
-int sc_main(int argc, char *argv[]) {
-	return 0;
-}
+// Verbose flag
+bool my_verbose = false;
 
-
-
-int main(int argc, char **argv) {
+// MyDriver class
+class MyDriver: public sim::Driver {
+public:
 	
-	elm::io::OutFileStream logStream("/home/rochange/ECLIPSE/otawa/otawa/test/simulator/log");
-	elm::io::Output logFile(logStream);
+	MyDriver(SimState& _state, Inst *start, Inst *_exit)
+	: state(_state), next(start), exit(_exit) {	 }
 	
-	sc_core::sc_elab_and_sim(argc, argv);
-	
-	
-	Manager manager;
-	PropList props;
-	props.set<Loader *>(Loader::ID_Loader, &Loader::LOADER_Gliss_PowerPC);
-	
-	
-	try {
-		
-		// Load program
-		if(argc < 2) {
-			elm::cerr << "ERROR: no argument.\n"
-				 << "Syntax is : exegraph <executable>\n";
-			exit(2);
+	virtual Inst *nextInstruction(sim::State &_state, Inst *inst) {
+		Inst *res = next;
+		if(next) {
+			if(my_verbose)
+				cerr << "executing " << next->address() << ": " << next << io::endl;
+			next = state.execute(next);
+			if(next == exit)
+				next = 0;
 		}
-		FrameWork *fw = manager.load(argv[1], props);
-		
-		
-		// Find main CFG
-		elm::cout << "Looking for the main CFG\n";
-		CFG *cfg = fw->getCFGInfo()->findCFG("main");
-		if(cfg == 0) {
-			elm::cerr << "ERROR: cannot find main !\n";
-			return 1;
+		return res;
+	}
+	
+	virtual void terminateInstruction (sim::State &_state, Inst *inst) { }
+	virtual void redirect (sim::State &_state, Inst *branch, bool direction) { }
+	virtual bool PredictBranch (sim::State &_state, Inst *branch, bool pred) { }
+
+private:
+	SimState& state;
+	Inst *next, *exit;
+};
+
+	
+int main(int argc, char **argv) {
+
+	PropList props;
+	String processor;
+	String cache;
+	
+	// Scan the arguments
+	int i = 1;
+	while(i < argc) {
+		if(CString(argv[i]) == "-p") {
+			i++;
+			if(i >= argc)
+				throw otawa::Exception("no argument after -p");
+			processor = argv[i++]; 
+		}
+		if(CString(argv[i]) == "-c") {
+			i++;
+			if(i >= argc)
+				throw otawa::Exception("no argument after -c");
+			cache = argv[i++]; 
+		}
+		else if(CString(argv[i]) == "-v") {
+			i++;
+			my_verbose = true;
 		}
 		else
-			elm::cout << "main found at 0x" << cfg->address() << '\n';
+			break;
+	}
+	if(i >= argc)
+		throw otawa::Exception("no executable given !");
+	
+	ARGC(props) = argc - i;
+	ARGV(props) = argv + i;
+	try {
 		
-		// Removing __eabi call if available
-		for(CFG::BBIterator bb(cfg); bb; bb++)
-			for(BasicBlock::OutIterator edge(bb); edge; edge++)
-				if(edge->kind() == otawa::Edge::CALL
-				&& edge->target()
-				&& edge->calledCFG()->label() == "__eabi") {
-					delete(*edge);
-					break;
-				}
-		
-		// Now, use a VCFG
-		VirtualCFG vcfg(cfg);
-		
-		// Prepare processor configuration
-		/*PropList props;
-		props.set(IPET::ID_Explicit, true);*/
-		
-		ARMSimulator simulator;
-		ARMState *simulator_state = (ARMState *) simulator.instantiate(fw,props);
-		simulator_state->init();
-		
-		// Compute BB times
-		//elm::cout << "Timing the BB\n";
-		//TrivialBBTime tbt(5, props);
-		//tbt.processCFG(fw, &vcfg);
-		
-		// Compute BB times with execution graphs
-		logFile << "\n----------------------------------------------\n";
-		logFile << "Timing the BB with execution graphs\n";
-		logFile << "----------------------------------------------\n";
-		logFile << "CFG: \n";
-		for(CFG::BBIterator bb(&vcfg); bb; bb++) {
-			if (bb->countInstructions() != 0) {
-				logFile << "block b" << bb->number() << ":";
-				int start_cycle = simulator_state->cycle();
-				//simulator_state->setPC(bb->address());
-				//simulator_state->runUntil(bb->address() + 4*bb->size());
-				sim::BasicBlockDriver driver(bb);
-				simulator_state->run(driver);
-				int finish_cycle = simulator_state->cycle();
-				logFile << finish_cycle - start_cycle << " cycles\n";
-			}
+		// Configure the process if any
+		if(processor) {
+			PROCESSOR_PATH(props) = processor;
+			if(cache)
+				CACHE_CONFIG_PATH(props) = cache;
 		}
 		
-//		ExeGraphBBTime tbt(props, &processor, logFile);
-//		tbt.processCFG(fw, &vcfg);
+		// Load program
+		Processor::VERBOSE(props) = my_verbose;
+		WorkSpace *ws = MANAGER.load(argv[i], props);
 		
+		// Find interesting instruction
+		Inst *start_inst = ws->process()->findInstAt("_start");
+		if(!start_inst)
+			throw otawa::Exception("no _start label !");
+		Inst *exit_inst = ws->process()->findInstAt("_exit");
+		if(!exit_inst)
+			throw otawa::Exception("no _exit label !");
+		if(my_verbose) {
+			cerr << "_start = " << start_inst->address() << io::endl;
+			cerr << "_exit = " << exit_inst->address() << io::endl;
+		}
+		Inst *main_inst = ws->process()->findInstAt(Address(0x100004c8));
+		
+		// Build the functional state
+		SimState *state = ws->process()->newState();
+		
+		// Full simulation
+		if(processor) {
+			MyDriver driver(*state, start_inst, exit_inst);
+			gensim::GenericSimulator simulator;
+			sim::State *simulator_state = simulator.instantiate(ws, props);
+			simulator_state->run(driver);
+		}
+		
+		// Functional simulation only
+		else {
+			Inst *inst = start_inst;
+			while(inst != exit_inst) {
+				if(inst == main_inst) {
+					cerr << "found !\n";
+				}
+				if(my_verbose)
+					cerr << "executing " << inst->address() << ": " << inst << io::endl;
+				inst = state->execute(inst);
+			}
+		}
 	}
-	catch(LoadException e) {
-//		elm::cerr << "ERROR: " << e.message() << '\n';
-		exit(1);
-	}
-	catch(ProcessorException e) {
-//		elm::cerr << "ERROR: " << e.message() << '\n';
-		exit(1);
+
+	catch(elm::Exception& e) {
+		elm::cerr << "ERROR: " << e.message() << '\n';
+		return 1;
 	}
 	return 0;
 
