3e4d228e57386cb733a63ae29361851014b5c386
casse <devnull@localhost>
2008-04-22 14:22:01 +0000

    Added GPL header. Converted to new registration system.

--- include/otawa/ipet/VarAssignment.h
+++ include/otawa/ipet/VarAssignment.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2005-06, IRIT UPS.
+ *	VarAssignment class interface
  *
- *	VarAssignment class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-08, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_IPET_VARASSIGNMENT_H
 #define OTAWA_IPET_VARASSIGNMENT_H
@@ -12,12 +27,13 @@
 
 namespace otawa {
 
+// pre-declaration
 namespace ilp { class System; }
 
 namespace ipet {
 
 // VarAsignment class
-class VarAssignment: public BBProcessor {
+class VarAssignment: public Registered<VarAssignment, BBProcessor> {
 	bool _explicit, _recursive;
 	ilp::System *sys;
 	String makeNodeVar(BasicBlock *bb, CFG *cfg);
@@ -30,6 +46,7 @@ protected:
 public:
 	VarAssignment(void);
 	virtual void configure(const PropList& props);
+	static void init(void);
 };
 
 // Features
