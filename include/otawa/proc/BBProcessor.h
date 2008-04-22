a0d63511591bf2a88f81c1d2f4ed9339c890b2b5
casse <devnull@localhost>
2008-04-22 14:23:00 +0000

    Added GPL header. Converted to new registration system.

--- include/otawa/proc/BBProcessor.h
+++ include/otawa/proc/BBProcessor.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2005-06, IRIT UPS.
+ *	BBProcessor class interface
  *
- *	otawa/proc/BBProcessor.h -- BBProcessor class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-08, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_PROC_BBPROCESSOR_H
 #define OTAWA_PROC_BBPROCESSOR_H
@@ -12,6 +27,8 @@
 
 namespace otawa {
 
+using namespace elm;
+
 class BBProcessor: public CFGProcessor {
 protected:
 	virtual void processCFG(WorkSpace *fw, CFG *cfg);
@@ -19,7 +36,10 @@ protected:
 	
 public:
 	BBProcessor(void);
-	BBProcessor(elm::String name, elm::Version version = elm::Version::ZERO);
+	inline BBProcessor(AbstractRegistration& reg): CFGProcessor(reg) { }
+	inline BBProcessor(cstring name, const Version& version, AbstractRegistration& reg)
+		: CFGProcessor(name, version, reg) { }
+	BBProcessor(cstring name, elm::Version version = elm::Version::ZERO);
 };
 
 } // otawa
