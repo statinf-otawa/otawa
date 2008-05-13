40c5993b62f15ca344f85f7b048886c4ef3c609d
ballabri <devnull@localhost>
2008-05-13 09:27:49 +0000

    added some comments

--- src/prog/cache_CAT2NCBuilder.cpp
+++ src/prog/cache_CAT2NCBuilder.cpp
@@ -9,7 +9,30 @@
 
 namespace otawa {
 	
-
+/**
+ * @class CAT2NCBuilder
+ *
+ * This processor produces categorization information for each l-block.
+ * It is essentially the same processor as CAT2Builder, except it differentiates the ALWAYS_MISS
+ * and NOT_CLASSIFIED categories by using the MAY ACS.
+ *
+ * @par Configuration
+ * none
+ *
+ * @par Required features
+ * @li @ref DOMINANCE_FEATURE
+ * @li @ref LOOP_HEADERS_FEATURE
+ * @li @ref LOOP_INFO_FEATURE
+ * @li @ref COLLECTED_LBLOCKS_FEATURE
+ * @li @ref ICACHE_ACS_FEATURE
+ * @li @ref ICACHE_FIRSTLAST_FEATURE
+ *
+ * @par Provided features
+ * @li @ref ICACHE_CATEGORY2_FEATURE
+ * 
+ * @par Statistics
+ * none
+ */
 CAT2NCBuilder::CAT2NCBuilder(void) : CAT2Builder() {
 	require(DOMINANCE_FEATURE);
 	require(LOOP_HEADERS_FEATURE);
