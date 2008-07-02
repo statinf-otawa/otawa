c5f08ef78daa33a6557242572f2e432df6859886
casse <devnull@localhost>
2008-07-02 20:05:18 +0000

    Fixed documentation.

--- src/mkff/mkff.cpp
+++ src/mkff/mkff.cpp
@@ -50,7 +50,7 @@ const char *nocall_labels[] = {
 
 /**
  * @page mkff mkff Command
- * @ingroup f4
+ * @ingroup ff
  * 
  * This command is used to generate F4 file template file template (usually
  * suffixed by a @e .ff) to pass flow facts
