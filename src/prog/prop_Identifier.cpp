5919f900a404e4379e8d6e7cc4b872ebe68c8a63
casse <devnull@localhost>
2008-07-07 11:19:58 +0000

    Added find.

--- src/prog/prop_Identifier.cpp
+++ src/prog/prop_Identifier.cpp
@@ -1,8 +1,20 @@
 /*
- *	$Id$
- *	Copyright (c) 2003, IRIT UPS.
+ * ADTREE -- OTAWA plugin for Eclipse
+ * Copyright (C) 2003-08  IRIT - UPS <casse@irit.fr>
  *
- *	prog/prop_PropList.cpp -- implementation of Identifier class.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.package ojawa;
  */
 
 #include <elm/io.h>
@@ -51,6 +63,16 @@ DuplicateIdentifierException::DuplicateIdentifierException(String& name)
  * Identifier pointers may be compared for testing equality.
  * @ingroup prop
  */
+
+
+/**
+ * Find an abstract identifier by its name.
+ * @param name	Name of the looked identifier.
+ * @return	Found identifier or null.
+ */
+AbstractIdentifier *AbstractIdentifier::find(const string& name) {
+	return NS.get(name);
+}
 
 
 /**
