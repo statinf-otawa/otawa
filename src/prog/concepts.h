2c2d43075b18f3a7db9856bf9b71e6a35459ae20
casse <devnull@localhost>
2008-07-21 08:38:12 +0000

    Added dfa::IterativeDFAProblem concept.

--- src/prog/concepts.h
+++ src/prog/concepts.h
@@ -103,4 +103,87 @@ public:
 	Vertex exit(void);
 };
 
-} } // otawa::concept
+} // concept
+
+namespace dfa {
+
+/** Concept used to implements @ref IterativeDFA problems. */
+class IterativeDFAProblem {
+public:
+
+	/** Type of the set of the problem. */
+	typedef struct Set Set;
+	
+	/**
+	 * Build a new empty set (set constructor).
+	 * @eturn	New empty set.
+	 */
+	Set *empty(void);
+
+	/**
+	 * Build the generating set for the given basic block.
+	 * @param bb	Basic block to get generating set from.
+	 * @return		Generating set.
+	 */	
+	Set *gen(BasicBlock *bb);
+	
+	/**
+	 * Build the killing set for the given basic block.
+	 * @param bb	Basic block to get killing set from.
+	 * @return		killing set.
+	 */	
+	Set *kill(BasicBlock *bb);
+
+	/**
+	 * Test if two sets are equals.
+	 * @param set1	First set to compare.
+	 * @param set2	Second set to compare.
+	 * @return	True if they are equals, false else.
+	 */
+	bool equals(Set *set1, Set *set2);
+
+	/**
+	 * Set to empty the given set. This operation is mainly called to allow
+	 * re-use of working set.
+	 * @param set	Set to reset.
+	 */
+	void reset(Set *set);
+	
+	/**
+	 * Merge two sets, as after a selection or at the entry of a loop.
+	 * @param set1	First set to merge and result of the merge.
+	 * @param set2	Second set to merge.
+	 */
+	void merge(Set *set1, Set *set2);
+	
+	/**
+	 * Assign a set to another one.
+	 * @param dset	Destination set.
+	 * @param tset	Source set.
+	 */
+	void set(Set *dset, Set *tset);
+	
+	/**
+	 * Perform union of a set in another one (generate action).
+	 * @param dset	First source and destination set.
+	 * @param tset	Second source set.
+	 */
+	void add(Set *dset, Set *tset);
+	
+	/**
+	 * Makes the difference between two sets (kill action).
+	 * @param dset	Destination and first operand set.
+	 * @param tset	Second operand set.
+	 */
+	void diff(Set *dset, Set *tset);
+	
+	/**
+	 * Free the ressources used by the given set.
+	 * @param set	Set to remove.
+	 */
+	void free(Set *set);
+};
+
+}	// dfa
+
+}	// otawa
