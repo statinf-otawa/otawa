1ff012dd5fdbb47a8c980bec75f41a5d4aed5f38
casse <devnull@localhost>
2008-07-22 08:56:48 +0000

    Removed getDeep().

--- include/otawa/prop/PropList.h
+++ include/otawa/prop/PropList.h
@@ -1,8 +1,23 @@
 /*
  *	$Id$
- *	Copyright (c) 2003, IRIT UPS.
+ *	PropList class interface
  *
- *	otawa/prop/PropList.h -- interface to PropList class.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2003-8, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_PROP_PROPLIST_H
 #define OTAWA_PROP_PROPLIST_H
@@ -22,8 +37,6 @@ extern const AbstractIdentifier END;
 class PropList {
 	mutable Property *head;
 	void init(const AbstractIdentifier *id, elm::VarArg& args);
-protected:
-	virtual Property *getDeep(const AbstractIdentifier *id) const;
 public:
 	static const PropList EMPTY;
 	inline PropList(const PropList& props) { addProps(props); };
