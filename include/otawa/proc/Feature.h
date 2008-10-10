ccfa6ce55d8bcae8b0e3667cbdf56374e4518984
casse <devnull@localhost>
2008-10-10 05:56:38 +0000

    Added SilentFeature to avoid to include the default builder.

--- include/otawa/proc/Feature.h
+++ include/otawa/proc/Feature.h
@@ -23,11 +23,7 @@
 #define OTAWA_PROC_FEATURE_H
 
 #include <elm/string.h>
-#include <elm/util/Cleaner.h>
-#include <elm/genstruct/DAGNode.h>
-#include <otawa/prop/PropList.h>
-#include <otawa/prop/Identifier.h>
-#include <otawa/proc/Processor.h>
+#include <otawa/proc/AbstractFeature.h>
 
 namespace otawa {
 	
@@ -37,28 +33,15 @@ class WorkSpace;
 
 class FeatureDependency;
 	
-// AbstractFeature class
-class AbstractFeature: public Identifier<Processor *> {
-public:
-	AbstractFeature(CString name = "");
-	inline void incUseCount();
-	inline void decUseCount();
-	virtual void process(WorkSpace *fw,
-		const PropList& props = PropList::EMPTY) const = 0;
-	virtual void check(WorkSpace *fw) const = 0;
-	virtual void clean(WorkSpace *ws) const = 0;
-};
-
-
-// default_checker_t structure
-typedef struct default_checker_t {
+// default_handler_t structure
+typedef struct default_handler_t {
 	static inline void check(WorkSpace *fw) { }
 	static inline void clean(WorkSpace *ws) { };
-} default_checker_t;
+} default_handler_t;
 
 
 // Feature class
-template <class T, class C = default_checker_t>
+template <class T, class C = default_handler_t>
 class Feature: public AbstractFeature {
 public:
 	Feature(CString name = "");
@@ -69,6 +52,7 @@ public:
 	virtual void check(WorkSpace *fw) const { C::check(fw); }
 	virtual void clean(WorkSpace *ws) const { C::clean(ws); }
 };
+
 
 // Inline
 template <class T, class C>
@@ -85,69 +69,6 @@ void Feature<T, C>::process(WorkSpace *fw, const PropList& props) const {
 		proc.process(fw, props);
 	}
 }
-
-
-// FeatureDependency class
-class FeatureDependency: public elm::CleanList {
-	int refcount;
-	bool invalidated;
-	const AbstractFeature *feature;
-	
-public:
-	genstruct::DAGNode<FeatureDependency*> *graph;
-	
-	inline FeatureDependency(const AbstractFeature *_feature);
-	inline ~FeatureDependency();
-	inline void addChild(FeatureDependency *fdep);
-	inline void removeChild(FeatureDependency *fdep);
-	inline bool isInUse();
-	inline bool isInvalidated();
-	inline void setInvalidated(bool inv);
-	inline const AbstractFeature *getFeature() const;
-};
-
-inline FeatureDependency::FeatureDependency(const AbstractFeature *_feature)
-	: refcount(0), invalidated(false), feature(_feature), graph(new genstruct::DAGNode<FeatureDependency*>(this))
-	{ }
-
-inline FeatureDependency::~FeatureDependency() {
-	delete graph;
-}
-
-inline const AbstractFeature *FeatureDependency::getFeature() const {
-	return feature;
-}
-
-inline void FeatureDependency::addChild(FeatureDependency *fdep)  {	
-	graph->addChild(fdep->graph);
-	fdep->refcount++;
-}
-
-inline void FeatureDependency::removeChild(FeatureDependency *fdep)  {
-	fdep->refcount--;
-	graph->removeChild(fdep->graph);
-	ASSERT(fdep->refcount >= 0);
-	if (fdep->refcount == 0) {
-		delete fdep;
-	}
-}
-inline bool FeatureDependency::isInUse() {
-	return (refcount > 0);
-}
-
-inline bool FeatureDependency::isInvalidated() {
-	return invalidated;
-}
-
-inline void FeatureDependency::setInvalidated(bool inv) {
-	invalidated = inv;
-	if (invalidated && (refcount == 0))
-		delete this;
-}
-
-
-// Feature
-extern Feature<NoProcessor> NULL_FEATURE;
 
 } // otawa
 
