e12a84389793fa605feb4f1e1af20a8b85114713
casse <devnull@localhost>
2008-10-13 20:46:17 +0000

    Added concept InstBlock and conversion of BasicBlock and pfg::BB to this concept.

--- src/prog/ipet_TrivialDataCacheManager.cpp
+++ src/prog/ipet_TrivialDataCacheManager.cpp
@@ -65,7 +65,7 @@ BasicBlock *bb) {
 	
 	// Count the memory accesses
 	int count = 0;
-	for(BasicBlock::InstIterator inst(bb); inst; inst++)
+	for(BasicBlock::InstIter inst(bb); inst; inst++)
 		if(inst->isMem())
 			count++;
 	
