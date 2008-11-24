20da40c0c69146efb1107d7515c72fa706f7a585
rochange <devnull@localhost>
2008-11-24 07:03:49 +0000

    Included last version of the simulator.

--- include/otawa/gensim/PipelineStage.h
+++ include/otawa/gensim/PipelineStage.h
@@ -1,94 +1,85 @@
 /*
  *	$Id$
- *	Copyright (c) 2006, IRIT-UPS <casse@irit.fr>.
+ *	gensim module interface
  *
- *	otawa/gensim/PipelineStage.h -- PipelineStage class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2006-08, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef OTAWA_GENSIM_PIPELINE_STAGE_H
 #define OTAWA_GENSIM_PIPELINE_STAGE_H
 
 #include <systemc.h>
 #include <elm/genstruct/SLList.h>
-#include <otawa/gensim/InstructionQueue.h>
-#include <otawa/gensim/SimulatedInstruction.h>
+#include "InstructionQueue.h"
+#include "SimulatedInstruction.h"
+#include <otawa/util/Trace.h>
+#include "SimulationStats.h"
 #include <elm/string/String.h>
 
-namespace otawa { namespace gensim {
+namespace otawa {
+namespace gensim {
 
-typedef enum {	FETCH, 
-				EXECUTE_IN_ORDER, 
-				EXECUTE_OUT_OF_ORDER, 
-				COMMIT, 
-				LAZYIQIQ} pipeline_stage_t;
+  typedef enum {FETCH,
+		EXECUTE_IN_ORDER,
+		EXECUTE_OUT_OF_ORDER,
+		COMMIT,
+		LAZYIQIQ, DECOMP} pipeline_stage_t;
 
-class PipelineStageConfiguration {
-	pipeline_stage_t stage_type;
-	InstructionQueueConfiguration * input_queue;
-	InstructionQueueConfiguration * output_queue;
-	InstructionQueueConfiguration * instruction_buffer;
-	CString stage_name;
-	int stage_width, in_stage_width, out_stage_width;
-	public:
-		PipelineStageConfiguration(CString name, pipeline_stage_t type,
-			InstructionQueueConfiguration * inqueue, InstructionQueueConfiguration * outqueue,
-			int in_width, int out_width);
-		PipelineStageConfiguration(CString name, pipeline_stage_t type,
-			InstructionQueueConfiguration * inqueue, InstructionQueueConfiguration * outqueue,
-			int width);
-		PipelineStageConfiguration(CString name, pipeline_stage_t type,
-			InstructionQueueConfiguration * buffer, int width);
-		
-		
-		pipeline_stage_t type();
-		CString name();
-		InstructionQueueConfiguration * inputQueue();
-		InstructionQueueConfiguration * outputQueue();
-		InstructionQueueConfiguration * instructionBuffer();
-		int width();
-		int inWidth();
-		int outWidth();
-		void dump(elm::io::Output& out_stream);
-};
+  class PipelineStageConfiguration {
+    pipeline_stage_t stage_type;
+    InstructionQueueConfiguration * input_queue;
+    InstructionQueueConfiguration * output_queue;
+    InstructionQueueConfiguration * instruction_buffer;
+    CString stage_name;
+    int stage_width, in_stage_width, out_stage_width;
+  public:
+    PipelineStageConfiguration(CString name, pipeline_stage_t type,
+			       InstructionQueueConfiguration * inqueue,
+			       InstructionQueueConfiguration * outqueue, int in_width,
+			       int out_width);
+    PipelineStageConfiguration(CString name, pipeline_stage_t type,
+			       InstructionQueueConfiguration * inqueue,
+			       InstructionQueueConfiguration * outqueue, int width);
+    PipelineStageConfiguration(CString name, pipeline_stage_t type,
+			       InstructionQueueConfiguration * buffer, int width);
 
-class PipelineStage : public sc_module {
-	public:
-		PipelineStage(sc_module_name name) {};
-		bool isEmpty() {
-			return true; 	// by default, pipeline stages do not retain instructions
-		}
-};
+    pipeline_stage_t type();
+    CString name();
+    InstructionQueueConfiguration * inputQueue();
+    InstructionQueueConfiguration * outputQueue();
+    InstructionQueueConfiguration * instructionBuffer();
+    int width();
+    int inWidth();
+    int outWidth();
+    void dump(elm::io::Output& out_stream);
+  };
 
+  class PipelineStage: public sc_module {
+  public:
+    PipelineStage(sc_module_name name) {
+    }
+    ;
+    bool isEmpty() {
+      return true; // by default, pipeline stages do not retain instructions
+    }
+  };
 
-class LazyStageIQIQ : public PipelineStage {
-	public:
-		// signals
-		sc_in<bool> in_clock;
-		sc_in<SimulatedInstruction *> * in_instruction;
-		sc_in<int> in_number_of_ins;
-		sc_out<int> out_number_of_accepted_ins;
-		sc_out<SimulatedInstruction *> * out_instruction;
-		sc_out<int> out_number_of_outs;
-		sc_in<int> in_number_of_accepted_outs;
-		
-	private:
-		// variables
-		int stage_width;
-		elm::genstruct::SLList<SimulatedInstruction *> leaving_instructions;
-		
-	public:
-		LazyStageIQIQ(sc_module_name name, int width);
-		inline int stageWidth();
-		
-		SC_HAS_PROCESS(LazyStageIQIQ);
-		void action();
-};
-
-inline int LazyStageIQIQ::stageWidth() {
-	return stage_width;
 }
-
-} } // otawa::gensim
+} // otawa::gensim
 
 #endif // OTAWA_GENSIM_PIPELINE_STAGE_H
-
 
