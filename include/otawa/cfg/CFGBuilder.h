9aac8ea03faf994a87e0fb19790a3858854807e0
casse <devnull@localhost>
2008-12-05 20:35:15 +0000

    cfg module re-organization in otawa/cfg/features.h.

--- include/otawa/cfg/CFGBuilder.h
+++ include/otawa/cfg/CFGBuilder.h
@@ -1,15 +1,30 @@
 /*
  *	$Id$
- *	Copyright (c) 2005, IRIT UPS.
+ *	CFGBuilder processor interface
  *
- *	otawa/cfg/CFGBuilder.h -- CFGBuilder class interface.
+ *	This file is part of OTAWA
+ *	Copyright (c) 2005-08, IRIT UPS.
+ * 
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software 
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef OTAWA_CFG_CFG_BUILDER_H
 #define OTAWA_CFG_CFG_BUILDER_H
 
 #include <elm/genstruct/Vector.h>
-#include <otawa/proc/Feature.h>
+#include <otawa/cfg/features.h>
 #include <otawa/proc/Processor.h>
 #include <otawa/cfg/CFGInfo.h>
 
@@ -33,9 +48,6 @@ public:
 	virtual void processWorkSpace(WorkSpace *fw);
 	virtual void configure(const PropList& props = PropList::EMPTY);
 };
-
-// Features
-extern Feature<CFGBuilder> CFG_INFO_FEATURE;
 
 } // otawa
 
