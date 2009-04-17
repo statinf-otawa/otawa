a1bdfb8e9fd7f8d5644d9238770c9a969cb36694
casse <devnull@localhost>
2009-04-17 20:34:18 +0000

    Added multi-output to constraint system.

--- src/oipet/oipet.cpp
+++ src/oipet/oipet.cpp
@@ -4,7 +4,7 @@
  *
  *	This file is part of OTAWA
  *	Copyright (c) 2006-07, IRIT UPS.
- * 
+ *
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation; either version 2 of the License, or
@@ -16,14 +16,15 @@
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with OTAWA; if not, write to the Free Software 
+ *	along with OTAWA; if not, write to the Free Software
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <stdlib.h>
 #include <elm/io.h>
-#include <elm/system/System.h>
+#include <elm/io/OutFileStream.h>
 #include <elm/options.h>
+#include <elm/io/OutFileStream.h>
 #include <otawa/otawa.h>
 #include <otawa/ipet.h>
 #include <otawa/ilp.h>
@@ -41,6 +42,7 @@
 #include <otawa/exegraph/ParamExeGraphBBTime.h>
 #include <otawa/util/FlowFactLoader.h>
 #include <otawa/util/BBRatioDisplayer.h>
+#include <otawa/display/ILPSystemDisplayer.h>
 
 using namespace elm;
 using namespace elm::option;
@@ -52,140 +54,102 @@ using namespace elm::option;
 
 /**
  * @defgroup oipet oipet Command
- * 
+ *
  * oipet allows to use WCET IPET computation facilities of OTAWA. Currently,
  * you may only choose the algorithm for instruction cache support:
- * 
+ *
  * @li ccg for Cache Conflict Graph from  Li, Malik, Wolfe, "Efficient
  * microarchitecture modelling and path analysis for real-time software",
  * Proceedings of the 16th IEEE Real-Time Systems Symposium, 1995.
- * 
- * @li cat for categorization approach (an adaptation to IPET of Healy, Arnold, 
+ *
+ * @li cat for categorization approach (an adaptation to IPET of Healy, Arnold,
  * Mueller, Whalley, Harmon, "Bounding pipeline and instruction cache performance,"
  * IEEE Trans. Computers, 1999).
- * 
+ *
  * @li cat2 for categorization by abstraction interpretation (Ferdinand,
  * Martin, Wilhelm, "Applying Compiler Techniques to Cache Behavior Prediction.",
  * ACM SIGPLAN Workshop on Language, Compiler and Tool Support for Real-Time
  * Systems, 1997) improved in OTAWA.
- * 
+ *
  * And the algorithm to handle the pipeline:
- * 
+ *
  * @li trivial : consider a scalar processor without pipeline with 5 cycle
  * per instruction,
- * 
+ *
  * @li sim : use a simulator to time the program blocks,
- * 
+ *
  * @li delta : use the simulator and the delta approach to take in account
- * the inter-block effects as in J. Engblom, A. Ermedahl, M. Sjoedin, 
+ * the inter-block effects as in J. Engblom, A. Ermedahl, M. Sjoedin,
  * J. Gustafsson, H. Hansson, "Worst-case execution-time analysis for embedded
  * real-time systems", Journal of Software Tools for Technology Transfer, 2001,
- * 
+ *
  * @li exegraph : use the execution graph to time blocks as in X. Li,
  * A. Roychoudhury, T. Mitra, "Modeling Out-of-Order Processors for Software
  * Timing Analysis", RTSS'04.
- * 
+ *
  * @par Syntax
- * 
+ *
  * @code
  * $ oipet [options] binary_file [function1 function2 ...]
  * @endcode
  * This command compute the WCET of the given function using OTAWA IPET facilities.
  * If no function is given, the main() function is used.
- * 
+ *
  * @par Generic Options
- * 
+ *
  * @li -I, --do-not-inline -- cause to not inline functions for the WCET computation.
  * Consider that this option may save computation time but, conversely,
  * may reduce the WCET accuracy.
+ *
  * @li --ilp @i solver -- select the ILP solver to use (currently lp_solver (V4) or
- * lp_solve5.
- * 
+ * lp_solve5).
+ *
  * @par Cache Management Options
- * 
- * @li -i method, --icache=method -- selects how the instruction must be managed.
- * The method may be one of none, ccg, cat, or cat2.
- * 
+ *
  * @li -c path, --cache=path -- load the cache description from the file whose
  * path is given.
- * 
+ *
  * @li -f path, --flow-facts=path -- use the given flow fact file,
  *
- * @li -p, --pseudounrolling -- enable Pseudo-Unrolling (cat2 only)
+ * @li -i method, --icache=method -- selects how the instruction must be managed.
+ * The method may be one of none, ccg, cat, or cat2.
  *
  * @li -l, --linkedblocks -- enable LinkedBlocksDetector (cat2 only)
  *
+ * @li -p, --pseudounrolling -- enable Pseudo-Unrolling (cat2 only)
+ *
  * @li -P type, --pers=type -- select persistence type for cat2: multi, outer,
  * or inner (default to multilevel).
- * 
+ *
  * @par Pipeline Management Options
- * 
- * @li -t method, --bbtiming=method -- selects the method to time the blocks. The
- * method may be one of trivial, sim, delta, exegraph or paramexegraph.
- * 
- * @li -p path, --processor=path -- load the processor description from the file
- * whose path is given.
- * 
+ *
  * @li -D depth, --delta=depth -- select the depth of the delta algorithm, that is,
  * how many blocks are used to compute inter-blocks effects (default to 4).
  * Bigger is the depth, better is the accuracy but longer is the computing time.
- * 
+ *
+ * @li -p path, --processor=path -- load the processor description from the file
+ * whose path is given.
+ *
+ * @li -t method, --bbtiming=method -- selects the method to time the blocks. The
+ * method may be one of trivial, sim, delta, exegraph or paramexegraph.
+ *
  * @par Dump Options
- * 
- * @li -C, --dump-constraints -- for each function, generate a file named
- * function_name.lp containing the generated ILP system. The generated file
- * use the lp_solve syntax and may feed the lp_solve command.
+ *
+ * @li -C, --dump-constraints=output_kind -- dump the constraints that has been used
+ * to compute the WCET. The output_kind may be "lp" for lp_solve compatible output
+ * (suffixed by ".lp") or "html" for HTML output.
  *
  * @li -G, --dump-graph -- for each function involved in the task, generate a file named
  * function_name.ps containing the graph of the processed functions in DOT
  * file format.
- * 
+ *
  * @li -o, --output prefix -- prepend the given prefix to the dump files names.
+ *
+ * @li -T, --dump-time -- output statistics about time and execution count of BB.
+ *
+ *
  */
 
-// BBRatioDisplayer class
-/*class BBRatioDisplayer: public BBProcessor {
-public:
-	BBRatioDisplayer(void): BBProcessor("BBTimeDisplayer", Version(1, 0, 0)) {
-		require(ipet::WCET_FEATURE);
-		require(ipet::ASSIGNED_VARS_FEATURE);
-	}
-protected:
-	virtual void setup(WorkSpace *ws) {
-		wcet = ipet::WCET(ws);
-		system = ipet::SYSTEM(ws);
-		out << "ADDRESS\t\tSIZE\tTIME\tCOUNT\tRATIO\t\tFUNCTION\n";
-	}
-	
-	virtual void processCFG(WorkSpace *fw, CFG *cfg) {
-		BBProcessor::processCFG(fw, cfg);
-		out << "TOTAL FUNCTION\t\t"
-			<< SUM(cfg) << '\t'
-			<< (int)system->valueOf(ipet::VAR(cfg->entry())) << '\t'
-			<< (float)SUM(cfg) * 100 / wcet << "%\t\t"
-			<< cfg->label() << io::endl; 		
-	}
-	
-	virtual void processBB(WorkSpace *fw, CFG *cfg, BasicBlock *bb) {
-		if(bb->isEnd())
-			return;
-		int count = (int)system->valueOf(ipet::VAR(bb)),
-			time = ipet::TIME(bb),
-			total = time * count;
-		SUM(cfg) = SUM(cfg) + total;
-		out << bb->address() << '\t'
-			<< bb->size() << '\t'
-			<< time << '\t'
-			<< count << '\t'
-			<< (float)total * 100 / wcet << "%\t"
-			<< cfg->label() << io::endl; 
-	}
-private:
-	static otawa::Identifier<int> SUM;
-	int wcet;
-	ilp::System *system;
-};
-otawa::Identifier<int> BBRatioDisplayer::SUM("", 0);*/
 
 // Options
 class Command: public elm::option::Manager {
@@ -200,7 +164,7 @@ public:
 	Command(void);
 	void compute(String fun);
 	void run(void);
-	
+
 	// Manager overload
 	virtual void process (String arg);
 };
@@ -264,11 +228,22 @@ IntOption delta(command, 'D', "delta", "use delta method with given sequence len
 
 
 // Dump options
-BoolOption dump_constraints(command, 'C', "dump-constraints",
-	"dump lp_solve constraints", false);
+typedef enum {
+	NO_OUTPUT,
+	LP,
+	HTML
+} const_output_t;
+EnumOption<int>::value_t cons_output_kinds[] = {
+		{ "output kind", NO_OUTPUT },
+		{ "lp", LP },
+		{ "html", HTML },
+		{ "" }
+};
+EnumOption<int> dump_constraints(command, 'C', "dump-constraints",
+	"dump lp_solve constraints", cons_output_kinds);
 BoolOption dump_graph(command, 'G', "dump-graph",
 	"dump DOT graph of the processed function", false);
-BoolOption dump_ratio(command, 'R', "dump-ratios",
+BoolOption dump_time(command, 'T', "dump-time",
 	"dump ratio of each BB for the WCET", false);
 
 
@@ -310,14 +285,14 @@ void Command::process (String arg) {
  * Compute the WCET for the given function.
  */
 void Command::compute(String fun) {
-	
+
 	// Inlining required for CCG
 	if(not_inlining && icache_option == icache_ccg) {
 		not_inlining.set(false);
 		cerr << "WARNING: using CCG without inlining may induce, in some cases, "
 			    "an invalid WCET: inlining is activated.\n";
 	}
-	
+
 	// Prepare processor configuration
 	PropList props;
 	TASK_ENTRY(props) = fun.toCString();
@@ -331,13 +306,13 @@ void Command::compute(String fun) {
 		ipet::ILP_PLUGIN_NAME(props) = ilp_plugin.value().toCString();
 	if(flow_facts)
 		FLOW_FACTS_PATH(props) = Path(flow_facts);
-	
+
 	// Virtualization
 	if(!not_inlining) {
 		Virtualizer virt;
 		virt.process(fw, props);
 	}
-	
+
 	// Compute BB times
 	switch(bbtime_option) {
 
@@ -348,42 +323,42 @@ void Command::compute(String fun) {
 			bbts.process(fw, props);
 		}
 		break;
-		
+
 	case bbtime_exegraph: {
 			LiExeGraphBBTime tbt;
 			tbt.process(fw, props);
 		}
 		break;
-		
+
 	case bbtime_trivial: {
 			TrivialBBTime tbt;
 			ipet::PIPELINE_DEPTH(props) = 5;
 			tbt.process(fw, props);
 		}
 		break;
-	
+
 	case bbtime_paramexegraph: {
 			ParamExeGraphBBTime tbt;
 			tbt.process(fw, props);
 		}
-		break;		
+		break;
 
 	default:
 		assert(0);
 	};
-		
+
 	// Trivial data cache
 	TrivialDataCacheManager dcache;
 	dcache.process(fw, props);
-		
+
 	// Assign variables
 	VarAssignment assign;
 	assign.process(fw, props);
-		
+
 	// Build the system
 	BasicConstraintsBuilder builder;
 	builder.process(fw, props);
-		
+
 	// Process the instruction cache
 	switch(icache_option) {
 	case icache_ccg:
@@ -391,34 +366,34 @@ void Command::compute(String fun) {
 			// build LBlock
 			LBlockBuilder lbb;
 			lbb.process(fw, props);
-			
+
 			// build ccg graph
 			CCGBuilder ccgbuilder;
 			ccgbuilder.process(fw, props);
-			
+
 			// Build ccg contraint
 			CCGConstraintBuilder decomp;
 			decomp.process(fw, props);
 		}
 		break;
-		
+
 	case icache_cat:
 		{
 			// build Cat lblocks
 			CATBuilder catbuilder;
 			catbuilder.process(fw, props);
-			
+
 			// Build CAT contraint
 			CATConstraintBuilder decomp;
 			decomp.process(fw, props);
 		}
 		break;
-	
+
 	case icache_cat2:
 		{
 			// build Cat lblocks
 			FIRSTMISS_LEVEL(props) = (fmlevel_t) (int) pers_option;
-			PSEUDO_UNROLLING(props) = pseudounrolling;			
+			PSEUDO_UNROLLING(props) = pseudounrolling;
 			CAT2NCBuilder cat2builder;
 			cat2builder.process(fw, props);
 
@@ -427,16 +402,16 @@ void Command::compute(String fun) {
 	                        LinkedBlocksDetector lbd;
 	                        lbd.process(fw, props);
 			}
-			
+
 			// Build CAT contraint
 			CAT2ConstraintBuilder decomp;
 			decomp.process(fw, props);
 		}
 	}
-	
+
 	// Build the object function to maximize
 	BasicObjectFunctionBuilder fun_builder;
-	fun_builder.process(fw, props);	
+	fun_builder.process(fw, props);
 
 	// Delta processing
 	if(bbtime_option == bbtime_delta) {
@@ -445,47 +420,51 @@ void Command::compute(String fun) {
 		delta.process(fw, props);
 	}
 
-	// Load flow facts
-	/*ipet::FlowFactLoader loader;
-	loader.process(fw, props);*/
-		
 	// Resolve the system
 	WCETComputation wcomp;
 	wcomp.process(fw, props);
 
 	// Get the result
-	ilp::System *sys = SYSTEM(fw); 
+	ilp::System *sys = SYSTEM(fw);
 	//vcfg.use<ilp::System *>(SYSTEM);
 	cout << "WCET [" << file << ":" << fun << "] = "
 		 << WCET(fw) << io::endl;
-	
+
 	// Dump the ILP system
-	if(dump_constraints) {
-		String out_file = _ << output_prefix.value() << fun << ".lp";
-		try {
-			io::OutStream *stream = elm::system::System::createFile(&out_file);
-			sys->dump(*stream);
+	switch(dump_constraints) {
+	case NO_OUTPUT:
+		break;
+	case LP: {
+			String out_file = _ << output_prefix.value() << fun << ".lp";
+			io::OutFileStream stream(&out_file);
+			if(!stream.isReady())
+				throw MessageException(_ << "cannot create file \"" << out_file << "\".");
+			sys->dump(stream);
 		}
-		catch(elm::system::SystemException& exn) {
-			throw MessageException(_ << "cannot create file \"" << out_file
-				<< "\": " << exn.message());
+		break;
+	case HTML: {
+			system::Path path(_ << output_prefix.value() << fun << ".html");
+			display::ILPSystemDisplayer::PATH(props) = path;
+			display::ILPSystemDisplayer disp;
+			disp.process(fw, props);
 		}
+		break;
 	}
-	
+
 	// Dump the CFG
 	if(dump_graph) {
-		
+
 		// Record results
 		WCETCountRecorder recorder;
 		recorder.process(fw, props);
-		
+
 		// Generates output
 		display::CFGOutput output;
 		output.process(fw, props);
 	}
-	
+
 	// Dump the ratio
-	if(dump_ratio) {
+	if(dump_time) {
 		BBRatioDisplayer displayer;
 		displayer.process(fw, props);
 	}
@@ -496,7 +475,7 @@ void Command::compute(String fun) {
  * Launch the work.
  */
 void Command::run(void) {
-	
+
 	// Any file
 	if(!file)
 		throw OptionException("binary file path required !");
@@ -514,7 +493,7 @@ void Command::run(void) {
 		CACHE_CONFIG_PATH(props) = elm::system::Path(cache);
 	otawa::Processor::VERBOSE(props) = verbose;
 	fw = manager.load(&file, props);
-	
+
 	// Instruction cache preparation
 	if(icache_option == icache_def) {
 		if(fw->platform()->cache().instCache())
@@ -522,7 +501,7 @@ void Command::run(void) {
 		else
 			icache_option = icache_none;
 	}
-	
+
 	// Now process the functions
 	if(!funs)
 		compute("main");
