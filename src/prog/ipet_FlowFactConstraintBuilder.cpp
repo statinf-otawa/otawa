b6c1b5e30c916ffc085fa6078a231bf74f1731c5
casse <devnull@localhost>
2009-04-17 20:06:13 +0000

    Fixed constraint labelling and added full support for totalcount.

--- src/prog/ipet_FlowFactConstraintBuilder.cpp
+++ src/prog/ipet_FlowFactConstraintBuilder.cpp
@@ -4,7 +4,7 @@
  *
  *	This file is part of OTAWA
  *	Copyright (c) 2005-08, IRIT UPS.
- * 
+ *
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation; either version 2 of the License, or
@@ -16,7 +16,7 @@
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with OTAWA; if not, write to the Free Software 
+ *	along with OTAWA; if not, write to the Free Software
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
@@ -41,16 +41,16 @@ namespace otawa { namespace ipet {
  * @class FlowFactConstraintBuilder
  * This processor allows using extern flow facts in an IPET system.
  * Uses the LOOP_COUNT properties provided by FlowFactLoader to build constraints
- * 
+ *
  * @par Configuration
  * @li @ref FLOW_FACTS_PATH
- * 
+ *
  * @par Required Features
  * @li @ref ipet::ILP_SYSTEM_FEATURE
  * @li @ref ipet::COLLECTED_CFG_FEATURE
  * @li @ref ipet::FLOW_FACTS_FEATURE
  * @li @ref LOOP_HEADERS_FEATURE
- * 
+ *
  * @par Provided Features
  * @li @ref ipet::FLOW_FACTS_CONSTRAINTS_FEATURE
  */
@@ -89,7 +89,7 @@ void FlowFactConstraintBuilder::processBB(WorkSpace *ws, CFG *cfg, BasicBlock *b
 			total = ContextualLoopBound::undefined;
 		if(isVerbose())
 			log << "\t\tlooking bound for " << bb << io::endl;
-		
+
 		// Test contextual
 		if(bound == ContextualLoopBound::undefined) {
 			ContextualLoopBound *cbound = CONTEXTUAL_LOOP_BOUND(bb);
@@ -102,33 +102,36 @@ void FlowFactConstraintBuilder::processBB(WorkSpace *ws, CFG *cfg, BasicBlock *b
 					log << "\t\tmax = " << bound << ", total = " << total << io::endl;
 			}
 		}
-		
+
 		// Look simple bound
 		if(bound == ContextualLoopBound::undefined)
 			bound = LOOP_COUNT(bb);
-		
+
 		// Generate the constraint
 		if(bound == ContextualLoopBound::undefined
 		&& total == ContextualLoopBound::undefined)
 			warn(_ << "no flow fact constraint for loop at " << bb->address());
 		else  {
-			
-			
-			
+
+
+
 			// sum{(i,h) / h dom i} eih <= count * sum{(i, h) / not h dom x} xeih
 			if(bound != ContextualLoopBound::undefined) {
-				
-				
+
+
 				ASSERT(bound >= 0);
-				
-				/* Substract unrolling from loop bound */
+
+				// Substract unrolling from loop bound
 				for (BasicBlock *bb2 = UNROLLED_FROM(bb); bb2; bb2 = UNROLLED_FROM(bb2))
 					bound--;
 
-				/* Set execution count to 0 for each unrolled iteration that cannot be taken */	
+				// Set execution count to 0 for each unrolled iteration that cannot be taken
 				if (bound < 0) {
 					BasicBlock *bb2 = bb;
-					otawa::ilp::Constraint *cons0 = system->newConstraint(otawa::ilp::Constraint::EQ);
+					string label;
+					if(_explicit)
+						label = _ << "unrolled loop constraint for BB" << INDEX(bb) << "/" << cfg->label();
+					otawa::ilp::Constraint *cons0 = system->newConstraint(label, otawa::ilp::Constraint::EQ);
 					for (int i = 0; i < (-bound); i++) {
 						for (BasicBlock::InIterator edge(bb); edge; edge++) {
 							ASSERT(edge->source());
@@ -138,12 +141,12 @@ void FlowFactConstraintBuilder::processBB(WorkSpace *ws, CFG *cfg, BasicBlock *b
 						bb2 = UNROLLED_FROM(bb2);
 					}
 				}
-				
+
 				// generate the constraint
 				string label;
 				if(_explicit)
 					label = _ << "loop constraint on BB" << INDEX(bb) << "/" << cfg->label();
-				otawa::ilp::Constraint *cons = system->newConstraint(label, otawa::ilp::Constraint::LE);	
+				otawa::ilp::Constraint *cons = system->newConstraint(label, otawa::ilp::Constraint::LE);
 				for(BasicBlock::InIterator edge(bb); edge; edge++) {
 					ASSERT(edge->source());
 					otawa::ilp::Var *var = VAR(edge);
@@ -154,25 +157,50 @@ void FlowFactConstraintBuilder::processBB(WorkSpace *ws, CFG *cfg, BasicBlock *b
 				}
 
 			}
-			
-			// eih <= total
-			/*if(total != ContextualLoopBound::undefined) {
-				otawa::ilp::Constraint *cons = system->newConstraint(otawa::ilp::Constraint::LE);
-				
-				BasicBlock *bb2 = bb;
-				do {
-					for(BasicBlock::InIterator edge(bb2); edge; edge++) {
-						ASSERT(edge->source());
-						otawa::ilp::Var *var = VAR(edge);
-						if(Dominance::dominates(bb2, edge->source()) || UNROLLED_FROM(bb2))
-							cons->addLeft(1, var);
-					}
-					bb2 = UNROLLED_FROM(bb2);				
-				} while (bb2 != NULL);
+
+			/* basic constraint
+			 * sum {h dom i} eih + sum {u in unrolled(h))} eiu <= total
+			 * ensure if enty constraint
+			 * sum {h dom i} eih <= total * sum {i dom h} eih + (total - 1) sum {u in unrolled(h))} eiu) */
+			if(total != ContextualLoopBound::undefined) {
+
+				// build constraints
+				string label;
+				if(_explicit)
+					label = _ << "total loop constraint for BB" << INDEX(bb) << "/" << cfg->label();
+				otawa::ilp::Constraint *cons = system->newConstraint(label, otawa::ilp::Constraint::LE);
+				label = "";
+				if(_explicit)
+					label = _ << "0-execution for total loop constraint for BB" << INDEX(bb) << "/" << cfg->label();
+				otawa::ilp::Constraint *zero = system->newConstraint(label, otawa::ilp::Constraint::LE);
+
+				// 0 <= total
+				// 0 <= 0
 				cons->addRight(total);
-			}*/
+
+				// sum {h dom i} eih <= total
+				// sum {h dom i} eih <= total * sum {i dom h} eih
+				for(BasicBlock::InIterator edge(bb); edge; edge++) {
+					ASSERT(edge->source());
+					if(Dominance::dominates(bb, edge->source())) {
+						cons->addLeft(1, VAR(edge));
+						zero->addLeft(1, VAR(edge));
+					}
+					else if(Dominance::dominates(edge->source(), bb))
+						zero->addRight(total, VAR(edge));
+				}
+
+				// eih + sum {u in unrolled(h))} eiu <= total
+				// eih + sum {u in unrolled(h))} eiu <= total * sum {i dom h} eih + (total - 1) sum {u in unrolled(h))} eiu)
+				for(BasicBlock *hd = UNROLLED_FROM(bb); hd; hd = UNROLLED_FROM(hd))
+					for(BasicBlock::InIterator edge(hd); edge; edge++)
+						if(Dominance::dominates(edge->source(), hd)) {
+							cons->addLeft(1, VAR(edge));
+							zero->addRight(total - 1, VAR(edge));
+						}
+			}
 		}
-	}	
+	}
 }
 
 
@@ -210,6 +238,6 @@ void FlowFactConstraintBuilder::configure(const PropList& props) {
 Feature<FlowFactConstraintBuilder>
 	FLOW_FACTS_CONSTRAINTS_FEATURE("otawa::ipet::flow_facts_constraints");
 
-} 
+}
 
 } // otawa::ipet
