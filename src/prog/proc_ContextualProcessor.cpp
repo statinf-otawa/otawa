188ada2715791239619404808046c03dc6414293
casse <devnull@localhost>
2009-05-26 11:51:35 +0000

    Fixed leavingCall() to get BB leaving to.

--- src/prog/proc_ContextualProcessor.cpp
+++ src/prog/proc_ContextualProcessor.cpp
@@ -4,7 +4,7 @@
  *
  *	This file is part of OTAWA
  *	Copyright (c) 2008, IRIT UPS.
- * 
+ *
  *	OTAWA is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation; either version 2 of the License, or
@@ -16,7 +16,7 @@
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with OTAWA; if not, write to the Free Software 
+ *	along with OTAWA; if not, write to the Free Software
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
@@ -32,14 +32,14 @@ using namespace elm::genstruct;
  * @class ContextualProcessor
  * A contextual processor allows to process basic block in a call-aware way
  * even with a virtual CFG (where called CFG are inlined).
- * 
+ *
  * First, the basic
  * blocks of the top are processed. When a virtual call is found, enteringCall()
  * method is called and the basic blocks of the called CFG are processed until
  * end of the call (leavingCall() is called at this moment). Then the traversal
  * of the caller CFG restarts. Note that thus behaviour is recursive and that
  * a call in the callee is processed in the same way.
- * 
+ *
  * This processor may be used to build @ref ContextPath.
  *
  * As an example is often better than any discussion, let three functions :
@@ -55,7 +55,7 @@ using namespace elm::genstruct;
  *	function h
  *		basic block h1
  * </code>
- * 
+ *
  * First the function f is traversed: enteringCall(f), processBB(f1) and processBB(f2).
  * As f2 calls h, we get enteringCall(h), processBB(h1), leavingCall(h).
  * We continue with f3: processBB(f3).
@@ -70,7 +70,7 @@ using namespace elm::genstruct;
  * Build a contextual processor.
  * @param name		Name of the implemented actual processor.
  * @param version	Version of the implemented atucal processor.
- * 
+ *
  */
 ContextualProcessor::ContextualProcessor(cstring name, const Version& version)
 :	CFGProcessor(name, version) {
@@ -88,22 +88,26 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 	todo.push(cfg->entry());
 	calls.push(cfg->entry());
 	MARK(cfg->entry()) = calls.top();
-	
+
 	// traverse the BB
 	while(todo) {
 		BasicBlock *bb = todo.pop();
-		
+
 		// is it a return ?
 		if(!bb) {
+
+			// verbosity and control
 			if(isVerbose())
 				log << "\tleaving\n";
-			leavingCall(ws, cfg);
 			if(!calls)
 				throw ProcessorException(*this, _ << "unstructured CFG " + cfg->label());
+
+			// pop call
 			calls.pop();
-			while((bb = returns.pop()))
-				todo.push(bb);
+			BasicBlock *to = returns.pop();
+			ASSERT(to);
 			level--;
+			leavingCall(ws, cfg, to);
 			continue;
 		}
 
@@ -111,22 +115,25 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 		if(isVerbose())
 			log << "\tprocessing " << level << ": BB" << bb->number() << " (" << bb->address() << ")\n";
 		processBB(ws, cfg, bb);
-		
+
 		// look outing edge
 		for(BasicBlock::OutIterator edge(bb); edge; edge++)
 			switch(edge->kind()) {
-			
+
 			case Edge::NONE:
 				ASSERT(false);
-			
+
 			case Edge::CALL:
 				break;
 
-			case Edge::VIRTUAL_RETURN:
-				ASSERT(edge->target());
-				returns.push(edge->target());
+			case Edge::VIRTUAL_RETURN: {
+					BasicBlock *to = edge->target();
+					ASSERT(to);
+					ASSERT(!returns[returns.length() - 1] || returns[returns.length() - 1] == to);
+					returns[returns.length() - 1] = to;
+				}
 				break;
-			
+
 			case Edge::TAKEN:
 			case Edge::NOT_TAKEN:
 				if(edge->target() && MARK(edge->target()) != calls.top()) {
@@ -134,18 +141,21 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 					MARK(edge->target()) = calls.top();
 				}
 				break;
-				
+
 			case Edge::VIRTUAL:
 				ASSERT(edge->target());
 				if(edge->target()->isExit()) {
-					returns.push(edge->target());
+					BasicBlock *to = edge->target();
+					ASSERT(to);
+					ASSERT(!returns[returns.length() - 1] || returns[returns.length() - 1] == to);
+					returns[returns.length() - 1] = to;
 					break;
 				}
 				// !!WORKAROUND!!
-				CALLED_CFG(edge) = cfg;			
+				CALLED_CFG(edge) = cfg;
 
 			case Edge::VIRTUAL_CALL:
-				
+
 				// check recursivity
 				for(int i = 0; i < calls.length(); i++)
 					if(calls[i] == edge->target()) {
@@ -159,7 +169,7 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 						avoidingRecursive(ws, cfg, bb, edge->target());
 						continue;
 					}
-				
+
 				// do the call
 				if(isVerbose()) {
 					log << "\tentering call";
@@ -167,7 +177,7 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 					CFG *cfg = CALLED_CFG(edge);
 					if(cfg)
 						log << " to " << cfg->label();
-					log << io::endl;					
+					log << io::endl;
 				}
 				calls.push(edge->target());
 				todo.push(0);
@@ -178,7 +188,7 @@ void ContextualProcessor::processCFG (WorkSpace *ws, CFG *cfg) {
 				break;
 			}
 	}
-	
+
 	// clean up
 	for(CFG::BBIterator bb(cfg); bb; bb++)
 		MARK(bb).remove();
@@ -205,8 +215,9 @@ void ContextualProcessor::enteringCall(
  * This function is called each time a function return is traversed.
  * @param ws		Current workspace.
  * @param cfg		Current top CFG (not the inlined one).
+ * @param bb		Basic block target of a returning call.
  */
-void ContextualProcessor::leavingCall(WorkSpace *ws, CFG *cfg)
+void ContextualProcessor::leavingCall(WorkSpace *ws, CFG *cfg, BasicBlock *bb)
 {
 }
 
