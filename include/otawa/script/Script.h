24e821bfdb923fe529080642a2081e0f817679f2
casse <devnull@localhost>
2009-06-22 12:20:46 +0000

    Added Script processor : WCET computation by interpretation of scripts.

---
+++ include/otawa/script/Script.h
@@ -0,0 +1,56 @@
+/*
+ *	$Id$
+ *	Script processor interface
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2009, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef OTAWA_SCRIPT_SCRIPT_H_
+#define OTAWA_SCRIPT_SCRIPT_H_
+
+#include <elm/system/Path.h>
+#include <otawa/proc/Processor.h>
+
+namespace elm { namespace xom { class Node; } }
+
+namespace otawa { namespace script {
+
+// Script class
+class Script: public Processor {
+public:
+
+	Script(void);
+	virtual void configure (const PropList &props);
+
+protected:
+	virtual void processWorkSpace(WorkSpace *fw);
+
+private:
+	void onError(xom::Node *node, const string& msg);
+	void onWarning(xom::Node *node, const string& msg);
+	void makeConfig(xom::Element *elem, PropList& props);
+	elm::system::Path path;
+	PropList props;
+};
+
+// script path
+extern Identifier<elm::system::Path> PATH;
+extern Identifier<Pair<string, string> > PARAM;
+
+} } // otawa::script
+
+#endif /* OTAWA_SCRIPT_SCRIPT_H_ */
