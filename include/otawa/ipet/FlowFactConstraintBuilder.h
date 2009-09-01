ccc3439c4995fe0e085b6010a6cd2477b6c3fdda
casse <devnull@localhost>
2009-09-01 13:25:04 +0000

    Added support for generic property context system.

--- include/otawa/ipet/FlowFactConstraintBuilder.h
+++ include/otawa/ipet/FlowFactConstraintBuilder.h
@@ -49,7 +49,10 @@ protected:
 		CFG *cfg,
 		BasicBlock *caller,
 		BasicBlock *callee);
-	virtual void leavingCall(WorkSpace *ws, CFG *cfg);
+	virtual void leavingCall(
+		WorkSpace *ws,
+		CFG *cfg,
+		BasicBlock *to);
 	virtual void processBB(WorkSpace *ws, CFG *cfg, BasicBlock *bb);
 	virtual void setup(WorkSpace *ws);
 private:
