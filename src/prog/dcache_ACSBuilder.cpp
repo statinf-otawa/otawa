de33726920484aa3df77b45a7ef8453c190d6233
casse <devnull@localhost>
2009-09-10 12:19:58 +0000

    Added D$ analysis.

---
+++ src/prog/dcache_ACSBuilder.cpp
@@ -0,0 +1,200 @@
+/*
+ * DataACSBuilder.cpp
+ *
+ *  Created on: 12 juil. 2009
+ *      Author: casse
+ */
+
+#include <elm/assert.h>
+#include <otawa/prog/WorkSpace.h>
+#include <otawa/util/HalfAbsInt.h>
+#include <otawa/hard/Cache.h>
+#include <otawa/util/UnrollingListener.h>
+#include <otawa/util/DefaultListener.h>
+#include <otawa/hard/Platform.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/dcache/ACSBuilder.h>
+#include <otawa/dcache/BlockBuilder.h>
+
+namespace otawa { namespace dcache {
+
+MUSTProblem::MUSTProblem(int _size, int _set, WorkSpace *_fw, const hard::Cache *_cache, int _A)
+:	fw(_fw),
+	set(_set),
+	cache(_cache),
+	bot(_size, _A),
+	ent(_size, _A),
+	callstate(_size, _A)
+{ ent.empty(); }
+
+const MUSTProblem::Domain& MUSTProblem::bottom(void) const { return bot; }
+const MUSTProblem::Domain& MUSTProblem::entry(void) const { return ent; }
+
+void MUSTProblem::update(Domain& out, const Domain& in, BasicBlock* bb) {
+	assign(out, in);
+	const Pair<int, BlockAccess *>& accesses = DATA_BLOCKS(bb);
+	for(int i = 0; i < accesses.fst; i++) {
+		BlockAccess& acc = accesses.snd[i];
+		switch(acc.kind()) {
+		case BlockAccess::RANGE:
+			if(acc.first() < acc.last()) {
+				if(set < acc.first() || set > acc.last())
+					break;
+			}
+			else if( acc.first() < set || set < acc.last())
+				break;
+		case BlockAccess::ANY:
+			out.ageAll();
+			break;
+		case BlockAccess::BLOCK:
+			if(acc.block().set() == set)
+				out.inject(acc.block().index());
+			break;
+		}
+	}
+}
+
+elm::io::Output& operator<<(elm::io::Output& output, const MUSTProblem::Domain& dom) {
+	dom.print(output);
+	return output;
+}
+
+
+Feature<ACSBuilder> DCACHE_ACS_FEATURE("otawa::DCACHE_ACS_FEATURE");
+ Identifier<genstruct::Vector<MUSTProblem::Domain*>* > DCACHE_ACS_MUST("otawa::DCACHE_ACS_MUST", NULL);
+ Identifier<Vector<MUSTProblem::Domain*>* > DCACHE_ACS_MUST_ENTRY("otawa::DCACHE_ACS_MUST_ENTRY", NULL);
+//Identifier<genstruct::Vector<PERSProblem::Domain*>* > CACHE_ACS_PERS("otawa::CACHE_ACS_PERS", NULL);
+Identifier<bool> DATA_PSEUDO_UNROLLING("otawa::DATA_PSEUDO_UNROLLING", true);
+Identifier<data_fmlevel_t> DATA_FIRSTMISS_LEVEL("otawa::DATA_FIRSTMISS_LEVEL", DFML_MULTI);
+
+ACSBuilder::ACSBuilder(void) : Processor("otawa::DataACSBuilder", Version(1, 0, 0)) {
+	require(DOMINANCE_FEATURE);
+	require(LOOP_HEADERS_FEATURE);
+	require(LOOP_INFO_FEATURE);
+	require(DATA_BLOCK_FEATURE);
+	provide(DCACHE_ACS_FEATURE);
+}
+
+
+void ACSBuilder::processLBlockSet(WorkSpace *fw, const BlockCollection& coll, const hard::Cache *cache) {
+	if(coll.count() == 0)
+		return;
+	int line = coll.set();
+
+	/*
+	 * Solve the problem for the current cache line:
+	 * Now that the first/last lblock are detected, execute the analysis.
+	 */
+
+#ifdef DEBUG
+	cout << "[TRACE] Doing line " << line << "\n";
+#endif
+	//if (level == FML_NONE) {
+		// do only the MUST
+		MUSTProblem mustProb(coll.count(), line, fw, cache, cache->wayCount());
+
+
+		if (unrolling) {
+			UnrollingListener<MUSTProblem> mustList(fw, mustProb);
+			FirstUnrollingFixPoint<UnrollingListener<MUSTProblem> > mustFp(mustList);
+			util::HalfAbsInt<FirstUnrollingFixPoint<UnrollingListener<MUSTProblem> > > mustHai(mustFp, *fw);
+			mustHai.solve(NULL, must_entry ? must_entry->get(line) : NULL);
+
+
+			for (CFGCollection::Iterator cfg(INVOLVED_CFGS(fw)); cfg; cfg++)
+				for (CFG::BBIterator bb(cfg); bb; bb++)
+					DCACHE_ACS_MUST(bb)->set(line, new MUSTProblem::Domain(*mustList.results[cfg->number()][bb->number()]));
+
+		}
+		else {
+			DefaultListener<MUSTProblem> mustList(fw, mustProb);
+			DefaultFixPoint<DefaultListener<MUSTProblem> > mustFp(mustList);
+			util::HalfAbsInt<DefaultFixPoint<DefaultListener<MUSTProblem> > > mustHai(mustFp, *fw);
+			mustHai.solve(NULL, must_entry ? must_entry->get(line) : NULL);
+
+
+			// Store the resulting ACS into the properties
+			for (CFGCollection::Iterator cfg(INVOLVED_CFGS(fw)); cfg; cfg++)
+				for (CFG::BBIterator bb(cfg); bb; bb++) {
+					ASSERT(line == coll.set());
+					DCACHE_ACS_MUST(bb)->set(line, new MUSTProblem::Domain(*mustList.results[cfg->number()][bb->number()]));
+				}
+		}
+	/*} else {
+		if (unrolling) {
+			// Do combined MUST/PERS analysis
+			MUSTPERS mustpers(lbset->cacheBlockCount(), lbset, fw, cache, cache->wayCount());
+			UnrollingListener<MUSTPERS> mustpersList( fw, mustpers);
+			FirstUnrollingFixPoint<UnrollingListener<MUSTPERS> > mustpersFp(mustpersList);
+			util::HalfAbsInt<FirstUnrollingFixPoint<UnrollingListener<MUSTPERS> > > mustHai(mustpersFp, *fw);
+			mustHai.solve();
+
+			// Store.
+			for (CFGCollection::Iterator cfg(INVOLVED_CFGS(fw)); cfg; cfg++)
+				for (CFG::BBIterator bb(cfg); bb; bb++) {
+					MUSTProblem::Domain &must= mustpersList.results[cfg->number()][bb->number()]->getMust();
+					PERSProblem::Domain &pers= mustpersList.results[cfg->number()][bb->number()]->getPers();
+					CACHE_ACS_MUST(bb)->add(new MUSTProblem::Domain(must));
+					CACHE_ACS_PERS(bb)->add(new PERSProblem::Domain(pers));
+
+				}
+		} else {
+
+			// Do combined MUST/PERS analysis
+			MUSTPERS mustpers(lbset->cacheBlockCount(), lbset, fw, cache, cache->wayCount());
+			DefaultListener<MUSTPERS> mustpersList( fw, mustpers);
+			DefaultFixPoint<DefaultListener<MUSTPERS> > mustpersFp(mustpersList);
+			util::HalfAbsInt<DefaultFixPoint<DefaultListener<MUSTPERS> > > mustHai(mustpersFp, *fw);
+			mustHai.solve();
+
+			// Store.
+			for (CFGCollection::Iterator cfg(INVOLVED_CFGS(fw)); cfg; cfg++)
+				for (CFG::BBIterator bb(cfg); bb; bb++) {
+					MUSTProblem::Domain &must= mustpersList.results[cfg->number()][bb->number()]->getMust();
+					PERSProblem::Domain &pers= mustpersList.results[cfg->number()][bb->number()]->getPers();
+					CACHE_ACS_MUST(bb)->add(new MUSTProblem::Domain(must));
+					CACHE_ACS_PERS(bb)->add(new PERSProblem::Domain(pers));
+			}
+		}
+	}*/
+}
+
+void ACSBuilder::configure(const PropList &props) {
+	Processor::configure(props);
+	level = DATA_FIRSTMISS_LEVEL(props);
+	unrolling = DATA_PSEUDO_UNROLLING(props);
+	must_entry = DCACHE_ACS_MUST_ENTRY(props);
+}
+
+void ACSBuilder::processWorkSpace(WorkSpace *fw) {
+	//int i;
+	const hard::Cache *cache = fw->platform()->cache().dataCache();
+
+	DATA_FIRSTMISS_LEVEL(fw) = level;
+
+	// prepare the template of the final vector
+	typedef genstruct::Vector<MUSTProblem::Domain*> acs_result_t;
+	acs_result_t temp(cache->rowCount());
+	for(int i = 0; i < cache->rowCount(); i++)
+		temp.add(0);
+
+	// Build the vectors for receiving the ACS...
+	for (CFGCollection::Iterator cfg(INVOLVED_CFGS(fw)); cfg; cfg++) {
+		for (CFG::BBIterator bb(cfg); bb; bb++) {
+			DCACHE_ACS_MUST(bb) = new acs_result_t(temp);
+			/*if (level != DFML_NONE)
+				DCACHE_ACS_PERS(bb) = new genstruct::Vector<PERSProblem::Domain*>;*/
+		}
+	}
+
+	// process block collections
+	const BlockCollection *colls = DATA_BLOCK_COLLECTION(fw);
+	for (int i = 0; i < cache->rowCount(); i++) {
+		ASSERT(i == colls[i].set());
+		processLBlockSet(fw, colls[i], cache);
+	}
+
+	// !!TODO!! code to do cleanup
+}
+
+} }	// otawa::dcache
---
+++ src/prog/dcache_CatBuilder.cpp
@@ -0,0 +1,264 @@
+/*
+ * DataCatBuilder.cpp
+ *
+ *  Created on: 12 juil. 2009
+ *      Author: casse
+ */
+
+#include <otawa/hard/Platform.h>
+#include <otawa/hard/CacheConfiguration.h>
+#include <otawa/cfg/CFGCollector.h>
+#include <otawa/dcache/CatBuilder.h>
+#include <otawa/dcache/ACSMayBuilder.h>
+
+namespace otawa { namespace dcache {
+
+Identifier<BasicBlock*> CATEGORY_HEADER("otawa::dcache::CATEGORY_HEADER", 0);
+Identifier<category_t> CATEGORY("otawa::dcache::category", INVALID_CATEGORY);
+
+
+/**
+ */
+CATBuilder::CATBuilder(void): Processor("otawa::dcache::CATBuilder", Version(1, 0, 0)) {
+	require(DOMINANCE_FEATURE);
+	require(LOOP_HEADERS_FEATURE);
+	require(LOOP_INFO_FEATURE);
+	require(DCACHE_ACS_FEATURE);
+	provide(CACHE_CATEGORY_FEATURE);
+}
+
+
+void CATBuilder::cleanup(WorkSpace *ws) {
+	static cstring cat_names[] = {
+			"INV",
+			"AH",
+			"FH",
+			"FM",
+			"AM",
+			"NC"
+	};
+
+	if(!isVerbose())
+		return;
+
+	CFGCollection *cfgs = INVOLVED_CFGS(ws);
+	ASSERT(cfgs);
+	for(int i = 0; i < cfgs->count(); i++)
+		for(CFG::BBIterator bb(cfgs->get(i)); bb; bb++) {
+			cerr << "\tBB " << bb->number() << " (" << bb->address() << ")\n";
+			Pair<int, BlockAccess *> ab = DATA_BLOCKS(bb);
+			for(int j = 0; j < ab.fst; j++) {
+				BlockAccess& b = ab.snd[j];
+				cerr << "\t\t" << b << " -> " << cat_names[dcache::CATEGORY(b)] << io::endl;
+			}
+		}
+}
+
+
+/**
+ * !!TODO!!
+ */
+void CATBuilder::processLBlockSet(WorkSpace *ws, const otawa::BlockCollection& coll, const hard::Cache *cache) {
+	if(coll.count() == 0)
+		return;
+
+	// prepare problem
+	int line = coll.set();
+	MUSTProblem::Domain dom(coll.count(), cache->wayCount());
+
+	CFGCollection *cfgs = INVOLVED_CFGS(ws);
+	ASSERT(cfgs);
+	for(int i = 0; i < cfgs->count(); i++)
+		for(CFG::BBIterator bb(cfgs->get(i)); bb; bb++) {
+
+			// get the input domain
+			genstruct::Vector<MUSTProblem::Domain *> *ins = DCACHE_ACS_MUST(bb);
+			dom.set(*ins->get(line));
+
+			// explore the adresses
+			Pair<int, BlockAccess *> ab = DATA_BLOCKS(bb);
+			for(int j = 0; j < ab.fst; j++) {
+				BlockAccess& b = ab.snd[j];
+				if(b.kind() != BlockAccess::BLOCK) {
+					CATEGORY(b) = NOT_CLASSIFIED;
+					dom.ageAll();
+				}
+				else if(b.block().set() == line) {
+
+					// initialization
+					MAYProblem::Domain *may = NULL;
+					if(ACS_MAY(bb) != NULL) {
+						may = ACS_MAY(bb)->get(line);
+						CATEGORY(b) = NOT_CLASSIFIED;
+					}
+					else
+						CATEGORY(b) = ALWAYS_MISS;
+
+					// in MUST
+					if(dom.contains(b.block().index()))
+						CATEGORY(b) = ALWAYS_HIT;
+					else if(may && !may->contains(b.block().index()))
+						CATEGORY(b) = ALWAYS_MISS;
+
+					// update state
+					dom.inject(b.block().index());
+					/*else if (firstmiss_level != FML_NONE) {
+						if (LOOP_HEADER(lblock->bb()))
+							header = lblock->bb();
+					  	else header = ENCLOSING_LOOP_HEADER(lblock->bb());
+
+					  	int bound;
+					  	bool perfect_firstmiss = true;
+						PERSProblem::Domain *pers = CACHE_ACS_PERS(lblock->bb())->get(line);
+						bound = 0;
+
+						if ((pers->length() > 1) && (firstmiss_level == FML_INNER))
+							bound = pers->length() - 1;
+						CATEGORY_HEADER(lblock) = NULL;
+					  	for (int k = pers->length() - 1 ; (k >= bound) && (header != NULL); k--) {
+							if (pers->isPersistent(lblock->cacheblock(), k)) {
+								CATEGORY(lblock) = FIRST_MISS;
+								CATEGORY_HEADER(lblock) = header;
+							} else perfect_firstmiss = false;
+							header = ENCLOSING_LOOP_HEADER(header);
+						}
+
+						if ((firstmiss_level == FML_OUTER) && (perfect_firstmiss == false))
+							CATEGORY(lblock) = ALWAYS_MISS;
+					}*/
+				}
+			}
+		}
+
+	// Use the results to set the categorization
+	/*for (LBlockSet::Iterator lblock(*lbset); lblock; lblock++) {
+		if ((lblock->id() == 0) || (lblock->id() == lbset->count() - 1))
+			continue;
+
+		if (LBLOCK_ISFIRST(lblock)) {
+		} else {
+			CATEGORY(lblock) = ALWAYS_MISS;
+		}
+
+		// record stats
+		if(cstats)
+			cstats->add(CATEGORY(lblock));
+	}
+
+*/
+}
+
+
+/**
+ */
+void CATBuilder::configure(const PropList &props) {
+	Processor::configure(props);
+	firstmiss_level = DATA_FIRSTMISS_LEVEL(props);
+	//cstats = CATEGORY_STATS(props);
+	//if(cstats)
+	//	cstats->reset();
+}
+
+
+/**
+ */
+void CATBuilder::processWorkSpace(otawa::WorkSpace *fw) {
+	//int i;
+	const BlockCollection *colls = DATA_BLOCK_COLLECTION(fw);
+	const hard::Cache *cache = fw->platform()->cache().dataCache();
+
+	for (int i = 0; i < cache->rowCount(); i++) {
+		ASSERT(i == colls[i].set());
+		processLBlockSet(fw, colls[i], cache );
+	}
+}
+
+
+Feature<CATBuilder> CACHE_CATEGORY_FEATURE("otawa::dcache::CACHE_CATEGORY_FEATURE");
+
+
+/**
+ * @class CategoryStats
+ * This class is used to store statistics about the categories about cache
+ * accesses. It it provided by cache category builders.
+ * @see CATBuilder, CAT2Builder
+ */
+
+/**
+ */
+/*CategoryStats::CategoryStats(void) {
+	reset();
+}*/
+
+/**
+ * Reset the statistics.
+ */
+/*void CategoryStats::reset(void) {
+	_total = 0;
+	_linked = 0;
+	for(int i = 0; i <= NOT_CLASSIFIED; i++)
+		counts[i] = 0;
+}*/
+
+/**
+ * @fn  void Categorystats::add(category_t cat);
+ * Increment the counter for the given category.
+ * @param cat	Category to increment the counter.
+ */
+
+/**
+ * @fn void CategoryStats::addLinked(void);
+ * Add a new linked l-block to the statistics.
+ */
+
+/**
+ * @fn int CategoryStats::get(category_t cat) const;
+ * Get the counter of a category.
+ * @param cat	Category to get counter for.
+ * @return		Category count.
+ */
+
+/**
+ * @fn int CategoryStats::total(void) const;
+ * Get the total count of categories.
+ * @return		Category total count.
+ */
+
+/**
+ * @fn int CategoryStats::linked(void) const;
+ * Get the count of linked statistics.
+ * @return	Linked l-block statistics.
+ */
+
+
+/**
+ * Put in the statistics to get statistics about cache categories.
+ *
+ * @par Hooks
+ * @li processor configuration property list
+ */
+//Identifier<CategoryStats *> CATEGORY_STATS("otawa::CATEGORY_STATS", 0);
+
+
+/**
+ */
+/*io::Output& operator<<(io::Output& out, const CategoryStats& stats) {
+	static cstring names[] = {
+			"invalid",
+			"always-hit",
+			"first-hit",
+			"first-miss",
+			"always-miss",
+			"not-classified"
+	};
+
+	for(int i = ALWAYS_HIT; i <= NOT_CLASSIFIED; i++)
+		out << names[i] << '\t' << (float(stats.get(category_t(i))) * 100 / stats.total())
+			<< "% (" << stats.get(category_t(i)) << ")\n";
+	out << "total\t\t100% (" << stats.total() << ")\n";
+	out << "linked\t\t" << (float(stats.linked()) * 100 / stats.total())
+		<< "% (" << stats.linked() << ")\n";
+	return out;
+}*/
+
+} }	// otawa::dcache
