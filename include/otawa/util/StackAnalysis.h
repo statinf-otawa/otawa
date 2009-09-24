01335f1bc3f46b7e99f554c4e9582e8cc0202716
casse <devnull@localhost>
2009-09-24 23:29:40 +0000

    Added INITIAL configuration identifier.

--- include/otawa/util/StackAnalysis.h
+++ include/otawa/util/StackAnalysis.h
@@ -1,28 +1,51 @@
 /*
- * StackAnalysis.h
+ *	$Id$
+ *	StackAnalysis process interface
  *
- *  Created on: 2 juil. 2009
- *      Author: casse
+ *	This file is part of OTAWA
+ *	Copyright (c) 2009, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *	02110-1301  USA
  */
 
-#ifndef STACKANALYSIS_H_
-#define STACKANALYSIS_H_
+#ifndef OTAWA_UTIL_STACKANALYSIS_H_
+#define OTAWA_UTIL_STACKANALYSIS_H_
 
 #include <otawa/proc/Processor.h>
 
 namespace otawa {
 
+namespace hard { class Register; }
 
+// StackAnalysis class
 class StackAnalysis: public Processor {
 public:
+	typedef Pair<const hard::Register *, Address> init_t;
+	static Identifier<init_t> INITIAL;
+
 	StackAnalysis(void);
+	virtual void configure(const PropList &props);
 
 protected:
 	virtual void processWorkSpace(WorkSpace *ws);
+	genstruct::Vector<init_t> inits;
 };
 
 extern Feature<StackAnalysis> STACK_ANALYSIS_FEATURE;
 
 }	// otawa
 
-#endif /* STACKANALYSIS_H_ */
+#endif /* OTAWA_UTIL_STACKANALYSIS_H_ */
