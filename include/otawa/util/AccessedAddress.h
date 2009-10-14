6985c5f8c5199207a5b860404de5475cfdcf398b
casse <devnull@localhost>
2009-10-14 12:32:40 +0000

    Added copyright, documentation and display capability.

--- include/otawa/util/AccessedAddress.h
+++ include/otawa/util/AccessedAddress.h
@@ -1,16 +1,32 @@
 /*
- * AccessedAddress.h
+ *	$Id$
+ *	AccessAddress and AccessesAddresses classes interface
  *
- *  Created on: 2 juil. 2009
- *      Author: casse
+ *	This file is part of OTAWA
+ *	Copyright (c) 2009, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *	02110-1301  USA
  */
-
-#ifndef ACCESSEDADDRESS_H_
-#define ACCESSEDADDRESS_H_
+#ifndef OTAWA_UTIL_ACCESSEDADDRESS_H_
+#define OTAWA_UTIL_ACCESSEDADDRESS_H_
 
 #include <otawa/base.h>
 #include <otawa/prop/Identifier.h>
 #include <otawa/proc/Feature.h>
+#include <otawa/proc/BBProcessor.h>
 
 namespace otawa {
 
@@ -63,6 +79,7 @@ private:
 inline io::Output& operator<< (io::Output& out, const AccessedAddress *addr) { addr->print(out); return out; }
 
 
+// AccessedAddresses class
 class AccessedAddresses {
 public:
 	AccessedAddresses(): _size(0) { }
@@ -101,9 +118,35 @@ private:
 };
 inline io::Output& operator<<(io::Output& out, const AccessedAddresses *aa) { aa->print(out); return out; }
 
+
+// AddressStats process
+class AddressStats: public BBProcessor {
+public:
+	typedef struct stat_t {
+		int all, sprel, abs, total;
+	} stat_t;
+	static Identifier<stat_t *> STATS;
+	static Identifier<bool> DISPLAY;
+
+	AddressStats(void);
+	virtual void configure(const PropList &props);
+
+protected:
+	virtual void setup (WorkSpace *fw);
+	virtual void cleanup (WorkSpace *fw);
+	virtual void processBB (WorkSpace *fw, CFG *cfd, BasicBlock *bb);
+
+private:
+	stat_t istats, *stats;
+	bool display;
+};
+
+
+//  features
 extern Feature<NoProcessor> ADDRESS_ANALYSIS_FEATURE;
 extern Identifier<AccessedAddresses *> ADDRESSES;
 
 }	//otawa
 
-#endif /* ACCESSEDADDRESS_H_ */
+#endif /* OTAWA_UTIL_ACCESSEDADDRESS_H_ */
+
