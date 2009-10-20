bad3118f3c37592e4dddc07734e705c935621584
casse <devnull@localhost>
2009-10-20 21:54:01 +0000

    owcet is now fully working.

--- src/owcet/owcet.cpp
+++ src/owcet/owcet.cpp
@@ -22,6 +22,8 @@
 
 #include <otawa/app/Application.h>
 #include <elm/option/StringList.h>
+#include <otawa/script/Script.h>
+#include <otawa/ipet/IPET.h>
 
 using namespace otawa;
 
@@ -34,17 +36,43 @@ public:
 		"H. Cassé <casse@irit.fr>",
 		"Copyright (c) IRIT - UPS <casse@irit.fr>"
 	),
-	params(*this, 'p', "param", "parameter passed to the script", "IDENTIFIER=VALUE")
+	params(*this, 'p', "param", "parameter passed to the script", "IDENTIFIER=VALUE"),
+	script(*this, 's', "script", "script used to compute WCET", "PATH", "")
 	{ }
 
 protected:
-	virtual void process(string arg) {
+	virtual void work (const string &entry, PropList &props) throw(elm::Exception) {
 
+		// any script
+		if(!script)
+			throw option::OptionException("a script must be given !");
+
+		// fullfill the parameters
+		for(int i = 0; i < params.count(); i++) {
+			string param = params[i];
+			int idx = param.indexOf('=');
+			if(idx < 0)
+				cerr << "WARNING: argument " << param << " is malformed: VARIABLE=VALUE\n";
+			else
+				script::PARAM(props).add(pair(param.substring(0, idx), param.substring(idx + 1)));
+		}
+
+		// launch the script
+		//Processor::VERBOSE(props) = true;
+		TASK_ENTRY(props) = entry;
+		script::PATH(props) = *script;
+		script::Script scr;
+		scr.process(workspace(), props);
+
+		// display the result
+		time_t wcet = ipet::WCET(workspace());
+		cout << "WCET[" << entry << "] = " << ipet::WCET(workspace()) << " cycles\n";
 	}
 
 private:
 	option::StringList params;
-	string script, bin, task;
+	option::StringOption script;
+	string bin, task;
 };
 
 int main(int argc, char **argv) {
