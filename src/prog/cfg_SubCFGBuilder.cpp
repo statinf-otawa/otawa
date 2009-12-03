9f4abfbb435123bbec9232516e9166dd5f436c04
rochange <devnull@localhost>
2009-12-03 08:55:28 +0000

    Added copy of bbs HashTable to fix bug in iterator.

--- src/prog/cfg_SubCFGBuilder.cpp
+++ src/prog/cfg_SubCFGBuilder.cpp
@@ -39,8 +39,7 @@ DEFINE_PROC(otawa::SubCFGBuilder,
 	use(VIRTUALIZED_CFG_FEATURE);
 	invalidate(COLLECTED_CFG_FEATURE);
 	provide(VIRTUALIZED_CFG_FEATURE);
-			invalidate(LOOP_HEADERS_FEATURE);
-			invalidate(FLOW_FACTS_FEATURE);
+		
 )
 
 namespace otawa {
@@ -361,6 +360,7 @@ void SubCFGBuilder::processWorkSpace(WorkSpace *ws) {
 	// build the new CFG
 	vcfg = new VirtualCFG(false);
 	genstruct::HashTable<BasicBlock *, BasicBlock *> bbs;
+	genstruct::HashTable<BasicBlock *, BasicBlock *> second_bbs;
 
 	// make all virtual BB
 	vcfg->addBB(vcfg->entry());
@@ -378,42 +378,43 @@ void SubCFGBuilder::processWorkSpace(WorkSpace *ws) {
 		BasicBlock *vbb = new VirtualBasicBlock(bb);
 		vcfg->addBB(vbb);
 		bbs.put(bb, vbb);
+		second_bbs.put(bb, vbb);
 	}
 	vcfg->addBB(vcfg->exit());
 
 	// build the virtual edges
 	for(genstruct::HashTable<BasicBlock *, BasicBlock *>::PairIterator pair(bbs); pair; pair++) {
 		BasicBlock *src = (*pair).fst, *vsrc = (*pair).snd;
-
-		// manage start
+			// manage start
 		if(IS_START(src)) {
 			new Edge(vcfg->entry(), vsrc, Edge::VIRTUAL_CALL);
 			src->removeProp(IS_START);
-		}
+			}
 
 		// manage stop
 		if(IS_STOP(src)) {
 			new Edge(vsrc, vcfg->exit(), Edge::VIRTUAL_RETURN);
 			src->removeProp(IS_STOP);
-			continue;
+					continue;
 		}
 
 		// manage successors
 		for(BasicBlock::OutIterator edge(src); edge; edge++) {
 
 			// !!BUGGY!! we should build new CFG for the collection !
-			if(edge->kind() == Edge::CALL)
+			if(edge->kind() == Edge::CALL){
 				new Edge(vsrc, edge->target(), edge->kind());
+			}
 
 			// try to duplicate
 			else {
-				BasicBlock *vtarget;
-				if (bbs.exists(edge->target()))
-					vtarget = bbs.get(edge->target());
+				BasicBlock *vtarget = NULL;
+				//if (bbs.exists(edge->target()))
+				vtarget = second_bbs.get(edge->target(),0);
+
 				if(vtarget) {
 					new Edge(vsrc, vtarget, edge->kind());
-
-					// take care of VIRTUAL_RETURN_BLOCK
+							// take care of VIRTUAL_RETURN_BLOCK
 					if(edge->kind() == Edge::VIRTUAL_CALL) {
 						BasicBlock *ret = VIRTUAL_RETURN_BLOCK(src);
 						BasicBlock *vret = bbs.get(ret);
