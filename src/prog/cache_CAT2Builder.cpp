28e821432b68d66478935fb21993472c8f4a3c4b
casse <devnull@localhost>
2010-04-01 14:07:41 +0000

    Fixed bug crashing virtualizer with an unknown call. Cleaned registration, now only the tag list configuration is accepted. Involve a lot of simplifications.

--- src/prog/cache_CAT2Builder.cpp
+++ src/prog/cache_CAT2Builder.cpp
@@ -43,18 +43,26 @@ using namespace otawa;
 using namespace otawa::ilp;
 using namespace otawa::ipet;
 
-DEFINE_PROC(otawa::CAT2Builder,
-	version(1, 0, 0);
-	require(DOMINANCE_FEATURE);
-	require(LOOP_HEADERS_FEATURE);
-	require(LOOP_INFO_FEATURE);
-	require(COLLECTED_LBLOCKS_FEATURE);
-	require(ICACHE_ACS_FEATURE);
-	require(ICACHE_FIRSTLAST_FEATURE);
-	provide(ICACHE_CATEGORY2_FEATURE);
-)
-
 namespace otawa {
+
+/**
+ */
+CAT2Builder::CAT2Builder(AbstractRegistration& registration): CFGProcessor(registration) {
+}
+
+Registration<CAT2Builder> CAT2Builder::reg(
+	"otawa::CAT2Builder",
+	Version(1, 0, 0),
+	p::require, &DOMINANCE_FEATURE,
+	p::require, &LOOP_HEADERS_FEATURE,
+	p::require, &LOOP_INFO_FEATURE,
+	p::require, &COLLECTED_LBLOCKS_FEATURE,
+	p::require, &ICACHE_ACS_FEATURE,
+	p::require, &ICACHE_FIRSTLAST_FEATURE,
+	p::provide, &ICACHE_CATEGORY2_FEATURE,
+	p::end
+);
+
 
 /**
  *
