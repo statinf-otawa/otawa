48cd2adef1a0541a77f856e70892e46604435c67
casse <devnull@localhost>
2010-05-31 13:26:41 +0000

    Added BasicBlock::lastInst() DelayedBuilder class DELAYED_FEATURE in Process and in cfg CFGCollection::add(CFG *)
    Fixed:
    INVOLVED_CFGS uses now const CFGCollection *

--- test/subcfg/subcfg.cpp
+++ test/subcfg/subcfg.cpp
@@ -7,6 +7,7 @@
 
 #include <otawa/otawa.h>
 #include <otawa/cfg/SubCFGBuilder.h>
+#include <otawa/display/CFGOutput.h>
 
 using namespace elm;
 using namespace otawa;
@@ -14,12 +15,18 @@ using namespace otawa;
 int main(void) {
 	PropList props;
 	Processor::VERBOSE(props) = true;
-	WorkSpace *ws = MANAGER.load("/home/casse/Benchs/snu-rt.old/bs/bs", props);
+	WorkSpace *ws = MANAGER.load("main_prodcons.elf", props);
+    /*display::CFGOutput::KIND(props) = display::OUTPUT_PDF;
+    display::CFGOutput::PATH(props) = "out.pdf";*/
 
-	CFG_START(props) = 0x50140;
-	CFG_STOP(props) = 0x50150;
+
+    TASK_ENTRY(props) = "consumer";
+	CFG_START(props) = 0xa0003e10;
+	CFG_STOP(props) = 0xa0003ebc;
 	SubCFGBuilder builder;
 	builder.process(ws, props);
+
+
 
 	delete ws;
 	return 0;
