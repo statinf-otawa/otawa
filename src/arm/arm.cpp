aed5a88886f1b5704c697d32d29bf7586c43e607
rochange <devnull@localhost>
2011-07-27 13:28:16 +0000

    fixed size_t to t::size

--- src/arm/arm.cpp
+++ src/arm/arm.cpp
@@ -240,7 +240,7 @@ public:
 	inline Inst(Process& process, kind_t kind, address_t addr)
 		: otawa::loader::old_gliss::Inst(process, kind, addr) { }
 
-	virtual size_t size(void) const { return 4; }
+	virtual t::size size(void) const { return 4; }
 
 protected:
 	virtual void decodeRegs(void) {
@@ -256,7 +256,7 @@ public:
 	inline BranchInst(Process& process, kind_t kind, address_t addr)
 		: otawa::loader::old_gliss::BranchInst(process, kind, addr) { }
 
-	virtual size_t size(void) const { return 4; }
+	virtual t::size size(void) const { return 4; }
 
 protected:
 	virtual address_t decodeTargetAddress(void);
