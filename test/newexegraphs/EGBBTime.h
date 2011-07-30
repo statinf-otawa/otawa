d094472edf694ccd3807fec5f515647cc09d89e9
rochange <devnull@localhost>
2011-07-30 17:19:14 +0000

    First version of new execution graphs. Development still in progress. Only the structure of graphs has been implemented. The instruction fetch stage is not properly handled so far.

---
+++ test/newexegraphs/EGBBTime.h
@@ -0,0 +1,321 @@
+/*
+ *	$Id$
+ *	Interface to the EGBBTime classes.
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _EG_BBTIME_H_
+#define _EG_BBTIME_H_
+
+#include <otawa/prop/Identifier.h>
+#include <otawa/cfg.h>
+#include "ExecutionGraph.h"
+#include <elm/io/OutFileStream.h>
+#include <otawa/proc/BBProcessor.h>
+
+
+namespace otawa { namespace newexegraph {
+    extern Identifier<String> GRAPHS_DIR;
+ //   extern Identifier<int> TIME;
+
+    using namespace elm::genstruct;
+
+    // -- class PathContext --------------------------------------------------------------------------------
+    class EGPathContext{
+    private:
+		elm::genstruct::SLList<BasicBlock *> _bb_list;
+		int _num_insts;
+		int _num_bbs;
+		BasicBlock * _bb;
+		Edge * _edge;
+    public:
+		EGPathContext(BasicBlock *bb){
+			_bb_list.addFirst(bb);
+			_num_insts = bb->countInstructions();
+			_num_bbs = 1;
+			_bb = bb;
+			_edge = NULL;
+		}
+		EGPathContext(const EGPathContext& ctxt){
+			for (elm::genstruct::SLList<BasicBlock *>::Iterator block(ctxt._bb_list) ; block ; block++)
+				_bb_list.addLast(block);
+			_num_insts = ctxt._num_insts;
+			_num_bbs = ctxt._num_bbs;
+			_bb = ctxt._bb;
+			_edge = ctxt._edge;
+		}
+		~EGPathContext(){
+			_bb_list.clear();
+		}
+		void addBlock(BasicBlock * new_bb, Edge * edge){
+			_bb_list.addFirst(new_bb);
+			_num_insts += new_bb->countInstructions();
+			_num_bbs += 1;
+			if (_num_bbs == 1)
+				_bb = new_bb;
+			if (_num_bbs == 2)
+				_edge = edge;
+		}
+		inline int numInsts()
+		{ return _num_insts;}
+		inline int numBlocks()
+		{ return _num_bbs;}
+		inline BasicBlock* lastBlock()
+		{ return _bb_list.last();}
+		inline BasicBlock* mainBlock()
+		{ return _bb;}
+		inline Edge * edge     ()
+		{ return _edge;}
+		void dump(io::Output& output) {
+			for (elm::genstruct::SLList<BasicBlock *>::Iterator bb(_bb_list) ; bb ; bb++){
+				output << "b" << bb->number() << "-";
+			}
+		}
+
+		class BasicBlockIterator: public elm::genstruct::SLList<BasicBlock *>::Iterator {
+		public:
+			inline BasicBlockIterator(const EGPathContext& ctxt)
+				: elm::genstruct::SLList<BasicBlock *>::Iterator(ctxt._bb_list) {}
+		};
+
+    };
+
+
+    // -- class EGBBTime ----------------------------------------------------------------------------------
+
+    template <class G>
+		class EGBBTime: public BBProcessor {
+    private:
+		WorkSpace *_ws;
+		EGProc *_microprocessor;
+		int _last_stage_cap;
+		PropList _props;
+		int _prologue_depth;
+		OutStream *_output_stream;
+		elm::io::Output *_output;
+		String _graphs_dir_name;
+		bool _do_output_graphs;
+
+    public:
+		EGBBTime(const PropList& props = PropList::EMPTY);
+		EGBBTime(AbstractRegistration& reg);
+		virtual void configure(const PropList& props);
+
+		void processWorkSpace(WorkSpace *ws);
+		void processBB(WorkSpace *ws, CFG *cfg, BasicBlock *bb);
+		elm::genstruct::SLList<EGPathContext *> * buildListOfPathContexts(BasicBlock *bb, int depth = 1);
+		void FillSequence(EGPathContext *ctxt,
+						  elm::genstruct::SLList<EGPathContext *> *context_list,
+						  int depth);
+		EGSequence * buildSequence(EGPathContext *ctxt);
+		void analyzePathContext(EGPathContext *ctxt, int context_index);
+		void outputGraph(G* graph, int bb_number, int context_number, int case_number, const string& info = "");
+
+    };
+	// -- EGBBTime ------------------------------------------------------------------------------------------
+
+
+	template <class G>
+		EGBBTime<G>::EGBBTime(const PropList& props)
+		: BBProcessor() {
+		require(otawa::hard::PROCESSOR_FEATURE);
+		_graphs_dir_name = GRAPHS_DIR(props);
+		if (!_graphs_dir_name.isEmpty())
+			_do_output_graphs = true;
+		else
+			_do_output_graphs = false;
+
+		_props = props;
+	}
+
+template <class G>
+EGBBTime<G>::EGBBTime(AbstractRegistration& reg)
+: BBProcessor(reg) {
+	require(otawa::hard::PROCESSOR_FEATURE);
+}
+
+template <class G>
+void EGBBTime<G>::configure(const PropList& props) {
+	BBProcessor::configure(props);
+	_graphs_dir_name = GRAPHS_DIR(props);
+	if(!_graphs_dir_name.isEmpty())
+		_do_output_graphs = true;
+	else
+		_do_output_graphs = false;
+	_props = props;
+}
+
+
+
+	// -- processWorkSpace ------------------------------------------------------------------------------------------
+
+	template <class G>
+		void EGBBTime<G>::processWorkSpace(WorkSpace *ws) {
+
+		_ws = ws;
+		const otawa::hard::Processor *proc = otawa::hard::PROCESSOR(_ws);
+
+		if(proc == &otawa::hard::Processor::null)
+			throw ProcessorException(*this, "no processor to work with");
+		else {
+			_microprocessor = new EGProc(proc);
+			_last_stage_cap = _microprocessor->lastStage()->width();
+
+		}
+
+		// Perform the actual process
+		BBProcessor::processWorkSpace(ws);
+	}
+
+
+	// -- FillSequence ------------------------------------------------------------------------------------------
+
+	template <class G>
+		void EGBBTime<G>::FillSequence(EGPathContext *ctxt,
+										  elm::genstruct::SLList<EGPathContext *> *context_list,
+										  int depth){
+
+		BasicBlock *bb = ctxt->lastBlock();
+		int num_preds = 0;
+		for(BasicBlock::InIterator edge(bb); edge; edge++) {
+			BasicBlock *pred = edge->source();
+			if (!pred->isEntry() && !pred->isExit()) {
+				num_preds++;
+				EGPathContext *new_ctxt = new EGPathContext(*ctxt);
+				new_ctxt->addBlock(pred, edge);
+				if ( (new_ctxt->numInsts() >= _last_stage_cap)
+					 &&
+					 (new_ctxt->numBlocks() > depth) )
+					context_list->addLast(new_ctxt);
+				else
+					FillSequence(new_ctxt, context_list, depth);
+			}
+		}
+		if (num_preds == 0){
+			context_list->addLast(ctxt);
+		}
+		else
+			delete ctxt;
+	}
+
+
+	// -- buildListOfPathContexts ---------------------------------------------------------------------------------------
+
+	template <class G>
+		elm::genstruct::SLList<EGPathContext *> * EGBBTime<G>::buildListOfPathContexts(BasicBlock *bb, int depth){
+		assert(depth > 0);
+		elm::genstruct::SLList<EGPathContext *> * context_list = new elm::genstruct::SLList<EGPathContext *>();
+		EGPathContext * ctxt = new EGPathContext(bb);
+
+		FillSequence(ctxt, context_list, depth);
+
+		return context_list;
+	}
+
+	// -- buildSequence ------------------------------------------------------------------------------------------
+
+	template <class G>
+		EGSequence * EGBBTime<G>::buildSequence(EGPathContext *ctxt){
+		EGSequence * seq = new EGSequence();
+		part_t part = PREDECESSOR;
+		int index = 0;
+		for (EGPathContext::BasicBlockIterator block(*ctxt) ; block ; block++){
+			if (block == ctxt->mainBlock())
+				part = BLOCK;
+			for(BasicBlock::InstIterator inst(block); inst; inst++) {
+				EGInst * par_exe_inst = new EGInst(inst, block, part, index++);
+				seq->add(par_exe_inst);
+			}
+		}
+		return seq;
+	}
+
+	// -- outputGraph ------------------------------------------------------------------------------------------
+
+	template <class G>
+		void EGBBTime<G>::outputGraph(G* graph, int bb_number, int context_index, int case_index, const string& info){
+		elm::StringBuffer buffer;
+		buffer << _graphs_dir_name << "/";
+		buffer << "b" << bb_number << "-ctxt" << context_index << "-case" << case_index << ".dot";
+		elm::io::OutFileStream dotStream(buffer.toString());
+		elm::io::Output dotFile(dotStream);
+		graph->dump(dotFile, info);
+	}
+
+	// -- analyzePathContext ------------------------------------------------------------------------------------------
+
+	template <class G>
+		void EGBBTime<G>::analyzePathContext(EGPathContext*ctxt, int context_index){
+
+		int case_index = 0;
+		BasicBlock * bb = ctxt->mainBlock();
+		Edge *edge = ctxt->edge();
+
+
+		EGSequence *sequence = buildSequence(ctxt);
+		G *execution_graph = new G(_ws,_microprocessor, sequence, _props);
+		execution_graph->build();
+
+		if (_do_output_graphs){
+			outputGraph(execution_graph, bb->number(), context_index, case_index++,
+				_ << "");
+		}
+
+
+		delete execution_graph;
+	}
+
+
+	// -- processBB ------------------------------------------------------------------------------------------
+
+	template <class G>
+		void EGBBTime<G>::processBB(WorkSpace *ws, CFG *cfg, BasicBlock *bb) {
+
+		// ignore empty basic blocks
+		if (bb->countInstructions() == 0)
+			return;
+
+		if(isVerbose()) {
+			log << "\n\t\t================================================================\n";
+			log << "\t\tProcessing block b" << bb->number() << " (starts at " << bb->address() << " - " << bb->countInstructions() << " instructions)\n";
+		}
+
+		int context_index = 0;
+
+		elm::genstruct::SLList<EGPathContext *> *path_context_list = buildListOfPathContexts(bb);
+
+		for (elm::genstruct::SLList<EGPathContext *>::Iterator ctxt(*path_context_list) ; ctxt ; ctxt++){
+			if(isVerbose()) {
+				log << "\n\t\t----- Considering context: ";
+				ctxt->dump(log);
+				log << "\n";
+			}
+			analyzePathContext(ctxt, context_index);
+			context_index ++;
+		}
+	}
+
+} // namespace newexegraph
+} // namespace otawa
+
+#endif // _EG_BBTIME_H_
+
+
+
+
---
+++ test/newexegraphs/ExecutionGraph.h
@@ -0,0 +1,260 @@
+/*
+ *	$Id$
+ *	Interface to the ExecutionGraph, EGNode, EGEdge classes.
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2007, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _BASIC_EXEGRAPH_H_
+#define _BASIC_EXEGRAPH_H_
+
+
+#include <elm/string/StringBuffer.h>
+#include <otawa/cfg/BasicBlock.h>
+#include <otawa/hard/Platform.h>
+#include <otawa/graph/PreorderIterator.h>
+#include "EGInst.h"
+#include "EGProc.h"
+#include "TAATimingInfo.h"
+
+namespace otawa { namespace newexegraph  {
+
+/*
+ * class EGNode
+ *
+ */
+class EGNode: public GenGraph<EGNode,EGEdge>::GenNode{
+private:
+	EGStage *_pipeline_stage;
+	EGInst *_inst;
+	int _latency;
+	elm::String _name;
+	TimingInfo *_timing;
+protected:
+	elm::genstruct::Vector<EGNode *> _producers;
+	elm::genstruct::Vector<EGNode *> _contenders;
+	elm::BitVector * _possible_contenders;
+	elm::genstruct::DLList<elm::BitVector *> _contenders_masks_list;
+	int _late_contenders;
+
+public:
+	inline EGNode(ExecutionGraph *graph, EGStage *stage, EGInst *inst)
+		: GenNode((otawa::graph::Graph *) graph),
+		_pipeline_stage(stage), _inst(inst),  _latency(stage->latency()){
+	}
+	~EGNode(){}
+	// get information
+	inline EGStage *stage()
+		{return _pipeline_stage;}
+	inline EGInst *inst()
+		{return _inst;}
+	inline int latency()
+		{return _latency;}
+	inline elm::String name()
+		{return _name;}
+	inline TimingInfo * timing()
+		{return _timing;}
+	inline int numProducers()
+		{return _producers.length();}
+	inline EGNode *producer(int index)
+		{return _producers[index];}
+	// set information
+	inline void setTiming(TimingInfo *timing)
+		{_timing = timing;}
+	inline void addProducer(EGNode *prod) {
+		if (!_producers.contains(prod))
+			_producers.add(prod);
+	}
+};
+
+/*
+ * class EGEdge
+ *
+ */
+class EGEdge: public GenGraph<EGNode,EGEdge>::GenEdge{
+public:
+	typedef enum edge_type_t {SOLID = 1, SLASHED = 2} edge_type_t_t;
+private:
+	edge_type_t _type;
+	elm::String _name;
+	int _latency;
+public:
+	inline EGEdge(EGNode *source, EGNode *target, edge_type_t type, int latency=0)
+		: GenEdge(source, target), _type(type), _latency(latency) {
+		assert(source!=target);
+		elm::StringBuffer _buffer;
+		_buffer << GenEdge::source()->name() << "->" ;
+		_buffer << GenEdge::target()->name();
+		_name = _buffer.toString();
+	}
+	//~ParExeEdge();
+	inline int latency()
+	{return _latency;}
+	inline edge_type_t type(void)
+	{return _type;}
+	inline elm::String name()
+	{return _name;}
+
+};
+
+	/*
+	 * class ExecutionGraph
+	 *
+	 */
+
+	class ExecutionGraph: public GenGraph<EGNode, EGEdge> {
+		friend class InstIterator;
+	protected:
+		WorkSpace * _ws;
+		PropList _props;
+		EGProc * _microprocessor;
+		typedef struct rename_table_t {
+			otawa::hard::RegBank * reg_bank;
+			elm::genstruct::AllocatedTable<EGNode *> *table;
+		} rename_table_t;
+//		elm::genstruct::Vector<Resource *> _resources;
+	private:
+		EGSequence * _sequence;
+		EGNode *_first_node;
+		EGNode *_first_bb_node;
+		EGNode *_last_prologue_node;
+		EGNode *_last_node;
+		int _cache_line_size;
+		int _capacity;
+		int _branch_penalty;
+
+
+	public:
+		ExecutionGraph(WorkSpace * ws, EGProc *proc, EGSequence *seq, const PropList& props = PropList::EMPTY);
+		~ExecutionGraph();
+		inline void setFetchSize(int size) { _cache_line_size = size; }
+		inline void setBranchPenalty(int penalty) { _branch_penalty = penalty; }
+
+		void build(bool compressed_code=false);
+		void createNodes();
+		void findDataDependencies();
+		void addEdgesForPipelineOrder();
+		void addEdgesForFetch();
+		void addEdgesForFetchWithDecomp();
+		void addEdgesForProgramOrder(elm::genstruct::SLList<EGStage *> *list_of_stages = NULL);
+		void addEdgesForMemoryOrder();
+		void addEdgesForDataDependencies();
+		void addEdgesForQueues();
+		void findContendingNodes();
+		void createResources();
+		void dump(elm::io::Output& dotFile, const string& info = "");
+		void display(elm::io::Output& output);
+
+		inline int numInstructions()
+			{return _sequence->length();}
+		inline EGNode * firstNode()
+				{return _first_node;}
+
+		class InstIterator : public EGSequence::InstIterator {
+		public:
+			inline InstIterator(const EGSequence *sequence)
+				: EGSequence::InstIterator(sequence) {}
+			inline InstIterator(const ExecutionGraph *graph)
+				: EGSequence::InstIterator(graph->_sequence) {}
+		};
+		class InstNodeIterator : public EGInst::NodeIterator {
+		public:
+			inline InstNodeIterator(const EGInst *inst)
+				: EGInst::NodeIterator(inst) {}
+		};
+		class StageIterator : public elm::genstruct::SLList<EGStage *>::Iterator {
+		public:
+			inline StageIterator(const SLList<EGStage *>  *list)
+				: elm::genstruct::SLList<EGStage *>::Iterator(*list) {}
+		};
+
+		class StageNodeIterator : public EGStage::NodeIterator {
+		public:
+			inline StageNodeIterator(const EGStage *stage)
+				: EGStage::NodeIterator(stage) {}
+		};
+
+		class PreorderIterator: public graph::PreorderIterator<ExecutionGraph> {
+		public:
+			inline PreorderIterator(ExecutionGraph * graph)
+				: graph::PreorderIterator<ExecutionGraph>(*graph, graph->firstNode()) {}
+		};
+
+		class Predecessor: public PreIterator<Predecessor, EGNode *> {
+		public:
+			inline Predecessor(const EGNode* node): iter(node) { }
+			inline bool ended(void) const;
+			inline EGNode *item(void) const;
+			inline void next(void);
+			inline EGEdge *edge(void) const;
+		private:
+			GenGraph<EGNode,EGEdge>::InIterator iter;
+		};
+
+		class Successor: public PreIterator<Successor, EGNode *> {
+		public:
+			inline Successor(const EGNode* node): iter(node) {}
+			inline bool ended(void) const;
+			inline EGNode *item(void) const;
+			inline void next(void);
+			inline EGEdge *edge(void) const;
+		private:
+			GenGraph<EGNode,EGEdge>::OutIterator iter;
+		};
+
+
+	};
+
+
+
+
+	inline bool ExecutionGraph::Predecessor::ended(void) const {
+		return iter.ended();
+	}
+	inline EGNode *ExecutionGraph::Predecessor::item(void) const {
+		return iter->source();
+	}
+	inline void ExecutionGraph::Predecessor::next(void) {
+		iter.next();
+	}
+	inline EGEdge *ExecutionGraph::Predecessor::edge(void) const {
+		return iter;
+	}
+
+	inline bool ExecutionGraph::Successor::ended(void) const {
+		return iter.ended();
+	}
+	inline EGNode *ExecutionGraph::Successor::item(void) const {
+		return iter->target();
+	}
+	inline void ExecutionGraph::Successor::next(void) {
+		iter.next();
+	}
+	inline EGEdge *ExecutionGraph::Successor::edge(void) const {
+		return iter;
+	}
+
+
+} // namespace newexegraph
+} // namespace otawa
+
+#endif //_BASIC_EXEGRAPH_H_
+
+
+
+
