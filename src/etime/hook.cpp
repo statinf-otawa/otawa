6cf87a1f196d20bc994e59ee7c17b7807857308b
casse <devnull@localhost>
2014-03-27 14:09:23 +0100

    etime: added module.

---
+++ src/etime/hook.cpp
@@ -0,0 +1,37 @@
+/*
+ *	etime plugin hook
+ *
+ *	This file is part of OTAWA
+ *	Copyright (c) 2014, IRIT UPS.
+ *
+ *	OTAWA is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OTAWA is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OTAWA; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <otawa/proc/ProcessorPlugin.h>
+
+namespace otawa { namespace etime {
+
+class Plugin: public ProcessorPlugin {
+public:
+	typedef genstruct::Table<AbstractRegistration * > procs_t;
+
+	Plugin(void): ProcessorPlugin("otawa::etime", Version(1, 0, 0), OTAWA_PROC_VERSION) { }
+	virtual procs_t& processors(void) const { return procs_t::EMPTY; };
+};
+
+} }	// otawa::etime
+
+otawa::etime::Plugin OTAWA_PROC_HOOK;
+otawa::etime::Plugin& otawa_etime = OTAWA_PROC_HOOK;
