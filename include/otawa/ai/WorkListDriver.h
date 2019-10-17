8e7193c32a9a8030cadca6c937395edbd913509a
clement <Clement.Ballabriga@univ-lille.fr>
2019-10-17 10:57:51 +0200

    added OrderedDriver, an AI worklist driver that supports an optional partial order to improve analysis speed.

--- include/otawa/ai/WorkListDriver.h
+++ include/otawa/ai/WorkListDriver.h
@@ -213,6 +213,186 @@ private:
 	bool end;
 };
 
+/**
+ * Driver of abstract interpretation with a simple to-do list, supporting an optional order.
+ * @param D		Current domain (must implement otawa::ai::Domain concept).
+ * @param G		Graph (must implement otawa::ai::Graph concept).
+ * @param S		Storage.
+ * @param O		Basic block order (must implement isBefore(G::vertex_t v1, G::vertex_tv2)
+ */
+
+template <class D, class G, class S, class O>
+class OrderedDriver: public PreIterator<OrderedDriver<D, G, S, O>, typename G::vertex_t > {
+public:
+	typedef typename G::vertex_t vertex_t;
+
+	/**
+	 * Initialize the driver.
+	 * @param dom	Domain.
+	 * @param graph	Graph to analyze.
+	 * @param store	Storage to get domain values.
+	 * @param order Optional order
+	 */
+	OrderedDriver(D& dom, const G& graph, S& store, O *order)
+	: _dom(dom), _graph(graph), _store(store), wl_set(graph.count()), end(false), _order(order) {
+		store.set(_graph.entry(), dom.init());
+		for(typename G::Successor succ(graph, _graph.entry()); succ; succ++)
+			push(graph.sinkOf(*succ));
+		next();
+	}
+
+	/**
+	 * Test if the traversal is ended.
+	 * @return	True if ended, false else.
+	 */
+	inline bool ended(void) {
+		return end;
+	}
+
+	/**
+	 * Go to the next vertex to process.
+	 */
+	inline void next(void) {
+		while(!wl_vertices.isEmpty()) {
+			cur = pop();
+			if(cur != _graph.exit())
+				return;
+		}
+		end = true;
+	}
+
+	/**
+	 * Get the current vertex.
+	 * @return	Current vertex.
+	 */
+	inline typename G::vertex_t item(void) {
+		return cur;
+	}
+
+	/**
+	 * Called when the output state of the current vertex is changed
+	 * (and successors must be updated).
+	 */
+	inline void change(void) {
+		for(typename G::Successor succ(_graph, cur); succ; succ++)
+			push(*succ);
+	}
+
+	/**
+	 * Set the new output state of the current vertex.
+	 */
+	inline void change(typename D::t s) {
+		_store.set(cur, s);
+		change();
+	}
+
+	/**
+	 * Called when the output state of the current vertex for the given edge is changed
+	 * (and successors must be updated).
+	 */
+	inline void change(typename G::edge_t edge) {
+		push(_graph.sinkOf(edge));
+	}
+
+	/**
+	 * Change the output value of the current vertex for the given edge.
+	 */
+	inline void change(typename G::edge_t edge, typename D::t s) {
+		_store.set(edge, s);
+		change(edge);
+	}
+
+	/**
+	 * Consider that all has been changed causing a re-computation of all.
+	 */
+	inline void changeAll(void) {
+
+		// collect vertices
+		wl_vertices.clear();
+		Vector<vertex_t> todo;
+		todo.push(_graph.entry());
+		while(todo) {
+			vertex_t v = todo.pop();
+			for(typename G::Successor s(_graph, v); s; s++)
+				if(!contains(_graph.sinkOf(s))) {
+					push(_graph.sinkOf(s));
+					todo.push(_graph.sinkOf(s));
+				}
+		}
+
+		// reverse the order
+		for(int i = 0, j = wl_vertices.length() - 1; i < j; i++, j--) {
+			vertex_t tmp = wl_vertices[i];
+			wl_vertices[i] = wl_vertices[j];
+			wl_vertices[j] = tmp;
+		}
+
+		// and start...
+		next();
+	}
+
+	/**
+	 * If there is a state change for the given edge.
+	 */
+	inline void check(typename G::edge_t edge, typename D::t s) {
+		typename D::t ps = _store.get(edge);
+		if(!_dom.equals(s, ps))
+			change(edge, s);
+	}
+
+	/**
+	 * Compute the input state.
+	 */
+	inline typename D::t input(void) {
+		return input(cur);
+	}
+
+	/**
+	 * Compute the input state for the given vertex.
+	 * @param 	vertex	Vertex whose input is required.
+	 * @return			Input state of vertex.
+	 */
+	inline typename D::t input(vertex_t vertex) {
+		typename D::t s = _dom.bot();
+		for(typename G::Predecessor pred(_graph, vertex); pred; pred++) {
+			s = _dom.join(s, _store.get(*pred));
+		}
+		return s;
+	}
+
+private:
+	inline void push(typename G::vertex_t v) {
+			if(!wl_set.bit(_graph.index(v))) {
+					if (_order != nullptr) {
+						int i;
+						for (i = 0; i < wl_vertices.count() && _order->isBefore(v, wl_vertices[i]); i++);
+						wl_vertices.insert(i, v);
+					} else wl_vertices.push(v);
+					wl_set.set(_graph.index(v));
+			}
+	}
+
+	inline typename G::vertex_t pop(void) {
+		typename G::vertex_t v = wl_vertices.pop();
+		wl_set.clear(_graph.index(v));
+		return v;
+	}
+
+	inline bool contains(vertex_t v) {
+		return wl_set.bit(_graph.index(v));
+	}
+
+	D& _dom;
+	const G& _graph;
+	S& _store;
+	Vector<typename G::vertex_t> wl_vertices;
+	BitVector wl_set;
+	typename G::vertex_t cur;
+	bool end;
+	O *_order;
+};
+
+
 } }	// otawa::ai
 
 #endif /* OTAWA_AI_WORKLISTDRIVER_H_ */
