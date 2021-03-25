ddc8aa1f158407c6309840ca2c32a704dfde84ad
hcasse <casse@irit.fr>
2021-03-25 07:16:18 +0100

    hard: small extensions to Stage.

--- src/prog/hard_Processor.cpp
+++ src/prog/hard_Processor.cpp
@@ -244,17 +244,28 @@ Stage::~Stage(void)
  * @return		Found functional unit or null.
  */
 const PipelineUnit *Stage::select(Inst *inst) const {
+	return select(inst->kind());
+}
+
+
+/**
+ * @fn bool Stage::hasFUs() const;
+ * Test if the stage contains functional units.
+ * @return	True if it contains functional, false else.
+ */
+
+const PipelineUnit *Stage::select(Inst::kind_t kind) const {
 	if(fus.isEmpty())
 		return this;
 	ASSERT(dispatch.count() > 0);
-	Inst::kind_t kind = inst->kind();
 	for(int i = 0; i < dispatch.count(); i++) {
 		Inst::kind_t mask = dispatch[i]->getType();
 		if((mask & kind) == mask)
 			return dispatch[i]->getFU();
 	}
-	return 0;
+	return nullptr;	
 }
+
 
 
 /**
