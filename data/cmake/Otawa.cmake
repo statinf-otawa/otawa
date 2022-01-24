b6a0b6182de385fff7a97732463d4353c41df608
casse <60-casse@users.noreply.022e47118ec0>
2022-01-24 15:20:27 +0100

    Take back the GLISS function that were finally used in loader rplugins.

--- data/cmake/Otawa.cmake
+++ data/cmake/Otawa.cmake
@@ -119,3 +119,37 @@ function(OTAWA_INSTALL_INCLUDE _DIR)
 		OUTPUT_VARIABLE OTAWA_PREFIX 	OUTPUT_STRIP_TRAILING_WHITESPACE)
 	install(DIRECTORY "${_DIR}" DESTINATION "${OTAWA_PREFIX}/include")
 endfunction()
+
+
+function(MAKE_GLISS_FUNCTION _VAR _KEY _NMP _IRG _DEF) 
+	set(_H "${CMAKE_CURRENT_BINARY_DIR}/${_KEY}.h")
+	set(${_VAR} "${_H}" PARENT_SCOPE)
+	set(_ARGN_NMP )
+	foreach(_NMP ${ARGN})
+		list(APPEND _ARGN_NMP -e ${_NMP})
+	endforeach()
+	add_custom_command(
+		OUTPUT "${_H}"
+		DEPENDS "${_KEY}.tpl" ${_NMP} "${_IRG}" ${ARGN}
+		COMMAND "${GLISS_ATTR}"
+		ARGS "${_IRG}" -o "${_H}" -a "${_KEY}" -f -t "${_KEY}.tpl" -d "${_DEF}" -e "${_NMP}" ${_ARGN_NMP}
+     	VERBATIM
+     )
+ endfunction()
+
+ 
+function(MAKE_GLISS_PROCEDURE _VAR _KEY _NMP _IRG _DEF) 
+	set(_H "${CMAKE_CURRENT_BINARY_DIR}/${_KEY}.h")
+	set(${_VAR} "${_H}" PARENT_SCOPE)
+	set(_ARGN_NMP )
+	foreach(_NMP ${ARGN})
+		list(APPEND _ARGN_NMP -e ${_NMP})
+	endforeach()
+	add_custom_command(
+		OUTPUT "${_H}"
+		DEPENDS "${_KEY}.tpl" ${_NMP} "${_IRG}" ${ARGN}
+		COMMAND "${GLISS_ATTR}"
+		ARGS "${_IRG}" -o "${_H}" -a "${_KEY}" -p -t "${_KEY}.tpl" -d "${_DEF}" -e "${_NMP}" ${_ARGN_NMP}
+     	VERBATIM
+	)
+endfunction()
